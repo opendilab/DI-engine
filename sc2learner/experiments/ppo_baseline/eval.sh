4e632f66f5c9114b7f62ea069afb909993b9630a
niuyazhe <niuyazhe@sensetime.com>
2019-12-09 15:40:23 +0800

    add new evaluate and ppo agent

---
+++ sc2learner/experiments/ppo_baseline/eval.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+work_path=$(dirname $0)
+ITER=0
+srun -p $1 --gres=gpu:1 python3 -u -m sc2learner.bin.evaluate \
+    --config_path $work_path/eval.yaml \
+    --load_path $work_path/checkpoints/iterations_$ITER.pth.tar
