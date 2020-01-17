945fcb85b61b95d09413bfd84b226b14987adadc
niuyazhe <niuyazhe@sensetime.com>
2020-01-17 21:28:30 +0800

    add multi-process replay decode

--- sc2learner/bin/replay_decode.py
+++ sc2learner/bin/replay_decode.py
@@ -41,6 +41,7 @@ from pysc2.lib.action_dict import ACTION_INFO_MASK
 from s2clientprotocol import sc2api_pb2 as sc_pb
 from sc2learner.envs.observations.alphastar_obs_wrapper import AlphastarObsParser
 from sc2learner.envs.actions.alphastar_act_wrapper import AlphastarActParser
+from functools import partial
 
 FLAGS = flags.FLAGS
 flags.DEFINE_integer("parallel", 1, "How many instances to run in parallel.")
@@ -165,8 +166,9 @@ class ReplayProcessor(multiprocessing.Process):
                 torch.save(meta_data_1, os.path.join(self.output_dir, name1+'.meta'))
                 torch.save(step_data_1, os.path.join(self.output_dir, name1+'.step'))
                 torch.save(stat1, os.path.join(self.output_dir, name1+'.stat'))
+                return "success parse replay " + replay_path
             else:
-                return
+                return "invalid replay " + replay_path
         except (protocol.ConnectionError, protocol.ProtocolError,
                 remote_controller.RequestError):
             raise Exception
@@ -222,14 +224,14 @@ class ReplayProcessor(multiprocessing.Process):
                 for unit_tag in act['selected_units']:
                     unit_type = get_unit_type(unit_tag.item(), obs)
                     if unit_type is None:
-                        print("not found selected unit in screen(id: {})".format(unit_tag.item()))
+                        print("not found selected unit(id: {})".format(unit_tag.item()))
                         continue
                     action_statistics[action_type]['selected_type'].add(unit_type)
             if isinstance(act['target_units'], torch.Tensor):
                 for unit_tag in act['target_units']:
                     unit_type = get_unit_type(unit_tag.item(), obs)
                     if unit_type is None:
-                        print("not found target unit in screen(id: {})".format(unit_tag.item()))
+                        print("not found target unit(id: {})".format(unit_tag.item()))
                         continue
                     action_statistics[action_type]['target_type'].add(unit_type)
 
@@ -321,7 +323,8 @@ class ReplayProcessor(multiprocessing.Process):
 
             controllers[0].step(FLAGS.step_mul)
             controllers[1].step(FLAGS.step_mul)
-            print('step', step)
+            if step % 1000 == 0:
+                print('step', step)
             step += FLAGS.step_mul
             delay[0] += FLAGS.step_mul
             delay[1] += FLAGS.step_mul
@@ -344,31 +347,67 @@ def main(unused_argv):
         print("Caught KeyboardInterrupt, exiting.")
 
 
+def replay_decode(paths, version):
+    run_config = run_configs.get(version=version)
+    p = ReplayProcessor(run_config, output_dir=FLAGS.output_dir)
+    success_msg = []
+    error_msg = []
+    for idx, path in enumerate(paths):
+        print('idx', idx, path)
+        try:
+            ret = p.run(path)
+            print('{}/{}---{}'.format(idx, len(paths), ret))
+            if "invalid" in ret:
+                error_msg.append(ret)
+            else:
+                success_msg.append(ret)
+        except KeyboardInterrupt:
+            print("Caught KeyboardInterrupt, exiting.")
+        except Exception as e:
+            error_msg.append(repr(e))
+    return success_msg, error_msg
+
+
 def main_multi(unused_argv):
     from multiprocessing import Pool
     """Dump stats about all the actions that are in use in a set of replays."""
     run_config = run_configs.get()
     replay_list = sorted(run_config.replay_paths(FLAGS.replays))
     version = replay.get_replay_version(run_config.replay_data(replay_list[0]))
-    run_config = run_configs.get(version=version)
-
-    def func(paths):
-        p = ReplayProcessor(run_config, output_dir=FLAGS.output_dir)
-        for idx, path in enumerate(paths):
-            try:
-                p.run(path)
-            except KeyboardInterrupt:
-                print("Caught KeyboardInterrupt, exiting.")
 
     if not gfile.Exists(FLAGS.replays):
         sys.exit("{} doesn't exist.".format(FLAGS.replays))
 
-    N = 1
+    def log_func(s, idx):
+        return s + '\n{}\t'.format(idx) + '-'*60 + '\n'
+
+    def combine_msg(msg):
+        msg = list(zip(*msg))
+        success_msg, error_msg = msg
+        new_success_msg, new_error_msg = [], []
+        for item in success_msg:
+            new_success_msg += item
+        for item in error_msg:
+            new_error_msg += item
+        new_success_msg = [log_func(s, idx) for idx, s in enumerate(new_success_msg)]
+        new_error_msg = [log_func(s, idx) for idx, s in enumerate(new_error_msg)]
+        return new_success_msg, new_error_msg
+
+    N = 40
     pool = Pool(N)
-    pool.map(func, replay_list)
+    group_num = int(len(replay_list) // N)
+    print('total len: {}, group: {}, each group: {}'.format(len(replay_list), N, group_num))
+    replay_split_list = [replay_list[i*group_num:(i+1)*group_num] for i in range(group_num)]
+    func = partial(replay_decode, version=version)
+    ret = pool.map(func, replay_split_list)
+    success_msg, error_msg = combine_msg(ret)
+    with open(os.path.join(FLAGS.output_dir, 'success.txt'), 'w') as f:
+        f.writelines(success_msg)
+    with open(os.path.join(FLAGS.output_dir, 'error.txt'), 'w') as f:
+        f.writelines(error_msg)
     pool.close()
     pool.join()
 
 
 if __name__ == "__main__":
-    app.run(main)
+    app.run(main_multi)
