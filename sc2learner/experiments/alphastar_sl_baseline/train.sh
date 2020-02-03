db7163e83532910de190ff48d26611a23d61834c
niuyazhe <niuyazhe@sensetime.com>
2020-02-03 13:03:19 +0800

    fix load_path bug in shell

--- sc2learner/experiments/alphastar_sl_baseline/train.sh
+++ sc2learner/experiments/alphastar_sl_baseline/train.sh
@@ -4,5 +4,5 @@ ITER=00
 srun -p $1 -w $2 --gres=gpu:1 python3 -u -m sc2learner.bin.train_sl \
     --use_distributed False \
     --config_path $work_path/config.yaml \
-    --replay_list $work_path/zerg_normal.txt
+    --replay_list $work_path/zerg_normal.txt \
 #    --load_path $work_path/checkpoints/iterations_$ITER.pth.tar \
