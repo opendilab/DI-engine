fac1a375148f0850a7111b0abe9c7379f8d44690
niuyazhe <niuyazhe@sensetime.com>
2020-03-01 15:43:53 +0800

    fix script bool variable bug

--- sc2learner/experiments/alphastar_sl_baseline/train_multi.sh
+++ sc2learner/experiments/alphastar_sl_baseline/train_multi.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 work_path=$(dirname $0)
 ITER=00
-srun --mpi=pmi2 -p $1 -n32 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=3 python3 -u -m sc2learner.bin.train_sl \
-    --use_distributed true \
+srun --mpi=pmi2 -p $1 -n8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=3 python3 -u -m sc2learner.bin.train_sl \
+    --use_distributed \
     --config_path $work_path/config.yaml \
-    --only_evaluate false \
+    --noonly_evaluate \
     --replay_list $work_path/zerg_500.txt \
     --eval_replay_list $work_path/zerg_8.txt \
 #    --load_path $work_path/checkpoints/iterations_$ITER.pth.tar \
