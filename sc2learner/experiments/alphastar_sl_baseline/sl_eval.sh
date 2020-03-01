178293b4c9bf09f58acb4b83cd5e344419bc5d35
niuyazhe <niuyazhe@sensetime.com>
2020-03-01 11:31:15 +0800

    update config and script(sl eval, batch size, tra_len)

---
+++ sc2learner/experiments/alphastar_sl_baseline/sl_eval.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+work_path=$(dirname $0)
+ITER=1400
+srun --mpi=pmi2 -p $1 -n2 --gres=gpu:2 --ntasks-per-node=2 --cpus-per-task=3 python3 -u -m sc2learner.bin.train_sl \
+    --use_distributed true \
+    --config_path $work_path/config.yaml \
+    --only_evaluate true \
+    --replay_list $work_path/zerg_500.txt \
+    --eval_replay_list $work_path/zerg_1.txt \
+    --load_path $work_path/checkpoints/iterations_$ITER.pth.tar \
--- sc2learner/experiments/alphastar_sl_baseline/train_multi.sh
+++ sc2learner/experiments/alphastar_sl_baseline/train_multi.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 work_path=$(dirname $0)
 ITER=00
-srun --mpi=pmi2 -p $1 -n32 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 python3 -u -m sc2learner.bin.train_sl \
-    --use_distributed True \
+srun --mpi=pmi2 -p $1 -n32 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=3 python3 -u -m sc2learner.bin.train_sl \
+    --use_distributed true \
     --config_path $work_path/config.yaml \
-    --replay_list $work_path/zerg_normal.txt \
+    --only_evaluate false \
+    --replay_list $work_path/zerg_500.txt \
+    --eval_replay_list $work_path/zerg_8.txt \
 #    --load_path $work_path/checkpoints/iterations_$ITER.pth.tar \
