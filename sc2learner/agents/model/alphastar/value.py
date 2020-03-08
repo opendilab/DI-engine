e6bdad809538846d327c63f8db7c9c361a2623a1
niuyazhe <niuyazhe@sensetime.com>
2020-03-08 10:51:56 +0800

    add value network

---
+++ sc2learner/agents/model/alphastar/value.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from sc2learner.nn_utils import fc_block, build_activation, ResFCBlock
+
+
+class ValueBaseline(nn.Module):
+    def __init__(self, cfg):
+        super(ValueBaseline, self).__init__()
+        self.act = build_activation(cfg.activation)
+        self.project = fc_block(cfg.input_dim, cfg.res_dim, activation=self.act, norm_type=cfg.norm_type)
+        blocks = [ResFCBlock(cfg.res_dim, cfg.res_dim, self.act, cfg.norm_type) for _ in range(cfg.res_num)]
+        self.res = nn.Sequential(*blocks)
+        self.value_fc = fc_block(cfg.res_dim, 1, activation=None, norm_type=None)
+        self.PI = np.pi
+
+    def forward(self, x):
+        x = self.project(x)
+        x = self.res(x)
+        x = self.value_fc(x)
+
+        x = x.squeeze(1)
+        x = (2.0/self.PI) * torch.atan((self.PI/2.0) * x)
+        return x
+
+
+def test_value_baseline():
+    class CFG:
+        self.activation = 'relu'
+        self.norm_type = 'LN'
+        self.input_dim = 1024
+        self.res_dim = 256
+        self.res_num = 16
+
+    model = ValueBaseline(CFG())
+    inputs = torch.randn(4, 1024)
+    output = model(inputs)
+    assert(output.shape == (4,))
+
+
+if __name__ == "__main__":
+    test_value_baseline()
