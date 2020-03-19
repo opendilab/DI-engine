9771b6686b2d0df446cb9b6c313cbec06cb4b689
niuyazhe <niuyazhe@sensetime.com>
2020-03-19 12:30:44 +0800

    remove TC envs and bins

--- sc2learner/envs/actions/function.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from collections import namedtuple
-
-Function = namedtuple('Function', ['name', 'function', 'is_valid'])
