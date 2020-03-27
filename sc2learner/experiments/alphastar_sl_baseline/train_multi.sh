2fa1255c3bf466fb08e6687bcc22c8ffdd98d0db
niuyazhe <niuyazhe@sensetime.com>
2020-03-27 14:10:04 +0800

    update train/eval script file list path

--- sc2learner/experiments/alphastar_sl_baseline/train_multi.sh
+++ sc2learner/experiments/alphastar_sl_baseline/train_multi.sh
@@ -5,6 +5,6 @@ srun --mpi=pmi2 -p $1 -n8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=3 pyt
     --use_distributed \
     --config_path $work_path/config.yaml \
     --noonly_evaluate \
-    --replay_list /mnt/lustre/share_data/niuyazhe/zerg_train_14355.txt \
-    --eval_replay_list /mnt/lustre/share_data/niuyazhe/zerg_val_64.txt \
+    --replay_list /mnt/lustre/share_data/niuyazhe/Zerg_None_None_3500_train_5200.txt \
+    --eval_replay_list /mnt/lustre/share_data/niuyazhe/Zerg_None_None_3500_val_274.txt \
 #    --load_path $work_path/checkpoints/iterations_$ITER.pth.tar \
