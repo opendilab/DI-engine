8004d14b3c92915d29a1017de3eefbc361a67fc5
niuyazhe <niuyazhe@sensetime.com>
2020-03-29 00:40:29 +0800

    add lock context

--- sc2learner/data/structure/cache.py
+++ sc2learner/data/structure/cache.py
@@ -1,6 +1,7 @@
 import time
 from queue import Queue
-from threading import Thread, Lock
+from threading import Thread
+from sc2learner.utils import LockContext
 
 
 class Cache(object):
@@ -12,18 +13,17 @@ class Cache(object):
         self.debug = debug
         self.receive_queue = Queue(maxlen)
         self.send_queue = Queue(maxlen)
-        self.receive_lock = Lock()
+        self.receive_lock = LockContext(lock_type='thread')
         self._timeout_thread = Thread(target=self._timeout_monitor)
         self._timeout_thread_flag = True
 
     def push_data(self, data):
-        self.receive_lock.acquire()
-        self.receive_queue.put([data, time.time()])
-        if self.receive_queue.full():
-            self.dprint('send total receive_queue, current len:{}'.format(self.receive_queue.qsize()))
-            while not self.receive_queue.empty():
-                self.send_queue.put(self.receive_queue.get()[0])
-        self.receive_lock.release()
+        with self.receive_lock:
+            self.receive_queue.put([data, time.time()])
+            if self.receive_queue.full():
+                self.dprint('send total receive_queue, current len:{}'.format(self.receive_queue.qsize()))
+                while not self.receive_queue.empty():
+                    self.send_queue.put(self.receive_queue.get()[0])
 
     def get_cached_data_iter(self):
         return iter(self.send_queue.get, 'STOP')
@@ -31,19 +31,18 @@ class Cache(object):
     def _timeout_monitor(self):
         while self._timeout_thread_flag:
             time.sleep(self.monitor_interval)
-            self.receive_lock.acquire()
-            while not self.receive_queue.empty():
-                wait_time = time.time() - self.receive_queue.queue[0][1]
-                if wait_time >= self.timeout:
-                    self.dprint(
-                        'excess the maximum wait time, eject from the cache.(wait_time/timeout: {}/{}'.format(
-                            wait_time, self.timeout
+            with self.receive_lock:
+                while not self.receive_queue.empty():
+                    wait_time = time.time() - self.receive_queue.queue[0][1]
+                    if wait_time >= self.timeout:
+                        self.dprint(
+                            'excess the maximum wait time, eject from the cache.(wait_time/timeout: {}/{}'.format(
+                                wait_time, self.timeout
+                            )
                         )
-                    )
-                    self.send_queue.put(self.receive_queue.get()[0])
-                else:
-                    break
-            self.receive_lock.release()
+                        self.send_queue.put(self.receive_queue.get()[0])
+                    else:
+                        break
 
     def run(self):
         self._timeout_thread.start()
