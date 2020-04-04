f8fcff3049b77371de1dcae05b47b7f906a2477a
niuyazhe <niuyazhe@sensetime.com>
2020-04-05 01:05:31 +0800

    add fake actor dataset and seperate collate_fn

--- sc2learner/data/dataloader.py
+++ sc2learner/data/dataloader.py
@@ -1,6 +1,7 @@
 from torch.utils.data import DataLoader
 
-from .offline import ReplayIterationDataLoader, policy_collate_fn
+from .offline import ReplayIterationDataLoader
+from .collate_fn import policy_collate_fn, actor_collate_fn
 from .online import OnlineIteratorDataLoader
 from .sampler import DistributedSampler
 
@@ -24,9 +25,11 @@ def build_dataloader(dataset, dataloader_type, batch_size, use_distributed, read
     elif dataloader_type == 'iter':
         # FIXME ReplayIterationDataLoader is not tested locally at all!
         # assert use_distributed
-        dataloader = ReplayIterationDataLoader(dataset, batch_size)
-    elif dataloader_type == 'online':
+        dataloader = ReplayIterationDataLoader(dataset, batch_size, collate_fn=policy_collate_fn)
+    elif dataloader_type == 'iterator':
         dataloader = OnlineIteratorDataLoader(dataset, batch_size=batch_size, read_data_fn=read_data_fn)
+    elif dataloader_type == 'fake_actor':
+        dataloader = DataLoader(dataset, batch_size=batch_size, collate_fn=actor_collate_fn, num_workers=0)
     else:
         raise NotImplementedError()
     return dataloader
--- sc2learner/data/dataset.py
+++ sc2learner/data/dataset.py
@@ -1,8 +1,10 @@
 from .offline import ReplayDataset
-from .tests.fake_dataset import FakeReplayDataset
+from .fake_dataset import FakeReplayDataset, FakeActorDataset
 
 
 def build_dataset(dataset_config, train_dataset):
     if dataset_config.dataset_type == "fake":
         return FakeReplayDataset(dataset_config)
+    elif dataset_config.dataset_type == "fake_actor":
+        return FakeActorDataset()
     return ReplayDataset(dataset_config, train_mode=train_dataset)
