b310a6c016e29b3c9f696c4f8b1fde00ffe29ab8
niuyazhe <niuyazhe@sensetime.com>
2020-07-19 22:13:49 +0800

    update actor.sh with gpu resource and indicated node ip

--- sc2learner/experiments/alphastar_rl_baseline/actor.sh
+++ sc2learner/experiments/alphastar_rl_baseline/actor.sh
@@ -5,7 +5,7 @@ work_path=$(dirname $0)
 cfg=config.yaml
 
 for i in $(seq 1 $2); do
-    srun --mpi=pmi2 -p $1 --job-name=actor \
+    srun --mpi=pmi2 -p $1 -w $3 --gres=gpu:1 --job-name=actor \
         python3 -u -m sc2learner.train.train_rl \
             --config_path=${cfg} \
             --job_name=actor &\
