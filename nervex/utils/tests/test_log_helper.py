e6f2f591f6fabc2617d5e7f19bbb57471fc2d03b
niuyazhe <niuyazhe@sensetime.com>
2020-09-01 19:05:34 +0800

    add AverageMeter and its test

---
+++ nervex/utils/tests/test_log_helper.py
@@ -0,0 +1,31 @@
+import pytest
+import random
+import numpy as np
+from collections import deque
+from nervex.utils import AverageMeter
+
+
+@pytest.mark.unittest
+class TestAverageMeter:
+    def test_naive(self):
+        handle = AverageMeter(length=1)
+        handle.reset()
+        assert handle.val == 0.0
+        assert handle.avg == 0.0
+        for _ in range(10):
+            t = random.uniform(0, 1)
+            handle.update(t)
+            assert handle.val == t
+            assert handle.avg == pytest.approx(t, abs=1e-6)
+
+        handle = AverageMeter(length=5)
+        handle.reset()
+        assert handle.val == 0.0
+        assert handle.avg == 0.0
+        queue = deque(maxlen=5)
+        for _ in range(10):
+            t = random.uniform(0, 1)
+            handle.update(t)
+            queue.append(t)
+            assert handle.val == t
+            assert handle.avg == pytest.approx(np.mean(queue, axis=0))
