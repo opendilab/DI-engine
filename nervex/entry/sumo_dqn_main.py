b706c8d05f88f3e0d43709d9d4fd62e0a5f81aa0
niuyazhe <niuyazhe@sensetime.com>
2020-09-11 16:36:39 +0800

    add sumo dqn sync single machine entry and fix sumo env max_steps

--- nervex/entry/sumo_dqn_main.py
+++ nervex/entry/sumo_dqn_main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import argparse
 from collections import namedtuple
 import torch
@@ -8,17 +9,20 @@ import os
 import math
 import random
 import threading
+from multiprocessing import Process
+import multiprocessing
 
 from typing import Optional
 
 from nervex.worker.actor.env_manager.sumowj3_env_manager import SumoWJ3EnvManager
+from nervex.envs.sumo.sumo_env import SumoWJ3Env
 from nervex.worker.learner.sumo_dqn_learner import SumoDqnLearner
 from nervex.data.structure.buffer import PrioritizedBufferWrapper
-from nervex.worker.agent.sumo_dqn_agent import SumoDqnActorAgent
 from nervex.data.collate_fn import sumo_dqn_collate_fn
+from nervex.worker.agent.sumo_dqn_agent import SumoDqnActorAgent
 from nervex.torch_utils import to_device
-from nervex.utils import read_config
 from nervex.rl_utils import epsilon_greedy
+from nervex.utils import read_config
 
 
 def setup_config(path=None):
@@ -33,57 +37,58 @@ class SumoDqnRun():
         self.use_cuda = self.cfg.learner.use_cuda
         self.batch_size = self.cfg.learner.batch_size
         self.env = SumoWJ3EnvManager(cfg.env)
-        self.total_frame_num = cfg.learner.dqn.total_frame_num
-        self.max_epoch_frame = cfg.learner.dqn.max_epoch_frame
-        self.buffer = PrioritizedBufferWrapper(cfg.learner.dqn.buffer_length)
+        self.buffer = PrioritizedBufferWrapper(cfg.learner.data.buffer_length, max_reuse=cfg.learner.data.max_reuse)
         self.bandit = epsilon_greedy(0.95, 0.03, 10000)
         self.learner = SumoDqnLearner(self.cfg)
         self.actor_agent = SumoDqnActorAgent(self.learner.computation_graph.agent.model)
         self.actor_agent.load_state_dict(self.learner.computation_graph.agent.state_dict())
         self._setup_data_source()
+        self.total_step_count = 0
 
     def _setup_data_source(self):
         def data_iterator():
             while True:
                 yield sumo_dqn_collate_fn(next(self.buffer.iterable_sample(self.batch_size)))
+
         self.learner._data_source = data_iterator()
 
     def actor(self):
-        total_frame_count = 0
+        obs = self.env.reset()
+        dones = [False for _ in range(self.env.env_num)]
         while True:
-            obs = self.env.reset()
-            dones = [False for _ in range(self.env.env_num)]
-            while True:
-                eps_threshold = self.bandit(self.learner.last_iter.val)
-                if self.use_cuda:
-                    obs = to_device(obs, 'cuda')
-                actions, _ = self.actor_agent.forward(obs, eps=eps_threshold)
-                if self.use_cuda:
-                    actions = to_device(actions, 'cpu')
-                timestep = self.env.step(actions)
-                for i, d in enumerate(dones):
-                    if not d:
-                        step = {
-                            'obs': obs[i],
-                            'action': actions[i],
-                            'next_obs': timestep.obs[i],
-                            'reward': timestep.reward[i],
-                            'done': timestep.done[i],
-                        }
-                        self.buffer.append(step)
-                        obs[i] = timestep.obs[i]
-                dones = timestep.done
-                total_frame_count += 1
+            eps_threshold = self.bandit(self.total_step_count)
+            if self.use_cuda:
+                obs = to_device(obs, 'cuda')
+            actions, _ = self.actor_agent.forward(obs, eps=eps_threshold)
+            if self.use_cuda:
+                actions = to_device(actions, 'cpu')
+            timestep = self.env.step(actions)
+            for i, d in enumerate(dones):
+                if not d:
+                    step = {
+                        'obs': obs[i],
+                        'action': actions[i],
+                        'next_obs': timestep.obs[i],
+                        'reward': timestep.reward[i],
+                        'done': timestep.done[i],
+                    }
+                    self.buffer.append(step)
+                    obs[i] = timestep.obs[i]
+            dones = timestep.done
+            self.total_step_count += 1
 
-                if all(dones):
-                    break
+            if all(dones):
+                break
+            if self.total_step_count % 50 == 0:
+                print(
+                    'actor run step {} with replay buffer size {}'.format(self.total_step_count, self.buffer.validlen)
+                )
 
     def run(self):
-        threads = []
-        threads.append(threading.Thread(target=self.learner.run))
-        threads.append(threading.Thread(target=self.actor))
-        for t in threads:
-            t.start()
+        while True:
+            self.actor()
+            self.learner.run()
+            self.actor_agent.load_state_dict(self.learner.computation_graph.agent.state_dict())
 
 
 if __name__ == "__main__":
