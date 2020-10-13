78e16784b39858dff1f0628d58575cead7681515
HansBug <killog@126.com>
2020-10-13 23:05:52 +0800

    decrease warning in test_replay_buffer.py

--- nervex/data/tests/test_replay_buffer.py
+++ nervex/data/tests/test_replay_buffer.py
@@ -40,20 +40,20 @@ def generate_data():
 class TestReplayBuffer:
     produce_count = 0
 
-    def produce(self, id, replay_buffer):
+    def produce(self, id_, replay_buffer):
         time.sleep(1)
         begin_time = time.time()
         count = 0
         while time.time() - begin_time < 20:
             t = np.random.randint(1, 6)
             time.sleep(t)
-            print('[PRODUCER] thread {} use {} second to produce a data'.format(id, t))
+            print('[PRODUCER] thread {} use {} second to produce a data'.format(id_, t))
             replay_buffer.push_data(generate_data())
             count += 1
-        print('[PRODUCER] thread {} finish job, total produce {} data'.format(id, count))
+        print('[PRODUCER] thread {} finish job, total produce {} data'.format(id_, count))
         self.produce_count += count
 
-    def consume(self, id, replay_buffer):
+    def consume(self, id_, replay_buffer):
         time.sleep(1)
         begin_time = time.time()
         iteration = 0
@@ -67,7 +67,7 @@ class TestReplayBuffer:
                     time.sleep(2)
             time.sleep(0.5)
             iteration += 1
-            print('[CONSUMER] thread {} iteration {} training finish'.format(id, iteration))
+            print('[CONSUMER] thread {} iteration {} training finish'.format(id_, iteration))
             # update
             info = {'priority': [], 'replay_unique_id': [], 'replay_buffer_idx': []}
             for idx, d in enumerate(data):
@@ -75,21 +75,21 @@ class TestReplayBuffer:
                 info['replay_unique_id'].append(d['replay_unique_id'])
                 info['replay_buffer_idx'].append(d['replay_buffer_idx'])
             replay_buffer.update(info)
-            print('[CONSUMER] thread {} iteration {} update finish'.format(id, iteration))
+            print('[CONSUMER] thread {} iteration {} update finish'.format(id_, iteration))
 
     def test(self, setup_replay_buffer):
         setup_replay_buffer._cache.debug = True
-        p_threadings = [Thread(target=self.produce, args=(i, setup_replay_buffer)) for i in range(PRODUCER_NUM)]
-        c_threadings = [Thread(target=self.consume, args=(i, setup_replay_buffer)) for i in range(CONSUMER_NUM)]
-        for t in p_threadings:
+        produce_threads = [Thread(target=self.produce, args=(i, setup_replay_buffer)) for i in range(PRODUCER_NUM)]
+        consume_threads = [Thread(target=self.consume, args=(i, setup_replay_buffer)) for i in range(CONSUMER_NUM)]
+        for t in produce_threads:
             t.start()
         setup_replay_buffer.run()
-        for t in c_threadings:
+        for t in consume_threads:
             t.start()
 
-        for t in p_threadings:
+        for t in produce_threads:
             t.join()
-        for t in c_threadings:
+        for t in consume_threads:
             t.join()
         setup_replay_buffer.close()
         time.sleep(1 + 0.5)
