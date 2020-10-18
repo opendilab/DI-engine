85bc2b566b10a3ba74c3a25e1804d7ed27e1563a
HansBug <killog@126.com>
2020-10-19 01:10:21 +0800

    update import_utils.py

--- nervex/utils/import_utils.py
+++ nervex/utils/import_utils.py
@@ -1,34 +1,52 @@
 import importlib
+import logging
+from typing import List, NoReturn
 
 
 def try_import_ceph():
+    """
+    Try import ceph module, if failed, return None
+
+    Returns:
+        module: imported module, or None when ceph not found
+    """
     try:
         import ceph
     except ModuleNotFoundError as e:
-        import logging
+        ceph = None
         logging.warning(
             "You have not installed ceph package! If you are not run locally and testing, "
             "ask coworker for help."
         )
-        ceph = None
+
     return ceph
 
 
 def try_import_link():
+    """
+    Try import linklink module, if failed, import nervex.tests.fake_linklink instead
+
+    Returns:
+        module: imported module (may be fake_linklink)
+    """
     try:
         import linklink as link
     except ModuleNotFoundError as e:
         from nervex.tests.fake_linklink import link
-        import logging
-
         logging.warning(
             "You have not installed linklink package! If you are not run locally and testing, "
             "ask coworker for help. We will run a fake linklink for you. "
             "Refer to nervex.tests.fake_linklink.py for details."
         )
+
     return link
 
 
-def import_module(names):
-    for n in names:
-        importlib.import_module(n)
+def import_module(modules: List[str]) -> NoReturn:
+    """
+    Import several module as a list
+    Args:
+        modules (list): List of module names
+    """
+    for name in modules:
+        importlib.import_module(name)
