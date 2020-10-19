afccd0feb15aa04ff6f63fd81a964d6a1efc5711
niuyazhe <niuyazhe@sensetime.com>
2020-10-19 19:55:26 +0800

    polish import ceph and linklink warning

--- nervex/utils/import_utils.py
+++ nervex/utils/import_utils.py
@@ -1,31 +1,39 @@
 import importlib
 
+global ceph_flag, linklink_flag
+ceph_flag, linklink_flag = True, True
+
 
 def try_import_ceph():
+    global ceph_flag
     try:
         import ceph
     except ModuleNotFoundError as e:
-        import logging
-        logging.warning(
-            "You have not installed ceph package! If you are not run locally and testing, "
-            "ask coworker for help."
-        )
+        if ceph_flag:
+            import logging
+            logging.warning(
+                "You have not installed ceph package! If you are not run locally and testing, "
+                "ask coworker for help."
+            )
         ceph = None
+        ceph_flag = False
     return ceph
 
 
 def try_import_link():
+    global linklink_flag
     try:
         import linklink as link
     except ModuleNotFoundError as e:
+        if linklink_flag:
+            import logging
+            logging.warning(
+                "You have not installed linklink package! If you are not run locally and testing, "
+                "ask coworker for help. We will run a fake linklink."
+                "Refer to nervex.tests.fake_linklink.py for details."
+            )
         from nervex.tests.fake_linklink import link
-        import logging
-
-        logging.warning(
-            "You have not installed linklink package! If you are not run locally and testing, "
-            "ask coworker for help. We will run a fake linklink for you. "
-            "Refer to nervex.tests.fake_linklink.py for details."
-        )
+        linklink_flag = False
     return link
 
 
