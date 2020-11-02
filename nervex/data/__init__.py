2b3fc1f6912d10df5242ace1d0fc760dbf1751bd
niuyazhe <niuyazhe@sensetime.com>
2020-11-03 00:40:44 +0800

    add default default_decollate

--- nervex/data/__init__.py
+++ nervex/data/__init__.py
@@ -1,4 +1,4 @@
 from .dataloader import build_dataloader
-from .collate_fn import diff_shape_collate, default_collate
+from .collate_fn import diff_shape_collate, default_collate, default_decollate
 from .online import ReplayBuffer
 from .structure import PrioritizedBuffer
