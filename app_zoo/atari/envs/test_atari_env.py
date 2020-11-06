change cum_reward calculation api and fix atari env bug

app_zoo/atari/envs/test_atari_env.py
@@ -15,7 +15,7 @@ class TestAtariEnv:
         pong_env.seed(0)
         obs = pong_env.reset()
         assert obs.shape == (cfg.frame_stack, 84, 84)
-        act_dim = pong_env.info().act_space['shape']
+        act_dim = pong_env.info().act_space.shape
         for i in range(10):
             random_action = torch.LongTensor([np.random.choice(range(act_dim))])
             timestep = pong_env.step(random_action)

app_zoo/sumo/envs/fake_sumo_env.py
@@ -41,7 +41,7 @@ class FakeSumoWJ3Env(SumoWJ3Env):
         obs = torch.randn(380)
         reward = {k: -torch.rand(1) for k in ['queue_len', 'wait_time', 'delay_time']}
         done = self.count >= 200
-        info = {'cum_reward': {k: -torch.rand(1) for k in ['queue_len', 'wait_time', 'delay_time']}}
+        info = {'eval_reward': -torch.rand(1).item()}
         self.count += 1
         return FakeSumoWJ3Env.timestep(obs, reward, done, info)
 