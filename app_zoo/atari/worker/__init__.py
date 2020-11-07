80763ae9335f2d9fb0ee2b0427e58cdf023a81b4
niuyazhe <niuyazhe@sensetime.com>
2020-11-08 01:20:11 +0800

    add atari ppo graph, agent and learner

---
+++ app_zoo/atari/worker/__init__.py
@@ -0,0 +1,2 @@
+from .atari_dqn_learner import AtariDqnLearner
+from .atari_ppo_learner import AtariPpoLearner
--- app_zoo/atari/worker/atari_agent.py
+++ app_zoo/atari/worker/atari_agent.py
@@ -1,4 +1,5 @@
 import torch
+from typing import Any, Optional
 from collections import OrderedDict
 from nervex.worker.agent import BaseAgent
 
@@ -19,7 +20,7 @@ class AtariDqnLearnerAgent(BaseAgent):
         super(AtariDqnLearnerAgent, self).__init__(model, self.plugin_cfg)
 
 
-class AtariActorAgent(BaseAgent):
+class AtariDqnActorAgent(BaseAgent):
 
     def __init__(self, model: torch.nn.Module) -> None:
         plugin_cfg = OrderedDict({
@@ -28,10 +29,10 @@ class AtariActorAgent(BaseAgent):
                 'enable_grad': False
             },
         })
-        super(AtariActorAgent, self).__init__(model, plugin_cfg)
+        super(AtariDqnActorAgent, self).__init__(model, plugin_cfg)
 
 
-class AtariEvaluateAgent(BaseAgent):
+class AtariDqnEvaluatorAgent(BaseAgent):
 
     def __init__(self, model: torch.nn.Module) -> None:
         plugin_cfg = OrderedDict({
@@ -40,4 +41,57 @@ class AtariEvaluateAgent(BaseAgent):
                 'enable_grad': False
             },
         })
-        super(AtariEvaluateAgent, self).__init__(model, plugin_cfg)
+        super(AtariDqnEvaluatorAgent, self).__init__(model, plugin_cfg)
+
+
+class AtariPpoLearnerAgent(BaseAgent):
+
+    def __init__(self, model: torch.nn.Module) -> None:
+        plugin_cfg = OrderedDict({
+            'grad': {
+                'enable_grad': True
+            },
+        })
+        super(AtariPpoLearnerAgent, self).__init__(model, plugin_cfg)
+
+    def forward(self, data: Any, param: Optional[dict] = None) -> dict:
+        if param is None:
+            param = {}
+        param['mode'] = 'compute_action_value'
+        return super().forward(data, param)
+
+
+class AtariPpoActorAgent(BaseAgent):
+
+    def __init__(self, model: torch.nn.Module) -> None:
+        plugin_cfg = OrderedDict({
+            'eps_greedy_sample': {},
+            'grad': {
+                'enable_grad': False
+            },
+        })
+        super(AtariPpoActorAgent, self).__init__(model, plugin_cfg)
+
+    def forward(self, data: Any, param: Optional[dict] = None) -> dict:
+        if param is None:
+            param = {}
+        param['mode'] = 'compute_action_value'
+        return super().forward(data, param)
+
+
+class AtariPpoEvaluatorAgent(BaseAgent):
+
+    def __init__(self, model: torch.nn.Module) -> None:
+        plugin_cfg = OrderedDict({
+            'argmax_sample': {},
+            'grad': {
+                'enable_grad': False
+            },
+        })
+        super(AtariPpoEvaluatorAgent, self).__init__(model, plugin_cfg)
+
+    def forward(self, data: Any, param: Optional[dict] = None) -> dict:
+        if param is None:
+            param = {}
+        param['mode'] = 'compute_action'
+        return super().forward(data, param)
