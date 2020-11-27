81303fd31fac4c432f5ec92d06405f822e342a3d
MosQuiTo <17373157@buaa.edu.cn>
2020-11-27 19:06:08 +0800

    init docs, no test of common function & model

--- nervex/model/common_arch/dueling.py
+++ nervex/model/common_arch/dueling.py
@@ -11,11 +11,11 @@ class DuelingHead(nn.Module):
     Overview:
         The Dueling head used in models.
     Notes:
-        Dueling head is one of the three most majoy improvements on DQN algorithm. The paper introduce \
-            this improvement Dueling Network Architectures for Deep Reinforcement Learning was published \
-                by Google in 2016. You can view the original paper <https://arxiv.org/pdf/1511.06581.pdf>
-        The other two majoy inprovements are double DQN and prioritized replay, which in our nerveX \
-            are implement though plugins and buffer.
+        Dueling head is one of the three most major improvements in DQN algorithm. The paper introducing \
+            this improvement `Dueling Network Architectures for Deep Reinforcement Learning` was published \
+            by Google in 2016. You can view the original paper on <https://arxiv.org/pdf/1511.06581.pdf>
+        The other two major improvements are double DQN and prioritized replay, which in nerveX \
+            are implemented though plugins and buffer.
     Interfaces:
         __init__, forward
     """
@@ -31,15 +31,15 @@ class DuelingHead(nn.Module):
     ) -> None:
         r"""
         Overview:
-            init the DuelingHead according to arguments.
+            Init the DuelingHead according to arguments.
         Arguments:
-            - hidden_dim (:obj:`int`): the hidden_dim used before connected to DuelingHeads
+            - hidden_dim (:obj:`int`): the hidden_dim used before connected to DuelingHead
             - action_dim (:obj:`int`): the num of actions
             - a_layer_num (:obj:`int`): the num of fc_block used in the network to compute action output
             - v_layer_num (:obj:`int`): the num of fc_block used in the network to compute value output
             - activation (:obj:`nn.Module`): the type of activation to use in the fc_block,\
                 if None then default set to nn.ReLU
-            - norm_type (:obj:`str`): the type of normaliztion to use, see nervex.torch_utils.fc_block for more details
+            - norm_type (:obj:`str`): the type of normalization to use, see nervex.torch_utils.fc_block for more details
         """
         super(DuelingHead, self).__init__()
         self.A = [
@@ -58,13 +58,12 @@ class DuelingHead(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         r"""
         Overview:
-            return the advantage and the value according to the input from hidden layers
+            Return the sum of advantage and the value according to the input from hidden layers
         Arguments:
             - x (:obj:`torch.Tensor`): the input from hidden layers
         Returns:
-            - return (:obj:`torch.Tensor`): the corresponding advantage and value
+            - return (:obj:`torch.Tensor`): the sum of advantage and value
         """
         a = self.A(x)
         v = self.V(x)
-
         return a - a.mean(dim=-1, keepdim=True) + v
--- nervex/model/dqn/dqn_network.py
+++ nervex/model/dqn/dqn_network.py
@@ -10,6 +10,12 @@ from nervex.utils import squeeze
 
 
 class DQNBase(nn.Module):
+    r"""
+    Overview:
+        Base class for DQN based models.
+    Interface:
+        __init__, forward, fast_timestep_forward
+    """
 
     def __init__(
             self,
@@ -18,6 +24,15 @@ class DQNBase(nn.Module):
             embedding_dim: int = 64,
             **kwargs,
     ) -> None:
+        r"""
+        Overview:
+            Init the DQNBase according to arguments, including encoder, lstm(if needed) and head.
+        Arguments:
+            - obs_dim (:obj:`Union[int, tuple]`): a tuple of observation dim
+            - action_dim (:obj:`int`): the num of action dim, \
+                note that it can be a tuple containing more than one element
+            - embedding_dim (:obj:`int`): encoder's embedding dim (output dim)
+        """
         super(DQNBase, self).__init__()
         encoder_kwargs, lstm_kwargs, head_kwargs = get_kwargs(kwargs)
         self._encoder = Encoder(obs_dim, embedding_dim, **encoder_kwargs)
@@ -28,13 +43,24 @@ class DQNBase(nn.Module):
         self._head = Head(action_dim, embedding_dim, **head_kwargs)
 
     def forward(self, inputs: Dict) -> Dict:
+        r"""
+        Overview:
+            Normal forward. Would use lstm between encoder and head if needed
+        Arguments:
+            - inputs (:obj:`Dict`): a dict containing all raw input tensors, e.g. 'obs', 'prev_state' (if use lstm), \
+                'enable_fast_timestep' (if use fast timestep forward)
+        Returns:
+            - return (:obj:`Dict`): a dict containing output tensors of this DQN model
+        """
         if isinstance(inputs, torch.Tensor):
             inputs = {'obs': inputs}
+        # fast_timestep_forward if enabled
         if inputs.get('enable_fast_timestep', False):
             return self.fast_timestep_forward(inputs)
+        # normal forward
         x = self._encoder(inputs['obs'])
         if hasattr(self, '_lstm'):
-            x = x.unsqueeze(0)
+            x = x.unsqueeze(0)  # unsqueeze the first dim (only 1 timestep) to fit lstm's input shape
             x, next_state = self._lstm(x, inputs['prev_state'])
             x = x.squeeze(0)
             x = self._head(x)
@@ -44,12 +70,22 @@ class DQNBase(nn.Module):
             return {'logit': x}
 
     def fast_timestep_forward(self, inputs: Dict) -> Dict:
+        r"""
+        Overview:
+            A simple implementation of timestep forward.
+            Multiply timstep T and batch size B and get a larger batch size T*B。
+        Arguments:
+            - inputs (:obj:`Dict`): a dict containing all raw input tensors, e.g. 'obs', 'prev_state' (if use lstm), \
+                'enable_fast_timestep' (if use fast timestep forward)
+        Returns:
+            - return (:obj:`Dict`): a dict containing output tensors of this DQN model
+        """
         assert hasattr(self, '_lstm')
         x, prev_state = inputs['obs'], inputs['prev_state']
         assert len(x.shape) in [3, 5], x.shape
         x = parallel_wrapper(self._encoder)(x)
         lstm_embedding = []
-        for t in range(x.shape[0]):
+        for t in range(x.shape[0]):  # T timesteps
             output, prev_state = self._lstm(x[t:t + 1], prev_state)
             lstm_embedding.append(output)
         x = torch.cat(lstm_embedding, 0)
@@ -58,6 +94,12 @@ class DQNBase(nn.Module):
 
 
 class Encoder(nn.Module):
+    r"""
+    Overview:
+        The Encoder used in DQN models. Encode env state into a tensor for further operation.
+    Interfaces:
+        __init__, forward
+    """
 
     def __init__(
             self,
@@ -65,6 +107,14 @@ class Encoder(nn.Module):
             embedding_dim: int,
             encoder_type: str,
     ) -> None:
+        r"""
+        Overview:
+            Init the Encoder according to arguments.
+        Arguments:
+            - obs_dim (:obj:`Union[int, tuple]`): a tuple of observation dim
+            - embedding_dim (:obj:`int`): encoder's embedding dim (output dim)
+            - encoder_type (:obj:`str`): type of encoder, now supports ['fc', 'conv2d']
+        """
         super(Encoder, self).__init__()
         self.act = nn.ReLU()
         assert encoder_type in ['fc', 'conv2d'], encoder_type
@@ -81,10 +131,24 @@ class Encoder(nn.Module):
             self.main = ConvEncoder(obs_dim, embedding_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        r"""
+        Overview:
+            Transform from raw observation into encoded tensor.
+        Arguments:
+            - x (:obj:`Dict`): raw observation
+        Returns:
+            - return (:obj:`Dict`): encoded embedding tensor
+        """
         return self.main(x)
 
 
 class Head(nn.Module):
+    r"""
+    Overview:
+        The Head used in DQN models. Receive encoded embedding tensor and use it to predict the action.
+    Interfaces:
+        __init__, forward
+    """
 
     def __init__(
             self,
@@ -94,10 +158,20 @@ class Head(nn.Module):
             a_layer_num: int = 1,
             v_layer_num: int = 1
     ) -> None:
+        r"""
+        Overview:
+            Init the Head according to arguments.
+        Arguments:
+            - action_dim (:obj:`tuple`): the num of action dim, \
+                note that it can be a tuple containing more than one element
+            - input_dim (:obj:`int`): input tensor dim of the head
+            - dueling (:obj:`bool`): whether to use ``DuelingHead`` or ``nn.Linear``
+            - a_layer_num (:obj:`int`): the num of layers in ``DuelingHead`` to compute action output
+            - v_layer_num (:obj:`int`): the num of layers in ``DuelingHead`` to compute value output
+        """
         super(Head, self).__init__()
         self.action_dim = squeeze(tuple(action_dim))
         self.dueling = dueling
-
         head_fn = partial(DuelingHead, a_layer_num=a_layer_num, v_layer_num=v_layer_num) if dueling else nn.Linear
         if isinstance(self.action_dim, tuple):
             self.pred = nn.ModuleList()
@@ -107,6 +181,14 @@ class Head(nn.Module):
             self.pred = head_fn(input_dim, self.action_dim)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        r"""
+        Overview:
+            Use encoded tensor to predict the action.
+        Arguments:
+            - x (:obj:`Dict`): encoded tensor
+        Returns:
+            - return (:obj:`Dict`): action in logits
+        """
         if isinstance(self.action_dim, tuple):
             x = [m(x) for m in self.pred]
         else:
@@ -115,7 +197,10 @@ class Head(nn.Module):
 
 
 FCDQN = partial(
-    DQNBase, encoder_kwargs={'encoder_type': 'fc'}, lstm_kwargs={'lstm_type': 'none'}, head_kwargs={'dueling': True}
+    DQNBase,
+    encoder_kwargs={'encoder_type': 'fc'},
+    lstm_kwargs={'lstm_type': 'none'},
+    head_kwargs={'dueling': True}
 )
 ConvDQN = partial(
     DQNBase,
@@ -124,7 +209,10 @@ ConvDQN = partial(
     head_kwargs={'dueling': True}
 )
 FCDRQN = partial(
-    DQNBase, encoder_kwargs={'encoder_type': 'fc'}, lstm_kwargs={'lstm_type': 'normal'}, head_kwargs={'dueling': True}
+    DQNBase,
+    encoder_kwargs={'encoder_type': 'fc'},
+    lstm_kwargs={'lstm_type': 'normal'},
+    head_kwargs={'dueling': True}
 )
 ConvDRQN = partial(
     DQNBase,
@@ -135,6 +223,15 @@ ConvDRQN = partial(
 
 
 def parallel_wrapper(forward_fn: Callable) -> Callable:
+    r"""
+    Overview:
+        Process timestep T and batch_size B at the same time, in other words, treat different timestep data as
+        different trajectories in a batch. Used in ``DQNBase``'s ``fast_timestep_forward``.
+    Arguments:
+        - forward_fn (:obj:`Callable`): normal nn.Module's forward function
+    Returns:
+        - wrapper (:obj:`Callable`): wrapped function
+    """
 
     def wrapper(x: torch.Tensor) -> Union[torch.Tensor, List[torch.Tensor]]:
         T, B = x.shape[:2]
@@ -149,7 +246,15 @@ def parallel_wrapper(forward_fn: Callable) -> Callable:
     return wrapper
 
 
-def get_kwargs(kwargs: dict) -> Tuple[dict]:
+def get_kwargs(kwargs: Dict) -> Tuple[Dict]:
+    r"""
+    Overview:
+        Get kwargs of encoder, lstm and head, according to input model kwargs.
+    Arguments:
+        - kwargs (:obj:`Dict`): model kwargs dict, might have keys ['encoder_kwargs', 'lstm_kwargs', 'head_kwargs']
+    Returns:
+        - ret (:obj:`Tuple[Dict]`): (encoder kwargs, lstm kwargs, head kwargs)
+    """
     if 'encoder_kwargs' in kwargs:
         encoder_kwargs = kwargs['encoder_kwargs']
     else:
