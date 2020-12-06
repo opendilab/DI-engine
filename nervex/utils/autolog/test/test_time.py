08b200ea5de56f6ef29a5a4bcfb6df148fc1ab02
HansBug <killog@126.com>
2020-12-06 22:35:07 +0800

    add test

--- nervex/utils/autolog/test/test_time.py
+++ nervex/utils/autolog/test/test_time.py
@@ -2,7 +2,7 @@ import time
 
 import pytest
 
-from nervex.utils.autolog import TickTime, NaturalTime
+from nervex.utils.autolog import TickTime, NaturalTime, TimeProxy
 
 
 @pytest.mark.unittest
@@ -42,3 +42,63 @@ class TestTickTime:
 
         with pytest.raises(ValueError):
             _time.step(0)
+
+
+@pytest.mark.unittest
+class TestTimeProxy:
+    def test_time_proxy_for_tick_time(self):
+        _time = TickTime()
+        _proxy = TimeProxy(_time)
+
+        assert _proxy.time() == 0
+        assert _proxy.current_time() == 0
+        assert not _proxy.is_frozen
+
+        _time.step()
+        assert _proxy.time() == 1
+        assert _proxy.current_time() == 1
+        assert not _proxy.is_frozen
+
+        _proxy.freeze()
+        _time.step(2)
+        assert _proxy.time() == 1
+        assert _proxy.current_time() == 3
+        assert _proxy.is_frozen
+
+        _time.step()
+        assert _proxy.time() == 1
+        assert _proxy.current_time() == 4
+        assert _proxy.is_frozen
+
+        _proxy.unfreeze()
+        assert _proxy.time() == 4
+        assert _proxy.current_time() == 4
+        assert not _proxy.is_frozen
+
+    def test_time_proxy_frozen_for_tick_time(self):
+        _time = TickTime()
+        _proxy = TimeProxy(_time, frozen=True)
+
+        assert _proxy.time() == 0
+        assert _proxy.current_time() == 0
+        assert _proxy.is_frozen
+
+        _time.step()
+        assert _proxy.time() == 0
+        assert _proxy.current_time() == 1
+        assert _proxy.is_frozen
+
+        _time.step(2)
+        assert _proxy.time() == 0
+        assert _proxy.current_time() == 3
+        assert _proxy.is_frozen
+
+        _time.step()
+        assert _proxy.time() == 0
+        assert _proxy.current_time() == 4
+        assert _proxy.is_frozen
+
+        _proxy.unfreeze()
+        assert _proxy.time() == 4
+        assert _proxy.current_time() == 4
+        assert not _proxy.is_frozen
