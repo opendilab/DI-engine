e9f1d60bb89049c9ce90153f8182da4d258e8285
HansBug <killog@126.com>
2020-12-06 22:51:12 +0800

    add more tests

--- nervex/utils/autolog/test/test_model.py
+++ nervex/utils/autolog/test/test_model.py
@@ -1,11 +1,13 @@
+import time
 from functools import partial
 from typing import Union
 
 import pytest
 
-from nervex.utils.autolog import LoggedModel, LoggedValue, TickTime
+from nervex.utils.autolog import LoggedModel, LoggedValue, TickTime, NaturalTime
 
 
+# noinspection DuplicatedCode
 @pytest.mark.unittest
 class TestAutologModel:
     # noinspection DuplicatedCode
@@ -21,7 +23,6 @@ class TestAutologModel:
                 self.__register()
 
             def __register(self):
-
                 def __avg_func(prop_name: str) -> float:
                     records = self.range_values[prop_name]()
                     _sum = sum([_value for (_begin_time, _end_time), _value in records])
@@ -32,28 +33,62 @@ class TestAutologModel:
 
         return _TickModel
 
-    def test_autolog_model(self):
+    def test_autolog_model_with_tick_time(self):
         _class = self.__get_demo_class()
 
-        tick_time = TickTime()
-        _tick_monitor = _class(tick_time, expire=10)
+        _time = TickTime()
+        _tick_monitor = _class(_time, expire=5)
 
         _assert_results = [
             (0.0, 0.0),
-            (0.1, 0.2),
-            (0.3, 0.6),
+            (0.2, 0.4),
             (0.6, 1.2),
-            (1.0, 2.0),
-            (1.5, 3.0),
+            (1.2, 2.4),
+            (2.0, 4.0),
+            (3.0, 6.0),
+            (4.2, 8.4),
+            (5.4, 10.8),
+            (6.6, 13.2),
+            (7.8, 15.6),
         ]
 
-        for i in range(0, 6):
+        for i in range(0, 10):
             _tick_monitor.in_time = 1.0 * i
             _tick_monitor.out_time = 2.0 * i
-            tick_time.step()
+            _time.step()
 
             _thin, _thout = _tick_monitor.thruput['in_time'](), _tick_monitor.thruput['out_time']()
             _exp_thin, _exp_thout = _assert_results[i]
 
             assert _thin == _exp_thin
             assert _thout == _exp_thout
+
+    def test_autolog_model_with_natural_time(self):
+        _class = self.__get_demo_class()
+
+        _time = NaturalTime()
+        _tick_monitor = _class(_time, expire=5)
+
+        _assert_results = [
+            (0.0, 0.0),
+            (0.2, 0.4),
+            (0.6, 1.2),
+            (1.2, 2.4),
+            (2.0, 4.0),
+            (3.0, 6.0),
+            (4.0, 8.0),
+            (5.0, 10.0),
+            (6.0, 12.0),
+            (7.0, 14.0),
+        ]
+
+        for i in range(0, 10):
+            _tick_monitor.in_time = 1.0 * i
+            _tick_monitor.out_time = 2.0 * i
+            time.sleep(1.0)
+
+            _thin, _thout = _tick_monitor.thruput['in_time'](), _tick_monitor.thruput['out_time']()
+            _exp_thin, _exp_thout = _assert_results[i]
+
+            assert abs(_thin - _exp_thin) < 0.1
+            assert abs(_thout - _exp_thout) < 0.1
