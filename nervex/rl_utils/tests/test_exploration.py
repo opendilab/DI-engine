ff9c6eac19fa8082c1500d53b398128ea93cb02b
niuyazhe <niuyazhe@sensetime.com>
2020-12-06 23:57:08 +0800

    polish test exploration noise accuracy

--- nervex/rl_utils/tests/test_exploration.py
+++ nervex/rl_utils/tests/test_exploration.py
@@ -24,15 +24,15 @@ def test_noise():
     assert g_noise.shape == logits.shape
     assert g_noise.device == logits.device
 
-    x0 = torch.Tensor(bs, dim)
+    x0 = torch.rand(bs, dim)
     ou = create_noise_generator(noise_type='ou', noise_kwargs={'mu': 0.1, 'sigma': 1.0, 'theta': 2.0, 'x0': x0})
     o_noise1 = ou((bs, dim), x0.device)
     o_noise2 = ou((bs, dim), x0.device)
     assert o_noise2.shape == x0.shape
     assert o_noise2.device == x0.device
     assert not torch.equal(ou.x0, ou._x)  # OUNoise._x is not the same as _x0 after 2 calls
-    assert torch.equal(x0, ou.x0)  # OUNoise._x0 does not change
+    assert torch.abs(x0 - ou.x0).max() < 1e-6  # OUNoise._x0 does not change
     x0 += 0.05
     ou.x0 = x0
-    assert torch.equal(ou.x0, x0) and torch.equal(ou.x0, ou._x)
+    assert torch.abs(ou.x0 - x0).max() < 1e-6 and torch.abs(ou.x0 - ou._x).max() < 1e-6
     o_noise3 = ou(x0.shape, x0.device)
