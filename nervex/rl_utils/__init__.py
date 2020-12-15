938ee79da3674591c02000507bb945e6c594bb03
zhangwenhao <zhangwenhao@sensetime.com>
2020-12-15 16:57:26 +0800

    n step dist td error

--- nervex/rl_utils/__init__.py
+++ nervex/rl_utils/__init__.py
@@ -5,7 +5,7 @@ from .a2c import a2c_data, a2c_error
 from .coma import coma_data, coma_error
 from .td import q_nstep_td_data, q_nstep_td_error, q_1step_td_data, q_1step_td_error, td_lambda_data, td_lambda_error,\
     q_nstep_td_error_with_rescale, v_1step_td_data, v_1step_td_error, generalized_lambda_returns, dist_1step_td_data, \
-    dist_1step_td_error
+    dist_1step_td_error, dist_nstep_td_error, dist_nstep_td_data
 from .vtrace import vtrace_loss, compute_importance_weights
 from .upgo import upgo_loss
 from .adder import Adder
