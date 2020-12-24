5c65cf6c0df21a2ffde58f42dfebedd176987727
zhangwenhao <zhangwenhao@sensetime.com>
2020-12-24 19:15:11 +0800

    coma comment

--- nervex/policy/coma.py
+++ nervex/policy/coma.py
@@ -15,6 +15,25 @@ from .common_policy import CommonPolicy
 class COMAPolicy(CommonPolicy):
 
     def _init_learn(self) -> None:
+        r"""
+        Overview:
+            Init the learner agent of COMAPolicy
+
+        Arguments:
+            .. note::
+
+                The _init_learn method takes the argument from the self._cfg.learn in the config file
+
+            - learning_rate (:obj:`float`): The learning rate fo the optimizer
+            - gamma (:obj:`float`): The discount factor
+            - lambda (:obj:`float`): The lambda factor, determining the mix of bootstrapping
+          vs further accumulation of multistep returns at each timestep,
+            - value_wight(:obj:`float`): The weight of value loss in total loss
+            - entropy_weight(:obj:`float`): The weight of entropy loss in total loss
+            - agent_num (:obj:`int`): Since this is a multi-agent algorithm, \
+                we need to input the agent num
+            - batch_size (:obj:`int`): Need batch size info to init hidden_state plugins
+        """
         self._optimizer = Adam(self._model.parameters(), lr=self._cfg.learn.learning_rate)
         self._agent = Agent(self._model)
         algo_cfg = self._cfg.learn.algo
@@ -45,6 +64,18 @@ class COMAPolicy(CommonPolicy):
         self._learn_setting_set = {}
 
     def _data_preprocess_learn(self, data: List[Any]) -> dict:
+        r"""
+        Overview:
+            Preprocess the data to fit the required data format for learning
+
+        Arguments:
+            - data (:obj:`List[Dict[str, Any]]`): the data collected from collect function, the Dict
+                in data should contain keys including at least ['obs', 'action', 'reward']
+
+        Returns:
+            - data (:obj:`Dict[str, Any]`): the processed data, including at least \
+                ['obs', 'action', 'reward', 'done', 'weight']
+        """
         # data preprocess
         data = timestep_collate(data)
         assert set(data.keys()) > set(['obs', 'action', 'reward'])
@@ -55,6 +86,23 @@ class COMAPolicy(CommonPolicy):
         return data
 
     def _forward_learn(self, data: dict) -> Dict[str, Any]:
+        r"""
+        Overview:
+            Forward and backward function of learn mode, acquire the data and calculate the loss and\
+            optimize learner model
+
+        Arguments:
+            - data (:obj:`dict`): Dict type data, including at least \
+                ['obs', 'action', 'reward', 'done', 'weight']
+
+        Returns:
+            - info_dict (:obj:`Dict[str, Any]`): Including cur_lr and total_loss
+                - cur_lr (:obj:`float`): Current learning rate
+                - total_loss (:obj:`float`): The calculated loss
+                - policy_loss (:obj:`float`): The policy(actor) loss of coma
+                - value_loss (:obj:`float`): The value(critic) loss of coma
+                - entropy_loss (:obj:`float`): The entropy loss
+        """
         # forward
         self._agent.reset(state=data['prev_state'][0])
         q_value = self._agent.forward(data, param={'mode': 'compute_q_value'})['q_value']
@@ -81,6 +129,12 @@ class COMAPolicy(CommonPolicy):
         }
 
     def _init_collect(self) -> None:
+        r"""
+        Overview:
+            Collect mode init moethod. Called by ``self.__init__``.
+            Init traj and unroll length, adder, collect agent.
+            Agent has eps_greedy_sample plugin and hidden state plugin
+        """
         self._traj_len = self._cfg.collect.traj_len
         if self._traj_len == "inf":
             self._traj_len = float("inf")
@@ -101,9 +155,31 @@ class COMAPolicy(CommonPolicy):
         self._collect_setting_set = {'eps'}
 
     def _forward_collect(self, data_id: List[int], data: dict) -> dict:
+        r"""
+        Overview:
+            Collect output according to eps_greedy plugin
+
+        Arguments:
+            - data_id (:obj:`List` of :obj:`int`): Not used, set in arguments for consistency
+            - data (:obj:`dict`): Dict type data, including at least ['obs'].
+
+        Returns:
+            - data (:obj:`dict`): The collected data
+        """
         return self._collect_agent.forward(data, eps=self._eps, data_id=data_id, param={'mode': 'compute_action'})
 
     def _process_transition(self, obs: Any, agent_output: dict, timestep: namedtuple) -> dict:
+        r"""
+        Overview:
+            Generate dict type transition data from inputs.
+        Arguments:
+            - obs (:obj:`Any`): Env observation
+            - agent_output (:obj:`dict`): Output of collect agent, including at least ['action', 'prev_state']
+            - timestep (:obj:`namedtuple`): Output after env step, including at least ['obs', 'reward', 'done'] \
+                (here 'obs' indicates obs after env step).
+        Returns:
+            - transition (:obj:`dict`): Dict type transition data.
+        """
         transition = {
             'obs': obs,
             'next_obs': timestep.obs,
@@ -115,6 +191,11 @@ class COMAPolicy(CommonPolicy):
         return EasyDict(transition)
 
     def _init_eval(self) -> None:
+        r"""
+        Overview:
+            Evaluate mode init method. Called by ``self.__init__``.
+            Init eval agent with argmax strategy and hidden_state plugin.
+        """
         self._eval_agent = Agent(self._model)
         self._eval_agent.add_plugin(
             'main',
@@ -130,21 +211,69 @@ class COMAPolicy(CommonPolicy):
         self._eval_setting_set = {}
 
     def _forward_eval(self, data_id: List[int], data: dict) -> dict:
+        r"""
+        Overview:
+            Forward function of collect mode, similar to ``self._forward_collect``.
+
+        Arguments:
+            - data_id (:obj:`List[int]`): Not used in this policy.
+            - data (:obj:`dict`): Dict type data, including at least ['obs'].
+
+        Returns:
+            - output (:obj:`dict`): Dict type data, including at least inferred action according to input obs.
+        """
         return self._eval_agent.forward(data, data_id=data_id, param={'mode': 'compute_action'})
 
     def _init_command(self) -> None:
+        r"""
+        Overview:
+            Command mode init method. Called by ``self.__init__``.
+            Init the self.epsilon_greedy according to eps config
+        """
         eps_cfg = self._cfg.command.eps
         self.epsilon_greedy = epsilon_greedy(eps_cfg.start, eps_cfg.end, eps_cfg.decay, eps_cfg.type)
 
     def _get_setting_collect(self, command_info: dict) -> dict:
+        r"""
+        Overview:
+            Collect mode setting information including eps
+
+        Arguments:
+            - command_info (:obj:`dict`): Dict type, including at least ['learner_step']
+
+        Returns:
+           - collect_setting (:obj:`dict`): Including eps in collect mode.
+        """
         learner_step = command_info['learner_step']
         return {'eps': self.epsilon_greedy(learner_step)}
 
     def _get_train_sample(self, traj_cache: deque) -> Union[None, List[Any]]:
+        r"""
+        Overview:
+            Get the trajectory, then sample from trajectory
+
+        Arguments:
+            - traj_cache (:obj:`deque`): The trajectory's cache
+
+        Returns:
+            - samples (:obj:`dict`): The training samples generated
+        """
         data = self._adder.get_traj(traj_cache, self._traj_len, return_num=0)
         return self._adder.get_train_sample(data)
 
     def _create_model_from_cfg(self, cfg: dict, model_type: Optional[type] = None) -> torch.nn.Module:
+        r"""
+        Overview:
+            Create a model according to input config. Defalut use ComaNetwork
+
+        Arguments:
+            - cfg (:obj:`dict`): Config, including the config contain model parameters
+            - model_type (:obj:`type` or None): The type of the model to create, if this is not None, this\
+                function will create an instance of the model_type.
+
+        Returns:
+            - model (:obj:`torch.nn.Module`): Generted model.
+        """
         if model_type is None:
             return ComaNetwork(**cfg.model)
         else:
