0d7074f11432a07a3f2ad8174147bf19cb2ccff4
HansBug <killog@126.com>
2020-12-25 15:24:27 +0800

    add base class

--- nervex/interaction/tests/interaction/test_errors.py
+++ nervex/interaction/tests/interaction/test_errors.py
@@ -1,48 +1,39 @@
 import pytest
+from requests import HTTPError
 
 from .bases import _TestInteractionBase
-from ..test_utils import random_port
+from ..test_utils import random_port, random_channel
+from ...base import get_values_from_response
+from ...slave import SlaveErrorCode
 
 
 @pytest.mark.unittest
 class TestInteractionErrors(_TestInteractionBase):
     @pytest.mark.execution_timeout(20.0, method='thread')
     def test_slave_simple_connection(self):
-        _slave_port, _channel = self._random_slave_channel_and_port()
-        slave_thread, open_slave_event, close_slave_event = self._slave_endpoint(_slave_port, _channel)
+        _slave_port, _slave_channel = self._random_slave_channel_and_port()
+        slave_thread, open_slave_event, close_slave_event = self._slave_endpoint(_slave_port, _slave_channel)
 
         slave_thread.start()
         open_slave_event.wait()
 
         try:
             _master_port = random_port()
-            master = self._get_master_endpoint(_master_port, _channel)
+            _master_channel = random_channel(excludes=[_slave_channel])
+            master = self._get_master_endpoint(_master_port, _master_channel)
             with master:
                 assert master.ping()
 
-                with master.new_connection('conn', '127.0.0.1', _slave_port) as conn:
-                    assert conn.is_connected
-                    assert 'conn' in master
-                    assert master['conn'] == conn
+                with pytest.raises(HTTPError) as ei:
+                    with master.new_connection('conn', '127.0.0.1', _slave_port) as conn:
+                        pytest.fail('Should not reach here!')
 
-                assert not conn.is_connected
-                assert 'conn' not in master
+                err = ei.value
+                _status_code, _success, _code, _, _ = get_values_from_response(err.response)
+                assert _status_code == 401
+                assert not _success
+                assert _code == SlaveErrorCode.CHANNEL_INVALID
 
-                conn = master.new_connection('conn', '127.0.0.1', _slave_port)
-                conn.connect()
-                assert conn.is_connected
-                assert 'conn' in master
-                assert master['conn'] == conn
-                conn.disconnect()
-                assert not conn.is_connected
-                assert 'conn' not in master
-
-                conn = master.new_connection('conn', '127.0.0.1', _slave_port)
-                conn.connect()
-                assert conn.is_connected
-                assert 'conn' in master
-                assert master['conn'] == conn
-                del master['conn']
                 assert not conn.is_connected
                 assert 'conn' not in master
 
--- nervex/interaction/tests/test_utils/random.py
+++ nervex/interaction/tests/test_utils/random.py
@@ -6,5 +6,9 @@ def random_port(excludes: Iterable[int] = None) -> int:
     return random.choice(list(set(range(10000, 20000)) - set(excludes or [])))
 
 
-def random_channel() -> int:
-    return random.randint(1000, (1 << 31) - 1)
+def random_channel(excludes: Iterable[int] = None) -> int:
+    excludes = set(list(excludes))
+    while True:
+        _channel = random.randint(1000, (1 << 31) - 1)
+        if _channel not in excludes:
+            return _channel
