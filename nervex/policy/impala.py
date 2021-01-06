4a1a92d2868aad0fd688f8a8698ae5a71bc5f913
pihuaijin <pihuaijin@sensetime.com>
2021-01-06 23:03:52 +0800

    update and add grad_clip in impala

--- nervex/policy/impala.py
+++ nervex/policy/impala.py
@@ -3,9 +3,11 @@ from collections import namedtuple, deque
 import torch
 import torch.nn.functional as F
 
+from nervex.data import default_collate, default_decollate
+from nervex.torch_utils import to_device
 from nervex.torch_utils import Adam
 from nervex.rl_utils import Adder, vtrace_data, vtrace_error
-from nervex.model import FCValueAC
+from nervex.model import FCValueAC, ConvValueAC
 from nervex.agent import Agent
 from .base_policy import Policy, register_policy
 from .common_policy import CommonPolicy
@@ -14,8 +16,18 @@ from .common_policy import CommonPolicy
 class IMPALAPolicy(CommonPolicy):
 
     def _init_learn(self) -> None:
-        self._optimizer = Adam(self._model.parameters(), lr=self._cfg.learn.learning_rate)
+        grad_clip_type = self._cfg.learn.get("grad_clip_type", None)
+        clip_value = self._cfg.learn.get("clip_value", None)
+        self._optimizer = Adam(
+            self._model.parameters(),
+            grad_clip_type=grad_clip_type,
+            clip_value=clip_value,
+            lr=self._cfg.learn.learning_rate
+        )
         self._agent = Agent(self._model)
+
+        self._action_dim = self._cfg.model.action_dim
+
         algo_cfg = self._cfg.learn.algo
         self._value_weight = algo_cfg.value_weight
         self._entropy_weight = algo_cfg.entropy_weight
@@ -30,10 +42,31 @@ class IMPALAPolicy(CommonPolicy):
         self._agent.reset()
         self._learn_setting_set = {}
 
+    def _data_preprocess_learn(self, data: List[Dict[str, Any]]) -> dict:
+        # data preprocess
+        data = default_collate(data)
+        if self._use_cuda:
+            data = to_device(data, 'cuda')
+        ignore_done = self._cfg.learn.get('ignore_done', False)
+        if ignore_done:
+            data['done'] = None
+        else:
+            data['done'] = data['done'].float()
+        use_priority = self._cfg.get('use_priority', False)
+        if use_priority:
+            data['weight'] = data['IS']
+        else:
+            data['weight'] = data.get('weight', None)
+        data['obs_plus_1'] = torch.cat((data['obs'] + data['next_obs'][-1:]), dim=0)
+        data['logit'] = torch.cat(data['logit'], dim=0).reshape(self._unroll_len, -1, self._action_dim)
+        data['action'] = torch.cat(data['action'], dim=0).reshape(self._unroll_len, -1)
+        data['reward'] = torch.cat(data['reward'], dim=0).reshape(self._unroll_len, -1)
+        data['weight'] = torch.cat(data['weight'], dim=0).reshape(self._unroll_len, -1) if data['weight'] else None
+        return data
+
     def _forward_learn(self, data: dict) -> Dict[str, Any]:
         # forward
-        obs_plus_1 = torch.cat((data['obs'] + data['next_obs'][-1:]), dim=0)
-        output = self._agent.forward(obs_plus_1, param={'mode': 'compute_action_value'})
+        output = self._agent.forward(data['obs_plus_1'], param={'mode': 'compute_action_value'})
         target_logit, behaviour_logit, actions, values, rewards, weights = self._reshape_data(output, data)
         # calculate vtrace error
         data = vtrace_data(target_logit, behaviour_logit, actions, values, rewards, weights)
@@ -54,12 +87,12 @@ class IMPALAPolicy(CommonPolicy):
         }
 
     def _reshape_data(self, output: dict, data: dict) -> tuple:
-        target_logit = output['logit'].reshape(self._unroll_len + 1, -1, output['logit'].shape[-1])[:-1]
-        behaviour_logit = torch.cat(data['logit'], dim=0).reshape(self._unroll_len, -1, output['logit'].shape[-1])
-        actions = torch.cat(data['action'], dim=0).reshape(self._unroll_len, -1)
+        target_logit = output['logit'].reshape(self._unroll_len + 1, -1, self._action_dim)[:-1]
         values = output['value'].reshape(self._unroll_len + 1, -1)
-        rewards = torch.cat(data['reward'], dim=0).reshape(self._unroll_len, -1)
-        weights = torch.cat(data['weight'], dim=0).reshape(self._unroll_len, -1) if data['weight'] else None
+        behaviour_logit = data['logit']
+        actions = data['action']
+        rewards = data['reward']
+        weights = data['weight']
         return target_logit, behaviour_logit, actions, values, rewards, weights
 
     def _init_collect(self) -> None:
@@ -67,7 +100,7 @@ class IMPALAPolicy(CommonPolicy):
         self._unroll_len = self._cfg.collect.unroll_len
         if self._traj_len == 'inf':
             self._traj_len = float('inf')
-        # because gae calculation need v_t+1
+        # v_trace need v_t+1
         assert self._traj_len > 1, "IMPALA traj len should be greater than 1"
         self._collect_agent = Agent(self._model)
         self._collect_agent.add_plugin('main', 'multinomial_sample')
@@ -108,7 +141,10 @@ class IMPALAPolicy(CommonPolicy):
 
     def _create_model_from_cfg(self, cfg: dict, model_type: Optional[type] = None) -> torch.nn.Module:
         if model_type is None:
-            return FCValueAC(**cfg.model)
+            if isinstance(cfg.model.obs_dim, List):
+                return ConvValueAC(**cfg.model)
+            else:
+                return FCValueAC(**cfg.model)
         else:
             return model_type(**cfg.model)
 
