ae59c34bc419677a87b7077a1f78db81d7bef1a3
HansBug <killog@126.com>
2021-01-17 14:27:53 +0800

    add slave exception test

--- nervex/interaction/tests/exception/test_slave.py
+++ nervex/interaction/tests/exception/test_slave.py
@@ -1,13 +1,15 @@
 import pytest
 
-from ...exception.slave import SlaveErrorCode, get_slave_exception_class_by_error_code, SlaveSystemShuttingDown, \
+from .test_base import _HTTPErrorGenerator
+from ...exception.slave import SlaveErrorCode, \
+    get_slave_exception_class_by_error_code, get_slave_exception_by_error, SlaveSystemShuttingDown, \
     SlaveSlaveConnectionRefused, SlaveSlaveDisconnectionRefused, SlaveSlaveNotConnected, SlaveSlaveAlreadyConnected, \
     SlaveTaskRefused, SlaveMasterTokenInvalid, SlaveMasterTokenNotFound, SlaveSelfTokenNotFound, \
     SlaveTaskAlreadyExist, SlaveSelfTokenInvalid, SlaveChannelNotFound, SlaveChannelInvalid, SlaveSuccess
 
 
 @pytest.mark.unittest
-class TestInteractionExceptionSlave:
+class TestInteractionExceptionSlave(_HTTPErrorGenerator):
 
     def test_error_code(self):
         assert len(SlaveErrorCode.__members__) == 14
@@ -43,3 +45,20 @@ class TestInteractionExceptionSlave:
 
         assert get_slave_exception_class_by_error_code(SlaveErrorCode.TASK_ALREADY_EXIST) == SlaveTaskAlreadyExist
         assert get_slave_exception_class_by_error_code(SlaveErrorCode.TASK_REFUSED) == SlaveTaskRefused
+
+    def test_get_slave_exception_by_error(self):
+        err = get_slave_exception_by_error(self._generate_exception(101, 'This is slave shutting down.'))
+        assert isinstance(err, SlaveSystemShuttingDown)
+        assert not err.success
+        assert err.status_code == 400
+        assert err.code == 101
+        assert err.message == 'This is slave shutting down.'
+        assert err.data == {}
+
+        err = get_slave_exception_by_error(self._generate_exception(602, 'Task refused.', data={'value': 233}))
+        assert isinstance(err, SlaveTaskRefused)
+        assert not err.success
+        assert err.status_code == 400
+        assert err.code == 602
+        assert err.message == 'Task refused.'
+        assert err.data == {'value': 233}
