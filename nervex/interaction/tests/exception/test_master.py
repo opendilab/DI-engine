863c983842ddcb0cb53f7b295bcf3467c12af4dc
HansBug <killog@126.com>
2021-01-17 14:25:14 +0800

    add test base

--- nervex/interaction/tests/exception/test_master.py
+++ nervex/interaction/tests/exception/test_master.py
@@ -1,13 +1,15 @@
 import pytest
 
-from ...exception.master import MasterErrorCode, get_master_exception_class_by_error_code, MasterSuccess, \
+from .test_base import _HTTPErrorGenerator
+from ...exception.master import MasterErrorCode, \
+    get_master_exception_class_by_error_code, get_master_exception_by_error, MasterSuccess, \
     MasterSystemShuttingDown, MasterTaskDataInvalid, MasterSlaveTokenNotGiven, MasterSlaveTokenInvalid, \
     MasterSelfTokenNotGiven, MasterSelfTokenInvalid, MasterChannelInvalid, \
     MasterChannelNotGiven, MasterMasterTokenInvalid, MasterMasterTokenNotGiven
 
 
 @pytest.mark.unittest
-class TestInteractionExceptionMaster:
+class TestInteractionExceptionMaster(_HTTPErrorGenerator):
 
     def test_error_code(self):
         assert len(MasterErrorCode.__members__) == 11
@@ -39,3 +41,20 @@ class TestInteractionExceptionMaster:
         assert get_master_exception_class_by_error_code(MasterErrorCode.SLAVE_TOKEN_INVALID) == MasterSlaveTokenInvalid
 
         assert get_master_exception_class_by_error_code(MasterErrorCode.TASK_DATA_INVALID) == MasterTaskDataInvalid
+
+    def test_get_master_exception_by_error(self):
+        err = get_master_exception_by_error(self._generate_exception(101, 'This is system shutting down.'))
+        assert isinstance(err, MasterSystemShuttingDown)
+        assert not err.success
+        assert err.status_code == 400
+        assert err.code == 101
+        assert err.message == 'This is system shutting down.'
+        assert err.data == {}
+
+        err = get_master_exception_by_error(self._generate_exception(601, 'Task data invalid.', data={'value': 233}))
+        assert isinstance(err, MasterTaskDataInvalid)
+        assert not err.success
+        assert err.status_code == 400
+        assert err.code == 601
+        assert err.message == 'Task data invalid.'
+        assert err.data == {'value': 233}
