da97344fb9942c256bdcfdfa17de8a68bb712155
niuyazhe <niuyazhe@sensetime.com>
2021-01-19 00:12:07 +0800

    fix stupid slave host bug

--- nervex/interaction/slave/slave.py
+++ nervex/interaction/slave/slave.py
@@ -50,7 +50,7 @@ class Slave(ControllableService):
                 'Token': lambda: self.__self_token,
             },
             http_error_gene=get_slave_exception_by_error,
-        )()(self.__port, self.__port, False)
+        )()(self.__host, self.__port, False)
         self.__self_token = random_token()
 
         # master-connection part
