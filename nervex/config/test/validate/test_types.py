c7d50d73ac4d2ee871d6eb85726e9b23272b1a9b
HansBug <killog@126.com>
2021-01-26 17:58:09 +0800

    split the tests

--- nervex/config/test/validate/test_types.py
+++ nervex/config/test/validate/test_types.py
@@ -24,7 +24,7 @@ class TestConfigValidateTypes:
         pass
 
     # noinspection DuplicatedCode
-    def test_numeric(self):
+    def test_numeric_plain(self):
         _validator = numeric()
 
         assert _validator(1)
@@ -41,6 +41,8 @@ class TestConfigValidateTypes:
         assert not _validator('-abcdef12345')
         assert not _validator('i n  f')
 
+    # noinspection DuplicatedCode
+    def test_numeric_int_ban(self):
         _validator = numeric(int_ok=False)
         assert not _validator(1)
         assert _validator(1.0)
@@ -56,6 +58,8 @@ class TestConfigValidateTypes:
         assert not _validator('-abcdef12345')
         assert not _validator('i n  f')
 
+    # noinspection DuplicatedCode
+    def test_numeric_float_ban(self):
         _validator = numeric(float_ok=False)
         assert _validator(1)
         assert not _validator(1.0)
@@ -71,9 +75,12 @@ class TestConfigValidateTypes:
         assert not _validator('-abcdef12345')
         assert not _validator('i n  f')
 
+    def test_numeric_double_ban(self):
         with pytest.raises(ValueError):
             numeric(int_ok=False, float_ok=False)
 
+    # noinspection DuplicatedCode
+    def test_numeric_inf_ban(self):
         _validator = numeric(inf_ok=False)
         assert _validator(1)
         assert _validator(1.0)
@@ -89,6 +96,8 @@ class TestConfigValidateTypes:
         assert not _validator('-abcdef12345')
         assert not _validator('i n  f')
 
+    # noinspection DuplicatedCode
+    def test_numeric_str_ban(self):
         _validator = numeric(str_ok=False)
         assert _validator(1)
         assert _validator(1.0)
@@ -104,6 +113,8 @@ class TestConfigValidateTypes:
         assert not _validator('-abcdef12345')
         assert not _validator('i n  f')
 
+    # noinspection DuplicatedCode
+    def test_numeric_inf_and_str_ban(self):
         _validator = numeric(str_ok=False, inf_ok=False)
         assert _validator(1)
         assert _validator(1.0)
