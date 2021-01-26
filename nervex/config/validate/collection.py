d20ef1cc2dd3e3cf0f58e34f0946765c949df6b9
HansBug <killog@126.com>
2021-01-26 15:02:11 +0800

    add collection

--- nervex/config/validate/collection.py
+++ nervex/config/validate/collection.py
@@ -3,6 +3,19 @@ from typing import Optional
 from .base import _IValidator, Validator
 
 
+def collection(validator) -> _IValidator:
+    validator = Validator(validator)
+
+    def _validate(value):
+        if hasattr(value, '__iter__'):
+            for item in value.__iter__():
+                validator.validate(item)
+        else:
+            raise TypeError('type {type} not support __iter__'.format(type=repr(value.__class__.__name__)))
+
+    return Validator(_validate)
+
+
 def length(min_length: Optional[int] = None, max_length: Optional[int] = None) -> _IValidator:
     def _validate(value):
         if hasattr(value, '__len__'):
