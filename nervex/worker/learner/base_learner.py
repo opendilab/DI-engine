14ff00c435fcc5349d17bb0cc3bbf159644a2922
zlx-sensetime <zhaoliangxuan@sensetime.com>
2021-01-28 16:16:52 +0800

    learner comment + overview, no pic

--- nervex/worker/learner/base_learner.py
+++ nervex/worker/learner/base_learner.py
@@ -5,7 +5,7 @@ Main Function:
     1. base class for model learning
 """
 import os
-from typing import Any, Union, Callable
+from typing import Any, Union, Callable, List
 from functools import partial
 from easydict import EasyDict
 import torch
@@ -23,10 +23,10 @@ from .learner_hook import build_learner_hook_by_cfg, add_learner_hook, merge_hoo
 class TickMonitor(LoggedModel):
     """
     Overview:
-        TickMonitor is to monitor related info of one training iteration.
-        Info include: cur_lr, time(data, train, forward, backward), loss(total,...)
-        These info variables would first be recorded in ``log_buffer``, then in ``LearnerHook`` will vars in
-        in this monitor be updated by``log_buffer``, then printed to ``TextLogger`` and ``TensorBoradLogger``.
+        TickMonitor is to monitor related info during training.
+        Info includes: cur_lr, time(data, train, forward, backward), loss(total,...)
+        These info variables are firstly recorded in ``log_buffer``, then in ``LearnerHook`` will vars in
+        in this monitor be updated by``log_buffer``, finally printed to ``TextLogger`` and ``TensorBoradLogger``.
     Interface:
         __init__, fixed_time, current_time, freeze, unfreeze, register_attribute_value, __getattr__
     Property:
@@ -61,7 +61,17 @@ class TickMonitor(LoggedModel):
             self.register_attribute_value('val', k, partial(__val_func, prop_name=k))
 
 
-def get_simple_monitor_type(properties: list = []):
+def get_simple_monitor_type(properties: List[str] = []) -> TickMonitor:
+    """
+    Overview:
+        Besides basic training variables provided in ``TickMonitor``, many policies have their own customized 
+        ones to record and monitor. This function can return a customized tick monitor. 
+        Compared with ``TickMonitor``, ``SimpleTickMonitor`` can record extra ``properties`` passed in by a policy. 
+    Argumenst:
+         - properties (:obj:`List[str]`): Customized properties to monitor.
+    Returns:
+        - simple_tick_monitor (:obj:`SimpleTickMonitor`): A simple customized tick monitor.
+    """
     if len(properties) == 0:
         return TickMonitor
     else:
@@ -78,12 +88,12 @@ def get_simple_monitor_type(properties: list = []):
 class BaseLearner(object):
     r"""
     Overview:
-        base class for model learning(SL/RL), which is able to multi-GPU learning
+        Base class for model learning.
     Interface:
-        __init__, run, close, call_hook, info, save_checkpoint, launch
+        __init__, train, start, setup_dataloader, close, call_hook, register_hook, save_checkpoint
     Property:
-        last_iter, policy, log_buffer, record,
-        load_path, checkpoint_manager, name, rank, tb_logger, use_distributed
+        learn_info, priority_info, collect_info, last_iter, name, rank, policy
+        tick_time, monitor, log_buffer, logger, tb_logger, load_path, checkpoint_manager
     """
 
     _name = "BaseLearner"  # override this variable for sub-class learner
@@ -91,53 +101,58 @@ class BaseLearner(object):
     def __init__(self, cfg: EasyDict) -> None:
         """
         Overview:
-            initialization method, load config setting and call ``_init`` for actual initialization,
-            set the communication mode to `single_machine` or `flask_fs`.
-            Use ``self._cfg`` setting to build common learner components, such as logger helper, checkpoint helper.
-            launch learner runtime components, each train job means a launch operation,
-            job related dataloader, policy and hook support.
+            Init method. Load config and use ``self._cfg`` setting to build common learner components,
+            e.g. logger helper, checkpoint helper, hooks.
+            Policy is not initialized here, but set afterwards through policy setter.
         Arguments:
-            - cfg (:obj:`EasyDict`): learner config, you can view `cfg <../../../configuration/index.html>`_ for ref.
+            - cfg (:obj:`EasyDict`): Learner config, you can view `cfg <../../../configuration/index.html>`_ for ref.
         Notes:
-            if you want to debug in sync CUDA mode, please use the following line code in the beginning of ``__init__``.
+            If you want to debug in sync CUDA mode, please add the following code at the beginning of ``__init__``.
 
             .. code:: python
 
                 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"  # for debug async CUDA
         """
         self._cfg = deep_merge_dicts(base_learner_default_config, cfg)
-
         self._learner_uid = get_task_uid()
         self._load_path = self._cfg.load_path
         self._use_cuda = self._cfg.get('use_cuda', False)
         self._use_distributed = self._cfg.use_distributed
+
+        # Learner rank. Used to discriminate which GPU it uses.
         self._rank = get_rank()
-        self._timer = EasyTimer()
         self._device = 'cuda:{}'.format(self._rank % 8) if self._use_cuda else 'cpu'
-        # monitor & logger
-        # Only rank == 0 learner needs monitor and tb_logger, else only needs text_logger to display terminal output
+
+        # Logger (Monitor is initialized in policy setter)
+        # Only rank == 0 learner needs monitor and tb_logger, others only need text_logger to display terminal output.
+        self._timer = EasyTimer()
         rank0 = True if self._rank == 0 else False
         self._logger, self._tb_logger = build_logger('./log/learner', 'learner', rank0)
         self._log_buffer = build_log_buffer()
-        # checkpoint helper
+
+        # Checkpoint helper. Used to save model checkpoint.
         self._checkpointer_manager = build_checkpoint_helper(self._cfg)
+        # Learner hook. Used to do specific things at specific time point. Will be set in ``_setup_hook``
         self._hooks = {'before_run': [], 'before_iter': [], 'after_iter': [], 'after_run': []}
+        # Collect info passed from actor. Used in serial entry for message passing.
         self._collect_info = {}
+        # Priority info. Used to update replay buffer according to data's priority.
         self._priority_info = None
+        # Last iteration. Used to record current iter.
         self._last_iter = CountVar(init_val=0)
         self.info(pretty_print({
             "config": self._cfg,
         }, direct_print=False))
 
-        # wrapper and hook
+        # Setup wrapper and hook.
         self._setup_wrapper()
         self._setup_hook()
 
     def _setup_hook(self) -> None:
         """
         Overview:
-            Setup hook for base_learner. Hook is the way to implement actual functions in base_learner.
-            You can reference learner_hook.py
+            Setup hook for base_learner. Hook is the way to implement some functions at specific time point 
+            in base_learner. You can refer to ``learner_hook.py``.
         """
         if hasattr(self, '_hooks'):
             self._hooks = merge_hooks(self._hooks, build_learner_hook_by_cfg(self._cfg.hook))
@@ -147,18 +162,20 @@ class BaseLearner(object):
     def _setup_wrapper(self) -> None:
         """
         Overview:
-            Setup time_wrapper to get data_time and train_time
+            Use ``_time_wrapper`` to get ``train_time``.
+        Note:
+            ``data_time`` is wrapped in ``setup_dataloader``.
         """
         self._wrapper_timer = EasyTimer()
-        self.train = self.time_wrapper(self.train, 'train_time')
+        self.train = self._time_wrapper(self.train, 'train_time')
 
-    def time_wrapper(self, fn: Callable, name: str):
+    def _time_wrapper(self, fn: Callable, name: str):
         """
         Overview:
-            Wrap a function and measure the time it used
+            Wrap a function and measure the time it used.
         Arguments:
-            - fn (:obj:`Callable`): function to be time_wrapped
-            - name (:obj:`str`): name to be registered in log_buffer
+            - fn (:obj:`Callable`): Function to be time_wrapped.
+            - name (:obj:`str`): Name to be registered in ``_log_buffer``.
         """
 
         def wrapper(*args, **kwargs) -> Any:
@@ -169,58 +186,65 @@ class BaseLearner(object):
 
         return wrapper
 
-    def train(self, data: dict) -> None:
-        """
-        Overview:
-            Train the input data for 1 iteration, called in ``start`` which involves:
-                - forward
-                - backward
-                - sync grad (if in distributed mode)
-                - parameter update
-        Arguments:
-            - data (:obj:`dict`): data used for training
-        """
-        assert hasattr(self, '_policy'), "please set learner policy"
-        self.call_hook('before_iter')
-        replay_buffer_idx = [d.get('replay_buffer_idx', None) for d in data]
-        replay_unique_id = [d.get('replay_unique_id', None) for d in data]
-        with self._timer:
-            data = self._policy.data_preprocess(data)
-        log_vars = self._policy.forward(data)
-        priority = log_vars.pop('priority', None)
-        self._priority_info = {
-            'replay_buffer_idx': replay_buffer_idx,
-            'replay_unique_id': replay_unique_id,
-            'priority': priority
-        }
-        log_vars['data_preprocess_time'] = self._timer.value
-        log_vars.update(self.collect_info)
-        self._log_buffer.update(log_vars)
-        self.call_hook('after_iter')
-        self._last_iter.add(1)
-
     def register_hook(self, hook: LearnerHook) -> None:
         """
         Overview:
             Add a new hook to learner.
         Arguments:
-            - hook (:obj:`LearnerHook`): the hook to be added to learner
+            - hook (:obj:`LearnerHook`): The hook to be added to learner.
         """
         add_learner_hook(self._hooks, hook)
+
+    def train(self, data: dict) -> None:
+        """
+        Overview:
+            Given training data, implement network update for one iteration and update related variables.
+            Learner's API for serial entry. Also called in ``start`` for each iteration's training.
+            "before_iter" and "after_iter" hooks are called once each.
+        Arguments:
+            - data (:obj:`dict`): Training data which is retrieved from repaly buffer.
+        Note:
+            ``_policy`` must be set before calling this method.
+            ``_policy.forward`` method contains: forward, backward, grad sync(if in distributed mode) and 
+            parameter update.
+        """
+        assert hasattr(self, '_policy'), "please set learner policy"
+        self.call_hook('before_iter')
+        # Pre-process data
+        with self._timer:
+            data = self._policy.data_preprocess(data)
+        # Forward
+        log_vars = self._policy.forward(data)
+        # Update replay buffer's priority info
+        priority = log_vars.pop('priority', None)
+        replay_buffer_idx = [d.get('replay_buffer_idx', None) for d in data]
+        replay_unique_id = [d.get('replay_unique_id', None) for d in data]
+        self._priority_info = {
+            'replay_buffer_idx': replay_buffer_idx,
+            'replay_unique_id': replay_unique_id,
+            'priority': priority
+        }
+        # Update log_buffer
+        log_vars['data_preprocess_time'] = self._timer.value
+        log_vars.update(self.collect_info)
+        self._log_buffer.update(log_vars)
+
+        self.call_hook('after_iter')
+        self._last_iter.add(1)
 
     @auto_checkpoint
     def start(self) -> None:
         """
         Overview:
-            Start to run the learner.
-            For each iteration, learner will get training data and train.
-            Learner will call hooks at four fixed positions(before_run, before_iter, after_iter, after_run).
+            Learner's API for parallel entry.
+            For each iteration, learner will get data through ``_next_data`` and call ``train`` to train.
+            Besides "before_iter" and "after_iter", "before_run" and "after_run" hooks are called once each.
         """
         self._finished_task = None
         # before run hook
-        max_iterations = self._cfg.max_iterations
         self.call_hook('before_run')
 
+        max_iterations = self._cfg.max_iterations
         for _ in range(max_iterations):
             data = self._next_data()
             self.train(data)
@@ -232,13 +256,13 @@ class BaseLearner(object):
     def setup_dataloader(self) -> None:
         """
         Overview:
-            Setup learner's dataloader, get_data need to be a callable,
-            and setup learner's collate_fn, which aggregate a listed data into a batch tensor.
+            Setup learner's dataloader.
+        Note:
+            Only in parallel version will we use ``get_data`` and ``_dataloader``(AsyncDataLoader);
+            Instead in serial version, we can sample data from replay buffer directly.
+            In parallel version, ``get_data`` is set by comm LearnerCommHelper, and should be callable.
+            Users don't need to know the related details if not necessary.
         """
-        # only in parallel version we use get_data and dataloader, instead in serial version,
-        # main entry directly uses `train` method.
-        # when parallel version, get_data is set by comm LearnerCommHelper
-        # users don't need to know the related details if not necessary
         cfg = self._cfg.dataloader
         self._dataloader = AsyncDataLoader(
             self.get_data,
@@ -248,15 +272,23 @@ class BaseLearner(object):
             collate_fn=lambda x: x,
             num_workers=cfg.num_workers
         )
-        self._next_data = self.time_wrapper(self._next_data, 'data_time')
+        self._next_data = self._time_wrapper(self._next_data, 'data_time')
 
     def _next_data(self) -> Any:
+        """
+        Overview:
+            Call ``_dataloader``'s ``__next__`` method to return next training data.
+        Returns:
+            - data (:obj:`Any`): Next training data from dataloader.
+        Note:
+            Only in parallel version will this method be called.
+        """
         return next(self._dataloader)
 
     def close(self) -> None:
         """
         Overview:
-            Close the related resources, such as dataloader
+            Close the related resources, e.g. dataloader, tensorboard logger, etc.
         """
         if hasattr(self, '_dataloader'):
             del self._dataloader
@@ -265,10 +297,10 @@ class BaseLearner(object):
     def call_hook(self, name: str) -> None:
         """
         Overview:
-            Call the corresponding hook plugins according to name
+            Call the corresponding hook plugins according to position name.
         Arguments:
-            - name (:obj:`str`): hooks in which position to call, \
-                should be in ['before_run', 'after_run', 'before_iter', 'after_iter']
+            - name (:obj:`str`): Hooks in which position to call, \
+                should be in ['before_run', 'after_run', 'before_iter', 'after_iter'].
         """
         for hook in self._hooks[name]:
             hook(self)
@@ -276,20 +308,24 @@ class BaseLearner(object):
     def info(self, s: str) -> None:
         """
         Overview:
-            Log string info by ``self._logger.info``
+            Log string info by ``self._logger.info``.
         Arguments:
-            - s (:obj:`str`): the message to add into the logger
+            - s (:obj:`str`): The message to add into the logger.
         """
         self._logger.info(s)
 
     def save_checkpoint(self) -> None:
         """
         Overview:
-            Automatically save checkpoints.
-            Directly call ``save_ckpt_after_run`` hook instead of calling ``call_hook`` function.
+            Directly call ``save_ckpt_after_run`` hook to save checkpoint.
         Note:
-            This method is called by `auto_checkpoint` function in `checkpoint_helper.py`,
-            designed for saving checkpoint whenever an exception raises.
+            Must guarantee that "save_ckpt_after_run" is registered in "after_run" hook.
+            This method is called in:
+                
+                - ``auto_checkpoint``(``torch_utils/checkpoint_helper.py``), which is designed for \
+                    saving checkpoint whenever an exception raises.
+                - ``serial_pipeline``(``entry/serial_entry.py``). Used to save checkpoint after convergence or \
+                    new highes reward during evaluation.
         """
         names = [h.name for h in self._hooks['after_run']]
         assert 'save_ckpt_after_run' in names
@@ -300,10 +336,10 @@ class BaseLearner(object):
     def learn_info(self) -> dict:
         """
         Overview:
-            Get current info dict, which will be sent to command for coordination,
-            e.g. hyper-parameter adjustment.
+            Get current info dict, which will be sent to commander, e.g. replay buffer priority update,
+            current iteration, hyper-parameter adjustment, whether task is finished, etc.
         Returns:
-            - info (:obj:`dict`): current info dict, including ['learner_step']
+            - info (:obj:`dict`): Current learner info dict.
         """
         ret = {'learner_step': self._last_iter.val, 'priority_info': self._priority_info}
         if hasattr(self, '_finished_task') and self._finished_task is not None:
@@ -364,6 +400,10 @@ class BaseLearner(object):
 
     @policy.setter
     def policy(self, _policy: 'Policy') -> None:  # noqa
+        """
+        Note:
+            Monitor is set alongside with policy, because variables in monitor are determined by specific policy.
+        """
         self._policy = _policy
         if self._rank == 0:
             self._monitor = get_simple_monitor_type(self._policy.monitor_vars())(TickTime(), expire=10)
@@ -396,8 +436,8 @@ def register_learner(name: str, learner: type) -> None:
         Add a new Learner class with its name to dict learner_mapping, any subclass derived from BaseLearner must
         use this function to register in nervex system before instantiate.
     Arguments:
-        - name (:obj:`str`): name of the new Learner
-        - learner (:obj:`type`): the new Learner class, should be subclass of BaseLearner
+        - name (:obj:`str`): Name of the new Learner.
+        - learner (:obj:`type`): The new Learner class, should be subclass of ``BaseLearner``.
     """
     assert isinstance(name, str)
     assert issubclass(learner, BaseLearner)
@@ -407,14 +447,14 @@ def register_learner(name: str, learner: type) -> None:
 def create_learner(cfg: EasyDict) -> BaseLearner:
     """
     Overview:
-        Given the key(learner_type/name), create a new learner instance if in learner_mapping's values,
-        or raise an KeyError. In other words, a derived learner must first register then call ``create_learner``
-        to get the instance object.
+        Given the key(learner_name), create a new learner instance if in learner_mapping's values,
+        or raise an KeyError. In other words, a derived learner must first register, then can call ``create_learner``
+        to get the instance.
     Arguments:
-        - cfg (:obj:`EasyDict`): learner config, necessary keys: [learner.import_module, learner.learner_type]
+        - cfg (:obj:`EasyDict`): Learner config. Necessary keys: [learner.import_module, learner.learner_type].
     Returns:
-        - learner (:obj:`BaseLearner`): the created new learner, should be an instance of one of\
-            learner_mapping's values
+        - learner (:obj:`BaseLearner`): The created new learner, should be an instance of one of \
+            learner_mapping's values.
     """
     import_module(cfg.import_names)
     learner_type = cfg.learner_type
