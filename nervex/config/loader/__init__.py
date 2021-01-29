6771efd4b8e940a01b01726401c3262f47421722
HansBug <killog@126.com>
2021-01-29 23:28:33 +0800

    remove model path && add check_only to __init__

--- nervex/config/loader/__init__.py
+++ nervex/config/loader/__init__.py
@@ -3,4 +3,4 @@ from .collection import length, length_is, contains, collection, mapping, tuple_
 from .exception import CompositeStructureError
 from .number import interval, numeric, negative, plus, minus, minus_with, multi, divide, divide_with, power, power_with
 from .string import enum, rematch, regrep
-from .utils import keep, is_type, to_type, optional
+from .utils import keep, is_type, to_type, optional, check_only
