d94087656c30a04761434105d00a85e1f035e871
HansBug <killog@126.com>
2021-01-31 18:38:54 +0800

    add some of the test of norm part

--- nervex/config/loader/norm.py
+++ nervex/config/loader/norm.py
@@ -2,31 +2,25 @@ import operator
 from abc import abstractmethod
 from typing import Callable, Any
 
-from .base import Loader, ILoaderClass
+from .base import ILoaderClass
 
 
 def _callable_to_norm(func: Callable[[Any], Any]) -> 'INormClass':
     class _Norm(INormClass):
+
         def _call(self, value):
             return func(value)
 
     return _Norm()
 
 
-def _to_norm(value) -> 'INormClass':
+def norm(value) -> 'INormClass':
     if isinstance(value, INormClass):
         return value
     elif isinstance(value, ILoaderClass):
         return _callable_to_norm(value)
     else:
         return _callable_to_norm(lambda v: value)
-
-
-def norm(loader) -> 'INormClass':
-    if isinstance(loader, INormClass):
-        return loader
-    else:
-        return _to_norm(Loader(loader))
 
 
 def _unary(a: 'INormClass', func: Callable[[Any], Any]) -> 'INormClass':
@@ -38,6 +32,7 @@ def _binary(a: 'INormClass', b: 'INormClass', func: Callable[[Any, Any], Any]) -
 
 
 class INormClass:
+
     @abstractmethod
     def _call(self, value):
         raise NotImplementedError
@@ -46,82 +41,82 @@ class INormClass:
         return self._call(value)
 
     def __add__(self, other):
-        return _binary(self, _to_norm(other), operator.__add__)
+        return _binary(self, norm(other), operator.__add__)
 
     def __radd__(self, other):
-        return _to_norm(other) + self
+        return norm(other) + self
 
     def __sub__(self, other):
-        return _binary(self, _to_norm(other), operator.__sub__)
+        return _binary(self, norm(other), operator.__sub__)
 
     def __rsub__(self, other):
-        return _to_norm(other) - self
+        return norm(other) - self
 
     def __mul__(self, other):
-        return _binary(self, _to_norm(other), operator.__mul__)
+        return _binary(self, norm(other), operator.__mul__)
 
     def __rmul__(self, other):
-        return _to_norm(other) * self
+        return norm(other) * self
 
     def __matmul__(self, other):
-        return _binary(self, _to_norm(other), operator.__matmul__)
+        return _binary(self, norm(other), operator.__matmul__)
 
     def __rmatmul__(self, other):
-        return _to_norm(other) @ self
+        return norm(other) @ self
 
     def __truediv__(self, other):
-        return _binary(self, _to_norm(other), operator.__truediv__)
+        return _binary(self, norm(other), operator.__truediv__)
 
     def __rtruediv__(self, other):
-        return _to_norm(other) / self
+        return norm(other) / self
 
     def __floordiv__(self, other):
-        return _binary(self, _to_norm(other), operator.__floordiv__)
+        return _binary(self, norm(other), operator.__floordiv__)
 
     def __rfloordiv__(self, other):
-        return _to_norm(other) // self
+        return norm(other) // self
 
     def __mod__(self, other):
-        return _binary(self, _to_norm(other), operator.__mod__)
+        return _binary(self, norm(other), operator.__mod__)
 
     def __rmod__(self, other):
-        return _to_norm(other) % self
+        return norm(other) % self
 
     def __pow__(self, power, modulo=None):
-        return _binary(self, _to_norm(power), operator.__pow__)
+        return _binary(self, norm(power), operator.__pow__)
 
     def __rpow__(self, other):
-        return _to_norm(other) ** self
+        return norm(other) ** self
 
     def __lshift__(self, other):
-        return _binary(self, _to_norm(other), operator.__lshift__)
+        return _binary(self, norm(other), operator.__lshift__)
 
     def __rlshift__(self, other):
-        return _to_norm(other) << self
+        return norm(other) << self
 
     def __rshift__(self, other):
-        return _binary(self, _to_norm(other), operator.__rshift__)
+        return _binary(self, norm(other), operator.__rshift__)
 
     def __rrshift__(self, other):
-        return _to_norm(other) >> self
+        return norm(other) >> self
 
     def __and__(self, other):
-        return _binary(self, _to_norm(other), operator.__and__)
+        return _binary(self, norm(other), operator.__and__)
 
     def __rand__(self, other):
-        return _to_norm(other) & self
+        return norm(other) & self
 
     def __or__(self, other):
-        return _binary(self, _to_norm(other), operator.__or__)
+        return _binary(self, norm(other), operator.__or__)
 
     def __ror__(self, other):
-        return _to_norm(other) | self
+        return norm(other) | self
 
     def __xor__(self, other):
-        return _binary(self, _to_norm(other), operator.__xor__)
+        return _binary(self, norm(other), operator.__xor__)
 
     def __rxor__(self, other):
-        return _to_norm(other) ^ self
+        return norm(other) ^ self
 
     def __invert__(self):
         return _unary(self, operator.__invert__)
@@ -132,8 +127,23 @@ class INormClass:
     def __neg__(self):
         return _unary(self, operator.__neg__)
 
-    def __contains__(self, item):
-        return _binary(self, _to_norm(item), operator.__contains__)
+    def __eq__(self, other):
+        return _binary(self, norm(other), operator.__eq__)
+
+    def __ne__(self, other):
+        return _binary(self, norm(other), operator.__ne__)
+
+    def __lt__(self, other):
+        return _binary(self, norm(other), operator.__lt__)
+
+    def __le__(self, other):
+        return _binary(self, norm(other), operator.__le__)
+
+    def __gt__(self, other):
+        return _binary(self, norm(other), operator.__gt__)
+
+    def __ge__(self, other):
+        return _binary(self, norm(other), operator.__ge__)
 
     def __abs__(self):
         return _unary(self, operator.__abs__)
@@ -149,7 +159,7 @@ class INormClass:
 
 
 def lnot(a) -> INormClass:
-    return _unary(_to_norm(a), lambda x: not x)
+    return _unary(norm(a), lambda x: not x)
 
 
 def _reducing(func: Callable[[Any, Any], Any]):
@@ -164,17 +174,17 @@ def _reducing(func: Callable[[Any, Any], Any]):
 
 @_reducing
 def land(a, b) -> INormClass:
-    return _binary(_to_norm(a), _to_norm(b), lambda x, y: x and y)
+    return _binary(norm(a), norm(b), lambda x, y: x and y)
 
 
 @_reducing
 def lor(a, b) -> INormClass:
-    return _binary(_to_norm(a), _to_norm(b), lambda x, y: x or y)
+    return _binary(norm(a), norm(b), lambda x, y: x or y)
 
 
 def lfunc(func, *args, **kwargs) -> INormClass:
-    args = [_to_norm(item) for item in args]
-    kwargs = [(_to_norm(key), _to_norm(value)) for key, value in kwargs.items()]
+    args = [norm(item) for item in args]
+    kwargs = [(norm(key), norm(value)) for key, value in kwargs.items()]
 
     def _call(v):
         _args = [item(v) for item in args]
@@ -182,3 +192,15 @@ def lfunc(func, *args, **kwargs) -> INormClass:
         return func(*_args, **_kwargs)
 
     return _callable_to_norm(_call)
+
+
+def lin(a, b) -> INormClass:
+    return _binary(norm(a), norm(b), operator.__contains__)
+
+
+def lis(a, b) -> INormClass:
+    return _binary(norm(a), norm(b), operator.is_)
+
+
+def lisnot(a, b) -> INormClass:
+    return _binary(norm(a), norm(b), operator.is_not)
---
+++ nervex/config/test/loader/test_norm.py
@@ -0,0 +1,220 @@
+import warnings
+
+import pytest
+
+from ...loader.mapping import index
+from ...loader.norm import norm, lin, lis, lisnot
+
+
+@pytest.mark.unittest
+class TestConfigLoaderNorm:
+
+    def test_norm_general(self):
+        _norm = norm(index('a')) * (norm(index('b')) - norm(index('c'))) * 2 == norm(index('result') | index('sum'))
+
+        assert not _norm({'a': 2, 'b': 10, 'c': -2, 'result': 1})
+        assert _norm({'a': 2, 'b': 10, 'c': -2, 'result': 48})
+        with pytest.raises(KeyError):
+            _norm({'a': 2, 'b': 10, 'cc': -2, 'result': 24})
+
+        assert not _norm({'a': 2, 'b': 10, 'c': -2, 'sum': 1})
+        assert _norm({'a': 2, 'b': 10, 'c': -2, 'result_': 23, 'sum': 48})
+
+    def test_add(self):
+        _norm = norm(index('a')) + 2
+        assert _norm({'a': 2}) == 4
+
+        _norm = 3 + norm(index('a'))
+        assert _norm({'a': 2}) == 5
+
+        _norm = norm(index('a')) + norm(index('b'))
+        assert _norm({'a': 2, 'b': 4}) == 6
+
+    def test_sub(self):
+        _norm = norm(index('a')) - 2
+        assert _norm({'a': 2}) == 0
+
+        _norm = 3 - norm(index('a'))
+        assert _norm({'a': 2}) == 1
+
+        _norm = norm(index('a')) - norm(index('b'))
+        assert _norm({'a': 2, 'b': 4}) == -2
+
+    def test_mul(self):
+        _norm = norm(index('a')) * 2
+        assert _norm({'a': 2}) == 4
+
+        _norm = 3 * norm(index('a'))
+        assert _norm({'a': 2}) == 6
+
+        _norm = norm(index('a')) * norm(index('b'))
+        assert _norm({'a': 2, 'b': 4}) == 8
+
+    def test_matmul(self):
+        # TODO: complete this part
+        warnings.warn('Testing of matmul for norm not implemented.')
+
+    def test_truediv(self):
+        _norm = norm(index('a')) / 2
+        assert _norm({'a': 3}) == 1.5
+
+        _norm = 3 / norm(index('a'))
+        assert _norm({'a': 2}) == 1.5
+
+        _norm = norm(index('a')) / norm(index('b'))
+        assert _norm({'a': 2.1, 'b': 4.2}) == 0.5
+
+    def test_floordiv(self):
+        _norm = norm(index('a')) // 2
+        assert _norm({'a': 3}) == 1
+
+        _norm = 3 // norm(index('a'))
+        assert _norm({'a': 2}) == 1
+
+        _norm = norm(index('a')) // norm(index('b'))
+        assert _norm({'a': 10.5, 'b': 4.2}) == 2
+
+    def test_mod(self):
+        _norm = norm(index('a')) % 3
+        assert _norm({'a': 2}) == 2
+        assert _norm({'a': 4}) == 1
+
+        _norm = 4 % norm(index('a'))
+        assert _norm({'a': 2}) == 0
+        assert _norm({'a': 3}) == 1
+
+        _norm = norm(index('a')) % norm(index('b'))
+        assert _norm({'a': 3, 'b': 2}) == 1
+        assert _norm({'a': 5, 'b': 3}) == 2
+
+    def test_pow(self):
+        _norm = norm(index('a')) ** 3
+        assert _norm({'a': 2}) == 8
+        assert _norm({'a': 4}) == 64
+
+        _norm = 4 ** norm(index('a'))
+        assert _norm({'a': 2}) == 16
+        assert _norm({'a': 3}) == 64
+
+        _norm = norm(index('a')) ** norm(index('b'))
+        assert _norm({'a': 3, 'b': 2}) == 9
+        assert _norm({'a': 5, 'b': 3}) == 125
+
+    def test_lshift(self):
+        _norm = norm(index('a')) << 3
+        assert _norm({'a': 2}) == 16
+        assert _norm({'a': 4}) == 32
+
+        _norm = 4 << norm(index('a'))
+        assert _norm({'a': 2}) == 16
+        assert _norm({'a': 3}) == 32
+
+        _norm = norm(index('a')) << norm(index('b'))
+        assert _norm({'a': 3, 'b': 2}) == 12
+        assert _norm({'a': 5, 'b': 3}) == 40
+
+    def test_rshift(self):
+        _norm = norm(index('a')) >> 3
+        assert _norm({'a': 283}) == 35
+        assert _norm({'a': 47}) == 5
+
+        _norm = 47 >> norm(index('a'))
+        assert _norm({'a': 2}) == 11
+        assert _norm({'a': 3}) == 5
+
+        _norm = norm(index('a')) >> norm(index('b'))
+        assert _norm({'a': 37, 'b': 2}) == 9
+        assert _norm({'a': 529, 'b': 5}) == 16
+
+    def test_and(self):
+        _norm = norm(index('a')) & 9
+        assert _norm({'a': 15}) == 9
+        assert _norm({'a': 1}) == 1
+
+        _norm = 11 & norm(index('a'))
+        assert _norm({'a': 15}) == 11
+        assert _norm({'a': 7}) == 3
+
+        _norm = norm(index('a')) & norm(index('b'))
+        assert _norm({'a': 15, 'b': 11}) == 11
+        assert _norm({'a': 9, 'b': 1}) == 1
+
+    def test_or(self):
+        _norm = norm(index('a')) | 9
+        assert _norm({'a': 15}) == 15
+        assert _norm({'a': 83}) == 91
+
+        _norm = 11 | norm(index('a'))
+        assert _norm({'a': 15}) == 15
+        assert _norm({'a': 17}) == 27
+
+        _norm = norm(index('a')) | norm(index('b'))
+        assert _norm({'a': 5, 'b': 11}) == 15
+        assert _norm({'a': 9, 'b': 3}) == 11
+
+    def test_xor(self):
+        _norm = norm(index('a')) ^ 9
+        assert _norm({'a': 15}) == 6
+        assert _norm({'a': 83}) == 90
+
+        _norm = 11 ^ norm(index('a'))
+        assert _norm({'a': 15}) == 4
+        assert _norm({'a': 17}) == 26
+
+        _norm = norm(index('a')) ^ norm(index('b'))
+        assert _norm({'a': 5, 'b': 11}) == 14
+        assert _norm({'a': 9, 'b': 3}) == 10
+
+    def test_invert(self):
+        _norm = ~norm(index('a'))
+        assert _norm({'a': 15}) == -16
+        assert _norm({'a': -2348}) == 2347
+
+    def test_pos(self):
+        _norm = +norm(index('a'))
+        assert _norm({'a': 15}) == 15
+        assert _norm({'a': -2348}) == -2348
+
+    def test_neg(self):
+        _norm = -norm(index('a'))
+        assert _norm({'a': 15}) == -15
+        assert _norm({'a': -2348}) == 2348
+
+    def test_lin(self):
+        _norm = lin(norm(index('a')), 'string')
+        assert _norm({'a': ['string', 1, 2]})
+        assert not _norm({'a': ['strng', 1, 2]})
+
+        _norm = lin([1, 2, 3], norm(index('a')))
+        assert _norm({'a': 1})
+        assert not _norm({'a': 4})
+
+        _norm = lin(norm(index('a')), norm(index('b')))
+        assert _norm({'a': [1, 2], 'b': 1})
+        assert not _norm({'a': [1, 2], 'b': 3})
+
+    def test_lis(self):
+        _norm = lis(norm(index('a')), 'string')
+        assert _norm({'a': 'string'})
+        assert not _norm({'a': ['strng', 1, 2]})
+
+        _norm = lis(None, norm(index('a')))
+        assert _norm({'a': None})
+        assert not _norm({'a': 4})
+
+        _norm = lis(norm(index('a')), norm(index('b')))
+        assert _norm({'a': 1, 'b': 1})
+        assert not _norm({'a': [1, 2], 'b': 3})
+
+    def test_lisnot(self):
+        _norm = lisnot(norm(index('a')), 'string')
+        assert not _norm({'a': 'string'})
+        assert _norm({'a': ['strng', 1, 2]})
+
+        _norm = lisnot(None, norm(index('a')))
+        assert not _norm({'a': None})
+        assert _norm({'a': 4})
+
+        _norm = lisnot(norm(index('a')), norm(index('b')))
+        assert not _norm({'a': 1, 'b': 1})
+        assert _norm({'a': [1, 2], 'b': 3})
