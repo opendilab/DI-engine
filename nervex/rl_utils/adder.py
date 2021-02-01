dc4006263f1dfb898e2baacfaed4dd59f013e498
niuyazhe <niuyazhe@sensetime.com>
2021-02-01 22:53:27 +0800

    add deepcopy for all type data in nstep data

--- nervex/rl_utils/adder.py
+++ nervex/rl_utils/adder.py
@@ -146,7 +146,7 @@ class Adder(object):
             return data
         if traj_len == float('inf') or len(data) < traj_len:
             # episode done case, append nstep fake datas
-            fake_data = {'obs': data[-1]['obs'].clone(), 'reward': torch.zeros(1), 'done': True}
+            fake_data = {'obs': copy.deepcopy(data[-1]['obs']), 'reward': torch.zeros(1), 'done': True}
             data += [fake_data for _ in range(nstep)]
         for i in range(len(data) - nstep):
             # update keys ['next_obs', 'reward', 'done'] with their n-step value
