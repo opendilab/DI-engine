feb3104602cd98b9adb0e3d7305072e301884625
niuyazhe <niuyazhe@sensetime.com>
2021-02-04 17:51:30 +0800

    remove app_zoo old code

--- app_zoo/atari/worker/__init__.py
+++
@@ -1,2 +0,0 @@
-from .atari_dqn_learner import AtariDqnLearner
-from .atari_ac_learner import AtariPpoLearner, AtariA2CLearner
--- app_zoo/mujoco/worker/__init__.py
+++
@@ -1,2 +0,0 @@
-from .mujoco_dqn_learner import MujocoDqnLearner
-from .mujoco_ppo_learner import MujocoPpoLearner
