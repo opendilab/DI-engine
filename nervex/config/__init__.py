26ec57f1a559b7061bf17df2143b6d94caed3c53
niuyazhe <niuyazhe@sensetime.com>
2021-02-06 17:33:12 +0800

    move loader dir and add the whole cartpole dqn config loader

--- nervex/config/__init__.py
+++ nervex/config/__init__.py
@@ -1,7 +1,6 @@
 from .buffer_manager import buffer_manager_default_config
 from .config import Config
 from .league import solo_league_default_config
-from .loader import *
 from .parallel import parallel_local_default_config, coordinator_default_config
 from .serial import base_learner_default_config
 from .utils import parallel_transform, parallel_transform_slurm
