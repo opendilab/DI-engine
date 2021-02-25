97b0496054c7fe34b1bb01502013266ed3008c1b
niuyazhe <niuyazhe@sensetime.com>
2021-02-25 23:59:25 +0800

    fix __version__ bug and add qa11 for install

--- nervex/__init__.py
+++ nervex/__init__.py
@@ -3,5 +3,6 @@ __VERSION__ = 'v0.1.0-release'
 __DESCRIPTION__ = 'X-Lab DRL Framework'
 __AUTHOR__ = "X-Lab"
 __AUTHOR_EMAIL__ = "niuyazhe@sensetime.com"
+__version__ = __VERSION__
 
 enable_hpc_rl = False
--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ with open(os.path.join(here, 'nervex', '__init__.py'), 'r') as f:
 setup(
     name=meta['__TITLE__'],
     version=meta['__VERSION__'],
-    description=['__DESCRIPTION__'],
-    author=['__AUTHOR__'],
+    description=meta['__DESCRIPTION__'],
+    author=meta['__AUTHOR__'],
     license='Apache License, Version 2.0',
     keywords='DRL Framework',
     packages=[
