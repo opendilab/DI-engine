54b5301827c69bc2d38bfea64383c2ff789a90be
zhangyinmin <zhangyinmin@sensetime.com>
2021-02-28 14:56:15 +0800

    add procgen env benchmark

---
+++ app_zoo/procgen/coinrun/entry/__init__.py
@@ -0,0 +1,2 @@
+from .coinrun_dqn_default_config import coinrun_dqn_default_config
+from .coinrun_ppo_default_config import coinrun_ppo_default_config
---
+++ app_zoo/procgen/starpilot/entry/starpilot_ppo_default_config.py
@@ -0,0 +1,81 @@
+from easydict import EasyDict
+
+traj_len = 6
+starpilot_ppo_default_config = dict(
+    env=dict(
+        env_manager_type='subprocess',
+        import_names=['app_zoo.procgen.starpilot.envs.starpilot_env'],
+        env_type='starpilot',
+        # frame_stack=4,
+        is_train=True,
+        actor_env_num=16,
+        evaluator_env_num=8,
+    ),
+    policy=dict(
+        use_cuda=False,
+        policy_type='ppo',
+        import_names=['nervex.policy.ppo'],
+        on_policy=False,
+        model=dict(
+            model_type='conv_vac',
+            import_names=['nervex.model.actor_critic'],
+            obs_dim=[3, 64, 64],
+            action_dim=1,
+            embedding_dim=64,
+        ),
+        learn=dict(
+            train_step=5,
+            batch_size=64,
+            learning_rate=0.001,
+            weight_decay=0.0001,
+            algo=dict(
+                value_weight=0.5,
+                entropy_weight=0.01,
+                clip_ratio=0.2,
+            ),
+        ),
+        collect=dict(
+            traj_len=traj_len,
+            unroll_len=1,
+            algo=dict(
+                discount_factor=0.9,
+                gae_lambda=0.95,
+            ),
+        ),
+        command=dict(),
+    ),
+    replay_buffer=dict(
+        buffer_name=['agent'],
+        agent=dict(
+            meta_maxlen=1000,
+            max_reuse=100,
+            min_sample_ratio=1,
+        ),
+    ),
+    actor=dict(
+        n_sample=16,
+        traj_len=traj_len,
+        traj_print_freq=100,
+        collect_print_freq=100,
+    ),
+    evaluator=dict(
+        n_episode=5,
+        eval_freq=200,
+        stop_val=195,
+    ),
+    learner=dict(
+        load_path='',
+        hook=dict(
+            log_show=dict(
+                name='log_show',
+                type='log_show',
+                priority=20,
+                position='after_iter',
+                ext_args=dict(freq=100, ),
+            ),
+        ),
+    ),
+    commander=dict(),
+)
+starpilot_ppo_default_config = EasyDict(starpilot_ppo_default_config)
+main_config = starpilot_ppo_default_config
