744ec1d86bb89d23e98cf8137fb5cce8dca0b704
谷心洋 <guxinyang@CN3614000202M.local>
2021-03-11 13:36:38 +0800

    add lunarlander for testing purpose, a recommend workflow is testing on CartPole->LunarLander->Pong->Breakout->Seaquest

---
+++ app_zoo/classic_control/lunarlander/envs/lunarlander_env.py
@@ -0,0 +1,96 @@
+from typing import Any, List, Union, Optional
+import time
+import gym
+import torch
+import numpy as np
+from nervex.envs import BaseEnv, register_env, BaseEnvTimestep, BaseEnvInfo
+from nervex.envs.common.env_element import EnvElement, EnvElementInfo
+from nervex.torch_utils import to_tensor, to_ndarray, to_list
+
+
+def disable_gym_view_window():
+    from gym.envs.classic_control import rendering
+    import pyglet
+
+    def get_window(width, height, display):
+        screen = display.get_screens()
+        config = screen[0].get_best_config()
+        context = config.create_context(None)
+        return pyglet.window.Window(
+            width=width, height=height, display=display, config=config, context=context, visible=False
+        )
+
+    rendering.get_window = get_window
+
+
+class LunarLanderEnv(BaseEnv):
+
+    def __init__(self, cfg: dict) -> None:
+        self._cfg = cfg
+        self._env = gym.make('LunarLander-v2')
+
+    def reset(self) -> torch.Tensor:
+        if hasattr(self, '_seed'):
+            np_seed = 100 * np.random.randint(1,1000)
+            self._env.seed(self._seed + np_seed)
+        self._final_eval_reward = 0
+        obs = self._env.reset()
+        obs = to_ndarray(obs)
+        return obs
+
+    def close(self) -> None:
+        self._env.close()
+
+    def seed(self, seed: int) -> None:
+        self._seed = seed
+        np.random.seed(self._seed)
+
+    def step(self, action: np.ndarray) -> BaseEnvTimestep:
+        assert isinstance(action, np.ndarray), type(action)
+        if action.shape == (1, ):
+            action = action.squeeze()  # 0-dim tensor
+        obs, rew, done, info = self._env.step(action)
+        self._final_eval_reward += rew
+        if done:
+            info['final_eval_reward'] = self._final_eval_reward
+        obs = to_ndarray(obs)
+        rew = to_ndarray([rew])  # wrapped to be transfered to a Tensor with shape (1,)
+        return BaseEnvTimestep(obs, rew, done, info)
+
+    def info(self) -> BaseEnvInfo:
+        T = EnvElementInfo
+        return BaseEnvInfo(
+            agent_num=1,
+            obs_space=T(
+                (8, ), {
+                    'min': [float("-inf")] * 8,
+                    'max': [float("inf")] * 8,
+                    'dtype': float,
+                }, None, None
+            ),
+            # [min, max)
+            act_space=T((4, ), {
+                'min': 0,
+                'max': 4
+            }, None, None),
+            rew_space=T((1, ), {
+                'min': -1000,
+                'max': 1000
+            }, None, None),
+        )
+
+    def __repr__(self) -> str:
+        return "nerveX CartPole Env"
+
+    def enable_save_replay(self, replay_path: Optional[str] = None) -> None:
+        if replay_path is None:
+            replay_path = './video'
+        self._replay_path = replay_path
+        # this function can lead to the meaningless result
+        # disable_gym_view_window()
+        self._env = gym.wrappers.Monitor(
+            self._env, self._replay_path, video_callable=lambda episode_id: True, force=True
+        )
+
+
+register_env('lunarlander', LunarLanderEnv)
