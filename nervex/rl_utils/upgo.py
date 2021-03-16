7e6392732d9d8ddcc217f8eb361cc02085f84b75
zlx-sensetime <zhaoliangxuan@sensetime.com>
2021-03-16 10:40:02 +0800

    add upgo unittest; fix typing bug in nn test helper

--- nervex/rl_utils/upgo.py
+++ nervex/rl_utils/upgo.py
@@ -28,17 +28,18 @@ def tb_cross_entropy(logit, label, mask=None):
     return ce
 
 
-def upgo_returns(rewards, bootstrap_values):
+def upgo_returns(rewards: torch.Tensor, bootstrap_values: torch.Tensor) -> torch.Tensor:
     r"""
     Overview:
         Computing UPGO return targets. Also notice there is no special handling for the terminal state.
     Arguments:
-        - rewards (:obj:`torch.Tensor`): the returns from time step 0 to T-1, of size [T_traj, batchsize]
-        - bootstrap_values (:obj:`torch.Tensor`):
-          estimation of the state value at step 0 to T, of size [T_traj+1, batchsize]
+        - rewards (:obj:`torch.Tensor`): the returns from time step 0 to T-1, \
+            of size [T_traj, batchsize]
+        - bootstrap_values (:obj:`torch.Tensor`): estimation of the state value at step 0 to T, \
+            of size [T_traj+1, batchsize]
     Returns:
-        - ret (:obj:`torch.Tensor`): Computed lambda return value
-          for each state from 0 to T-1, of size [T_traj, batchsize]
+        - ret (:obj:`torch.Tensor`): Computed lambda return value for each state from 0 to T-1, \
+            of size [T_traj, batchsize]
     """
     # UPGO can be viewed as a lambda return! The trace continues for V_t (i.e. lambda = 1.0) if r_tp1 + V_tp2 > V_tp1.
     # as the lambdas[-1, :] is ignored in generalized_lambda_returns, we don't care about bootstrap_values_tp2[-1]
@@ -47,19 +48,19 @@ def upgo_returns(rewards, bootstrap_values):
     return generalized_lambda_returns(bootstrap_values, rewards, 1.0, lambdas)
 
 
-def upgo_loss(target_output, rhos, action, rewards, bootstrap_values, mask=None):
+def upgo_loss(target_output: torch.Tensor, rhos: torch.Tensor, action: torch.Tensor, rewards: torch.Tensor, bootstrap_values: torch.Tensor, mask=None) -> torch.Tensor:
     r"""
     Overview:
         Computing UPGO loss given constant gamma and lambda. There is no special handling for terminal state value,
         if the last state in trajectory is the terminal, just pass a 0 as bootstrap_terminal_value.
     Arguments:
-        - target_output (:obj:`torch.Tensor`): the output computed by the target policy network,
-          of size [T_traj, batchsize, n_output]
+        - target_output (:obj:`torch.Tensor`): the output computed by the target policy network, \
+            of size [T_traj, batchsize, n_output]
         - rhos (:obj:`torch.Tensor`): the importance sampling ratio, of size [T_traj, batchsize]
         - action (:obj:`torch.Tensor`): the action taken, of size [T_traj, batchsize]
         - rewards (:obj:`torch.Tensor`): the returns from time step 0 to T-1, of size [T_traj, batchsize]
-        - bootstrap_values (:obj:`torch.Tensor`): estimation of the state value at step 0 to T,
-          of size [T_traj+1, batchsize]
+        - bootstrap_values (:obj:`torch.Tensor`): estimation of the state value at step 0 to T, \
+            of size [T_traj+1, batchsize]
     Returns:
         - loss (:obj:`torch.Tensor`): Computed importance sampled UPGO loss, averaged over the samples, of size []
     """
