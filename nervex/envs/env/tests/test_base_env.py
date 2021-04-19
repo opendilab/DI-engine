c854a06a37994e70950336e273b69a8865df93b2
zhangming <zhangming@sensetime.com>
2021-04-19 19:35:23 +0800

    fix nervex env wrapepr bug

--- nervex/envs/env/tests/test_base_env.py
+++ nervex/envs/env/tests/test_base_env.py
@@ -1,10 +1,12 @@
 import random
-
+import gym
 import numpy as np
 import pytest
 import torch
+from easydict import EasyDict
 
-from nervex.envs.env.base_env import NervexEnvWrapper
+from nervex.envs.env.nervex_env_wrapper import NervexEnvWrapper
+
 
 @pytest.mark.unittest
 class TestNervexEnvWrapper:
@@ -14,10 +16,14 @@ class TestNervexEnvWrapper:
         nervex_env = NervexEnvWrapper(env)
         info = nervex_env.info()
         assert info is not None
-        l1 = nervex_env.create_actor_env_cfg()
+        cfg = EasyDict(dict(
+            actor_env_num=16,
+            evaluator_env_num=3,
+            is_train=True,
+        ))
+        l1 = nervex_env.create_actor_env_cfg(cfg)
         assert isinstance(l1, list)
-        l1 = nervex_env.create_evaluator_env_cfg()
+        l1 = nervex_env.create_evaluator_env_cfg(cfg)
         assert isinstance(l1, list)
         obs = nervex_env.reset()
         assert isinstance(obs, np.ndarray)
-
