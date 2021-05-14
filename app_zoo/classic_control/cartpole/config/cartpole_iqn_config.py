4148005d7af381726424f379481e8d4bbdb03d1d
niuyazhe <niuyazhe@sensetime.com>
2021-05-14 16:54:18 +0800

    adapt il and format unittest config

--- app_zoo/classic_control/cartpole/config/cartpole_iqn_config.py
+++ app_zoo/classic_control/cartpole/config/cartpole_iqn_config.py
@@ -38,10 +38,7 @@ cartpole_iqn_config = dict(
                 start=0.95,
                 end=0.1,
                 decay=10000,
-            ),
-            replay_buffer=dict(
-                replay_buffer_size=20000,
-            )
+            ), replay_buffer=dict(replay_buffer_size=20000, )
         ),
     ),
 )
