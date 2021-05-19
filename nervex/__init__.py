f9d1b0c3fcab704745f551fe5cdd97a1f1a68c7c
niuyazhe <niuyazhe@sensetime.com>
2021-05-20 01:26:24 +0800

    v0.2.0b1

--- nervex/__init__.py
+++ nervex/__init__.py
@@ -1,5 +1,5 @@
 __TITLE__ = 'nerveX'
-__VERSION__ = 'v0.2.0-b0'
+__VERSION__ = 'v0.2.0-b1'
 __DESCRIPTION__ = 'X-Lab DRL Framework'
 __AUTHOR__ = "X-Lab"
 __AUTHOR_EMAIL__ = "niuyazhe@sensetime.com"
