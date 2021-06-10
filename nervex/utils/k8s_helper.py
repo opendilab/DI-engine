54c7a1e2b08061babdfedc6e3bdffe88b665dd24
lijianwen <lijianwen@sensetime.com>
2021-06-10 15:51:42 +0800

    get server url from config or env vars

--- nervex/utils/k8s_helper.py
+++ nervex/utils/k8s_helper.py
@@ -9,8 +9,8 @@ DEFAULT_POD_NAME = 'nervexjob-example-coordinator'
 def get_operator_server_kwargs(cfg: EasyDict) -> dict:
     namespace = os.environ.get('KUBERNETES_POD_NAMESPACE', DEFAULT_NAMESPACE)
     name = os.environ.get('KUBERNETES_POD_NAME', DEFAULT_POD_NAME)
-    url = os.environ.get('NERVEX_SERVER_URL', None)
-    assert url, 'please set environment variable NERVEX_SERVER_URL in Kubenetes platform.'
+    url = cfg.get('system_addr', None) or os.environ.get('KUBERNETES_NERVEX_SERVER_URL', None)
+    assert url, 'please set environment variable KUBERNETES_NERVEX_SERVER_URL in Kubenetes platform.'
     host, port, _, _ = split_http_address(url)
     return {
         'api_version': cfg.api_version,
