17825a2b2fdf13879ac71f87af0e2ccca088fc3c
niuyazhe <niuyazhe@sensetime.com>
2021-06-25 18:25:54 +0800

    add sac qac model

--- nervex/model/template/qac.py
+++ nervex/model/template/qac.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 
 from nervex.utils import SequenceType, squeeze
-from ..common import RegressionHead
+from ..common import RegressionHead, ReparameterizationHead
 
 
 class QAC(nn.Module):
@@ -13,6 +13,7 @@ class QAC(nn.Module):
             self,
             obs_shape: Union[int, SequenceType],
             action_shape: Union[int, SequenceType],
+            actor_head_type: str,
             twin_critic: bool = False,
             actor_head_hidden_size: int = 64,
             actor_head_layer_num: int = 1,
@@ -24,17 +25,32 @@ class QAC(nn.Module):
         super(QAC, self).__init__()
         obs_shape: int = squeeze(obs_shape)
         action_shape: int = squeeze(action_shape)
-        self.actor = nn.Sequential(
-            nn.Linear(obs_shape, actor_head_hidden_size), activation,
-            RegressionHead(
-                actor_head_hidden_size,
-                action_shape,
-                actor_head_layer_num,
-                final_tanh=True,
-                activation=activation,
-                norm_type=norm_type
+        self.actor_head_type = actor_head_type
+        assert self.actor_head_type in ['regression', 'reparameterization']
+        if self.actor_head_type == 'regression':
+            self.actor = nn.Sequential(
+                nn.Linear(obs_shape, actor_head_hidden_size), activation,
+                RegressionHead(
+                    actor_head_hidden_size,
+                    action_shape,
+                    actor_head_layer_num,
+                    final_tanh=True,
+                    activation=activation,
+                    norm_type=norm_type
+                )
             )
-        )
+        elif self.actor_head_type == 'reparameterization':
+            self.actor = nn.Sequential(
+                nn.Linear(obs_shape, actor_head_hidden_size), activation,
+                ReparameterizationHead(
+                    actor_head_hidden_size,
+                    action_shape,
+                    actor_head_layer_num,
+                    sigma_type='conditioned',
+                    activation=activation,
+                    norm_type=norm_type
+                )
+            )
         self.twin_critic = twin_critic
         if self.twin_critic:
             self.critic = nn.ModuleList()
@@ -72,10 +88,13 @@ class QAC(nn.Module):
     def compute_actor(self, inputs: torch.Tensor) -> Dict:
         """
         ReturnsKeys:
-            - necessary: ``action``
+            - optional: ``action``, ``logit``
         """
         x = self.actor(inputs)
-        return {'action': x['pred']}
+        if self.actor_head_type == 'regression':
+            return {'action': x['pred']}
+        elif self.actor_head_type == 'reparameterization':
+            return {'logit': [x['mu'], x['sigma']]}
 
     def compute_critic(self, inputs: Dict) -> Dict:
         """
--- nervex/model/template/tests/test_qac.py
+++ nervex/model/template/tests/test_qac.py
@@ -13,27 +13,20 @@ embedding_size = 32
 action_shape_args = [(6, ), [
     1,
 ]]
-args = list(product(*[action_shape_args, [True, False]]))
+args = list(product(*[action_shape_args, [True, False], ['regression', 'reparameterization']]))
 
 
 @pytest.mark.unittest
-@pytest.mark.parametrize('action_shape, twin', args)
+@pytest.mark.parametrize('action_shape, twin, actor_head_type', args)
 class TestQAC:
 
-    @staticmethod
-    def output_check(action_shape, models, outputs):
-        if isinstance(action_shape, tuple) or isinstance(action_shape, list):
-            loss = sum([t.sum() for t in outputs])
-        elif np.isscalar(action_shape):
-            loss = outputs.sum()
-        is_differentiable(loss, models)
-
-    def test_fcqac(self, action_shape, twin):
+    def test_fcqac(self, action_shape, twin, actor_head_type):
         N = 32
         inputs = {'obs': torch.randn(B, N), 'action': torch.randn(B, squeeze(action_shape))}
         model = QAC(
             obs_shape=(N, ),
             action_shape=action_shape,
+            actor_head_type=actor_head_type,
             critic_head_hidden_size=embedding_size,
             actor_head_hidden_size=embedding_size,
             twin_critic=twin,
@@ -41,15 +34,23 @@ class TestQAC:
         # compute_q
         q = model(inputs, mode='compute_critic')['q_value']
         if twin:
-            self.output_check(action_shape, model.critic[0], q[0])
-            self.output_check(action_shape, model.critic[1], q[1])
+            is_differentiable(q[0].sum(), model.critic[0])
+            is_differentiable(q[1].sum(), model.critic[1])
         else:
-            self.output_check(action_shape, model.critic, q)
+            is_differentiable(q.sum(), model.critic)
 
         # compute_action
-        action = model(inputs['obs'], mode='compute_actor')['action']
-        if squeeze(action_shape) == 1:
-            assert action.shape == (B, )
-        else:
-            assert action.shape == (B, squeeze(action_shape))
-        assert action.eq(action.clamp(-1, 1)).all()
+        print(model)
+        if actor_head_type == 'regression':
+            action = model(inputs['obs'], mode='compute_actor')['action']
+            if squeeze(action_shape) == 1:
+                assert action.shape == (B, )
+            else:
+                assert action.shape == (B, squeeze(action_shape))
+            assert action.eq(action.clamp(-1, 1)).all()
+            is_differentiable(action.sum(), model.actor)
+        elif actor_head_type == 'reparameterization':
+            (mu, sigma) = model(inputs['obs'], mode='compute_actor')['logit']
+            assert mu.shape == (B, *action_shape)
+            assert sigma.shape == (B, *action_shape)
+            is_differentiable(mu.sum() + sigma.sum(), model.actor)
