6c586b65156256995d99063d2e4050b3f3cec164
niuyazhe <niuyazhe@sensetime.com>
2021-06-27 01:30:10 +0800

    remove smac experiment code

--- app_zoo/smac/model_zoo/experiment/3s5z/qmix/run.sh
+++
@@ -1,3 +0,0 @@
-srun --mpi=pmi2 --job-name 3s5z_qmix -p VI_SP_Y_V100_A -n1 --gres=gpu:1 --ntasks-per-node=8 python -u train.py
-# spring.submit run --mpi=pmi2 --job-name qmix_12_8_3_1 -n 1 --gpu  --cpus-per-task=5 \
-# "python -u train.py"
