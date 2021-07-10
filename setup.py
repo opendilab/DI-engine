5adf80021e8b0f646a46219627ead273bedc801a
niuyazhe <niuyazhe@sensetime.com>
2021-07-11 01:14:03 +0800

    hotfix(nyz): fix subprocess env manager state transition bug and exec usage in setup.py

--- setup.py
+++ setup.py
@@ -18,11 +18,11 @@ from __future__ import print_function
 import os
 
 from setuptools import setup, find_packages
+from importlib import import_module
 
 here = os.path.abspath(os.path.dirname(__file__))
-meta = {}
-with open(os.path.join(here, 'ding', '__init__.py'), 'r') as f:
-    exec(f.read(), meta)
+meta_module = import_module('ding')
+meta = meta_module.__dict__
 
 setup(
     name=meta['__TITLE__'],
