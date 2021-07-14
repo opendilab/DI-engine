e30a3d3c1f5b753626bf20c1ce63c7eca2bf5cff
zhangyinmin <yisraelzhang@gmail.com>
2021-07-14 20:16:59 +0800

    modify the gae recomputation; add/update ppo config/entry.

--- dizoo/mujoco/config/hopper_ppo_default_config.py
+++ dizoo/mujoco/config/hopper_ppo_default_config.py
@@ -5,7 +5,7 @@ hopper_ppo_default_config = dict(
         env_id='Hopper-v3',
         norm_obs=dict(use_norm=False, ),
         norm_reward=dict(use_norm=False, ),
-        collector_env_num=64,
+        collector_env_num=8,
         evaluator_env_num=10,
         use_act_scale=True,
         n_evaluator_episode=10,
@@ -14,6 +14,7 @@ hopper_ppo_default_config = dict(
     policy=dict(
         cuda=True,
         on_policy=True,
+        recompute_adv=True,
         model=dict(
             obs_shape=11,
             action_shape=3,
@@ -28,7 +29,7 @@ hopper_ppo_default_config = dict(
             entropy_weight=0.0,
             clip_ratio=0.2,
             adv_norm=True,
-            recompute_adv=True,
+            value_norm=True,
         ),
         collect=dict(
             n_sample=2048,
