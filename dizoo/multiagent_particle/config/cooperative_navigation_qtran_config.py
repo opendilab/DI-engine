98e2c133c4b0acaee71bcd5abb127b8ab553fc9c
Jie Liu <ljie@buaa.edu.cn>
2021-08-23 14:53:49 +0800

    Dev modified predator prey (#30)
    * add modifiled predator_prey env
    
    * add collision_ratio
    
    * add readme and cfg for modified_predator_prey env
    
    * add readme imgs for modified_predator_prey
    
    * check format
    
    * fix format

--- dizoo/multiagent_particle/config/cooperative_navigation_qtran_config.py
+++ dizoo/multiagent_particle/config/cooperative_navigation_qtran_config.py
@@ -1,10 +1,12 @@
+from copy import deepcopy
+from ding.entry import serial_pipeline
 from easydict import EasyDict
 
 n_agent = 5
 collector_env_num = 4
 evaluator_env_num = 2
 num_landmarks = n_agent
-cooperative_navigation_qtran_config = dict(
+main_config = dict(
     env=dict(
         num_landmarks=num_landmarks,
         max_step=100,
@@ -16,23 +18,26 @@ cooperative_navigation_qtran_config = dict(
         stop_value=0,
     ),
     policy=dict(
-        cuda=False,
-        on_policy=False,
         model=dict(
             agent_num=n_agent,
             obs_shape=2 + 2 + (n_agent - 1) * 2 + num_landmarks * 2,
             global_obs_shape=n_agent * 2 + num_landmarks * 2 + n_agent * 2,
             action_shape=5,
-            hidden_size_list=[128, 128, 64],
+            hidden_size_list=[128],
             embedding_size=64,
+            lstm_type='gru',
+            dueling=False,
         ),
-        agent_num=n_agent,
         learn=dict(
             update_per_collect=100,
             batch_size=32,
             learning_rate=0.0005,
+            double_q=True,
             target_update_theta=0.001,
             discount_factor=0.99,
+            td_weight=1,
+            opt_weight=0.1,
+            nopt_min_weight=0.0001,
         ),
         collect=dict(
             n_sample=600,
@@ -40,17 +45,24 @@ cooperative_navigation_qtran_config = dict(
             env_num=collector_env_num,
         ),
         eval=dict(env_num=evaluator_env_num, ),
-        other=dict(eps=dict(
-            type='exp',
-            start=1.0,
-            end=0.05,
-            decay=100000,
-        ), ),
+        other=dict(
+            eps=dict(
+                type='exp',
+                start=1.0,
+                end=0.05,
+                decay=100000,
+            ),
+            replay_buffer=dict(
+                replay_buffer_size=15000,
+                # (int) The maximum reuse times of each data
+                max_reuse=1e+9,
+                max_staleness=1e+9,
+            ),
+        ),
     ),
 )
-cooperative_navigation_qtran_config = EasyDict(cooperative_navigation_qtran_config)
-main_config = cooperative_navigation_qtran_config
-cooperative_navigation_qtran_create_config = dict(
+main_config = EasyDict(main_config)
+create_config = dict(
     env=dict(
         import_names=['dizoo.multiagent_particle.envs.particle_env'],
         type='cooperative_navigation',
@@ -58,5 +70,22 @@ cooperative_navigation_qtran_create_config = dict(
     env_manager=dict(type='subprocess'),
     policy=dict(type='qtran'),
 )
-cooperative_navigation_qtran_create_config = EasyDict(cooperative_navigation_qtran_create_config)
-create_config = cooperative_navigation_qtran_create_config
+create_config = EasyDict(create_config)
+
+cooperative_navigation_qtran_config = main_config
+cooperative_navigation_qtran_create_config = create_config
+
+
+def train(args):
+    config = [main_config, create_config]
+    serial_pipeline(config, seed=args.seed)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--seed', '-s', type=int, default=0)
+    args = parser.parse_args()
+
+    train(args)
