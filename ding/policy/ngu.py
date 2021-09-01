3c981a42687db4b632572902251195a77891c18b
puyuan1996 <2402552459@qq.com>
2021-09-01 15:15:45 +0800

    feature(pu): add action and reward as inputs of q network

--- ding/policy/ngu.py
+++ ding/policy/ngu.py
@@ -174,38 +174,49 @@ class NGUPolicy(Policy):
                 ['main_obs', 'target_obs', 'burnin_obs', 'action', 'reward', 'done', 'weight']
             - data_info (:obj:`dict`): the data info, such as replay_buffer_idx, replay_unique_id
         """
-    
+
         # data preprocess
         data = timestep_collate(data)
         if self._cuda:
             data = to_device(data, self._device)
         bs = self._burnin_step
-        data['weight'] = data.get('weight', [None for _ in range(self._unroll_len_add_burnin_step -self._nstep)])
+        data['weight'] = data.get('weight', [None for _ in range(self._unroll_len_add_burnin_step - self._nstep)])
         ignore_done = self._cfg.learn.ignore_done
 
         if ignore_done:
-            data['done'] = [None for _ in range(self._unroll_len_add_burnin_step -self._nstep)]
+            data['done'] = [None for _ in range(self._unroll_len_add_burnin_step - self._nstep)]
         else:
             # data['done'] = data['done'][bs:self._unroll_len_add_burnin_step ].float()
-            data['done'] = data['done'][bs:self._unroll_len_add_burnin_step -self._nstep].float()
+            data['done'] = data['done'][bs:self._unroll_len_add_burnin_step - self._nstep].float()
 
-        data['action'] = data['action'][bs:self._unroll_len_add_burnin_step -self._nstep]
-        data['reward'] = data['reward'][bs:self._unroll_len_add_burnin_step -self._nstep]
+       
+
+        data['burnin_action'] = data['action'][:bs]
+        data['main_action'] = data['action'][bs:self._unroll_len_add_burnin_step - self._nstep]
+        data['target_action'] = data['action'][bs + self._nstep:]
+
+        data['burnin_reward'] = data['reward'][:bs]
+        data['main_reward'] = data['reward'][bs:self._unroll_len_add_burnin_step - self._nstep]
+        data['target_reward'] = data['reward'][bs + self._nstep:]
+
         # split obs into three parts ['burnin_obs'(0~bs), 'main_obs'(bs~bs+nstep), 'target_obs'(bs+nstep~bss+2nstep)]
-        data['burnin_obs'] = data['obs'][:bs]     
-        data['main_obs'] = data['obs'][bs:self._unroll_len_add_burnin_step -self._nstep]
+        data['burnin_obs'] = data['obs'][:bs]
+        data['main_obs'] = data['obs'][bs:self._unroll_len_add_burnin_step - self._nstep]
         data['target_obs'] = data['obs'][bs + self._nstep:]
 
-        data['burnin_beta'] = data['beta'][:bs]     
-        data['main_beta'] = data['beta'][bs:self._unroll_len_add_burnin_step -self._nstep]
+        data['burnin_beta'] = data['beta'][:bs]
+        data['main_beta'] = data['beta'][bs:self._unroll_len_add_burnin_step - self._nstep]
         data['target_beta'] = data['beta'][bs + self._nstep:]
 
+        # Must be after the previous slicing operation
+        data['action'] = data['action'][bs:self._unroll_len_add_burnin_step - self._nstep]
+        data['reward'] = data['reward'][bs:self._unroll_len_add_burnin_step - self._nstep]
+
         if 'value_gamma' not in data:
-            data['value_gamma'] =  [None for _ in range(self._unroll_len_add_burnin_step-self._nstep)]
+            data['value_gamma'] = [None for _ in range(self._unroll_len_add_burnin_step - self._nstep)]
         else:
-            data['value_gamma'] = data['value_gamma'][bs:self._unroll_len_add_burnin_step-self._nstep]
-       
-       
+            data['value_gamma'] = data['value_gamma'][bs:self._unroll_len_add_burnin_step - self._nstep]
+
         return data
 
     def _forward_learn(self, data: dict) -> Dict[str, Any]:
@@ -229,15 +240,33 @@ class NGUPolicy(Policy):
         self._target_model.train()
         self._learn_model.reset(data_id=None, state=data['prev_state'][0])
         self._target_model.reset(data_id=None, state=data['prev_state'][0])
-    
+
         if len(data['burnin_obs']) != 0:
             with torch.no_grad():
-                inputs = {'obs': data['burnin_obs'], 'beta': data['burnin_beta'], 'enable_fast_timestep': True} #'prev_state':[None for i in range(32)]
+                inputs = {
+                    'obs': data['burnin_obs'],
+                    'action': data['burnin_action'],
+                    'reward': data['burnin_reward'],
+                    'beta': data['burnin_beta'],
+                    'enable_fast_timestep': True
+                }  #'prev_state':[None for i in range(32)]
                 tmp = self._learn_model.forward(inputs)
                 tmp_target = self._target_model.forward(inputs)
-        inputs = {'obs': data['main_obs'],'beta': data['main_beta'],  'enable_fast_timestep': True} #'prev_state':tmp['hidden_state'][-1].unsqueeze(0),
+        inputs = {
+            'obs': data['main_obs'],
+            'action': data['main_action'],
+            'reward': data['main_reward'],
+            'beta': data['main_beta'],
+            'enable_fast_timestep': True
+        }  #'prev_state':tmp['hidden_state'][-1].unsqueeze(0),
         q_value = self._learn_model.forward(inputs)['logit']
-        next_inputs = {'obs': data['target_obs'],'beta': data['target_beta'],  'enable_fast_timestep': True} #'prev_state':tmp_target['hidden_state'][-1].unsqueeze(0), 
+        next_inputs = {
+            'obs': data['target_obs'],
+            'action': data['target_action'],
+            'reward': data['target_reward'],
+            'beta': data['target_beta'],
+            'enable_fast_timestep': True
+        }  #'prev_state':tmp_target['hidden_state'][-1].unsqueeze(0),
         with torch.no_grad():
             target_q_value = self._target_model.forward(next_inputs)['logit']
             target_q_action = self._learn_model.forward(next_inputs)['action']
@@ -250,7 +279,7 @@ class NGUPolicy(Policy):
         value_gamma = data['value_gamma']
         # self._gamma=  data['gamma']
         # reward torch.Size([4, 5, 64])
-        for t in range(self._unroll_len_add_burnin_step - self._burnin_step-self._nstep):
+        for t in range(self._unroll_len_add_burnin_step - self._burnin_step - self._nstep):
             td_data = q_nstep_td_data(
                 q_value[t], target_q_value[t], action[t], target_q_action[t], reward[t], done[t], weight[t]
             )
@@ -300,8 +329,8 @@ class NGUPolicy(Policy):
         self._burnin_step = self._cfg.burnin_step
         self._gamma = self._cfg.discount_factor
         # self._unroll_len_add_burnin_step  = self._burnin_step + 2 * self._nstep # pu
-        self._unroll_len_add_burnin_step  = self._cfg.unroll_len + self._cfg.burnin_step # TODO
-        self._unroll_len =   self._unroll_len_add_burnin_step
+        self._unroll_len_add_burnin_step = self._cfg.unroll_len + self._cfg.burnin_step  # TODO
+        self._unroll_len = self._unroll_len_add_burnin_step
         self._collect_model = model_wrap(
             self._model, wrapper_name='hidden_state', state_num=self._cfg.collect.env_num, save_prev_state=True
         )
@@ -331,8 +360,8 @@ class NGUPolicy(Policy):
     #         output = to_device(output, 'cpu')
     #     output = default_decollate(output)
     #     return {i: d for i, d in zip(data_id, output)}
-    
-    def _forward_collect(self, data: dict, beta:dict, eps: dict) -> dict:
+
+    def _forward_collect(self, beta: dict, obs: dict, prev_action:dict,  prev_reward_e:dict, eps: dict) -> dict:
         r"""
         Overview:
             Collect output according to eps_greedy plugin
@@ -343,18 +372,21 @@ class NGUPolicy(Policy):
         Returns:
             - data (:obj:`dict`): The collected data
         """
-        data_id = list(data.keys())
-        data = default_collate(list(data.values()))
+        eps = {i: 0.4 ** (1 + 8 * i / (len(obs) - 1)) for i in range(len(obs))}  # TODO
+        data_id = list(obs.keys())
+        obs = default_collate(list(obs.values()))
+        beta = default_collate(list(beta.values()))
+        prev_action = default_collate(list(prev_action.values()))
+        prev_reward_e = default_collate(list( prev_reward_e.values()))
         if self._cuda:
-            data = to_device(data, self._device)
-        beta= default_collate(list(beta.values()))
-        if self._cuda:
+            obs = to_device(obs, self._device)
             beta = to_device(beta, self._device)
-        # data = {'obs': data}
-        data = {'obs': data, 'beta':beta}
+            prev_action = to_device(prev_action, self._device)
+            prev_reward_e  = to_device( prev_reward_e, self._device)
+        data = {'obs': obs, 'beta': beta, 'prev_action': prev_action,'prev_reward_e':prev_reward_e}
         self._collect_model.eval()
         with torch.no_grad():
-            output = self._collect_model.forward(data, data_id=data_id, eps=eps, inference=True) # TODO eps 8个
+            output = self._collect_model.forward(data, data_id=data_id, eps=eps, inference=True)  # TODO eps 8个
         if self._cuda:
             output = to_device(output, 'cpu')
         output = default_decollate(output)
@@ -376,11 +408,13 @@ class NGUPolicy(Policy):
             - transition (:obj:`dict`): Dict type transition data.
         """
         transition = {
-            'beta':beta_index,
+            'beta': beta_index,
             'obs': obs,
             'action': model_output['action'],
+            # 'prev_action': model_output['action'],
             'prev_state': model_output['prev_state'],
             'reward': timestep.reward,
+            # 'prev_reward_e': timestep.reward,
             'done': timestep.done,
         }
         return transition
@@ -397,7 +431,7 @@ class NGUPolicy(Policy):
             - samples (:obj:`dict`): The training samples generated
         """
         data = get_nstep_return_data(data, self._nstep, gamma=self._gamma)
-        return get_train_sample(data, self._unroll_len_add_burnin_step )
+        return get_train_sample(data, self._unroll_len_add_burnin_step)
 
     def _init_eval(self) -> None:
         r"""
@@ -409,7 +443,7 @@ class NGUPolicy(Policy):
         self._eval_model = model_wrap(self._eval_model, wrapper_name='argmax_sample')
         self._eval_model.reset()
 
-    def _forward_eval(self, data: dict,beta:dict) -> dict:
+    def _forward_eval(self, beta: dict, obs: dict, prev_action:dict,  prev_reward_e:dict,) -> dict:
         r"""
         Overview:
             Forward function of collect mode, similar to ``self._forward_collect``.
@@ -420,14 +454,18 @@ class NGUPolicy(Policy):
         Returns:
             - output (:obj:`dict`): Dict type data, including at least inferred action according to input obs.
         """
-        data_id = list(data.keys())
-        data = default_collate(list(data.values()))
+        data_id = list(obs.keys())
+        obs = default_collate(list(obs.values()))
+        beta = default_collate(list(beta.values()))
+        prev_action = default_collate(list(prev_action.values()))
+        prev_reward_e = default_collate(list( prev_reward_e.values()))
         if self._cuda:
-            data = to_device(data, self._device)
-        beta= default_collate(list(beta.values()))
-        if self._cuda:
-            beta = to_device(beta, self._device)    
-        data = {'obs': data, 'beta':beta}
+            obs = to_device(obs, self._device)
+            beta = to_device(beta, self._device)
+            prev_action = to_device(prev_action, self._device)
+            prev_reward_e  = to_device( prev_reward_e, self._device)
+        data = {'obs': obs, 'beta': beta, 'prev_action': prev_action,'prev_reward_e':prev_reward_e}
+
         self._eval_model.eval()
         with torch.no_grad():
             output = self._eval_model.forward(data, data_id=data_id, inference=True)
--- dizoo/minigrid/config/minigrid_ngu_config.py
+++ dizoo/minigrid/config/minigrid_ngu_config.py
@@ -1,22 +1,22 @@
 from easydict import EasyDict
 from ding.entry import serial_pipeline_reward_model
 import torch
-print(torch.cuda.is_available(),torch.__version__)
+
+print(torch.cuda.is_available(), torch.__version__)
 collector_env_num = 8
 evaluator_env_num = 5
-nstep=5
-minigrid_ppo_rnd_config = dict(   
-    exp_name='minigrid_empty8_ngu_r2d2_rnd_beta_debug',
-    # exp_name='minigrid_empty8_r2d2_debug',
+nstep = 5
+minigrid_ppo_rnd_config = dict(
+    exp_name='minigrid_empty8_ngu_r2d2_rnd_beta', #TODO
     env=dict(
-        collector_env_num=collector_env_num ,
-        evaluator_env_num=evaluator_env_num ,
+        collector_env_num=collector_env_num,
+        evaluator_env_num=evaluator_env_num,
         n_evaluator_episode=5,
         env_id='MiniGrid-Empty-8x8-v0',
         stop_value=0.96,
     ),
     reward_model=dict(
-        intrinsic_reward_type='add', #add',  # 'assign'
+        intrinsic_reward_type='add',  #add',  # 'assign'
         learning_rate=0.001,
         obs_shape=2739,
         batch_size=32,
@@ -25,27 +25,28 @@ minigrid_ppo_rnd_config = dict(
     ),
     policy=dict(
         continuous=False,
-        on_policy=False, 
+        on_policy=False,
         cuda=True,
         priority=False,
         discount_factor=0.997,
         burnin_step=20,
         nstep=nstep,
-        unroll_len=80, #
+        unroll_len=80,  #
         model=dict(
             obs_shape=2739,
             action_shape=7,
-            encoder_hidden_size_list=[256, 128, 64, 64],
+            # encoder_hidden_size_list=[256, 128, 64, 64],
+            encoder_hidden_size_list=[256, 64], #TODO
         ),
         learn=dict(
-            update_per_collect=20,#4,
-            batch_size=32, #64,
+            update_per_collect=20,  #4,
+            batch_size=32,  #64,
             learning_rate=0.0005,
             value_weight=0.5,
             entropy_weight=0.001,
             clip_ratio=0.2,
             adv_norm=False,
-            target_update_freq=500,#100,
+            target_update_freq=500,  #100,
         ),
         collect=dict(
             # n_sample=128,
@@ -61,7 +62,8 @@ minigrid_ppo_rnd_config = dict(
                 start=0.95,
                 end=0.05,
                 decay=10000,
-            ), replay_buffer=dict(replay_buffer_size=1000000, ) #10000
+            ),
+            replay_buffer=dict(replay_buffer_size=1000000, )  #10000
         ),
     ),
 )
@@ -72,8 +74,8 @@ minigrid_ppo_rnd_create_config = dict(
         type='minigrid',
         import_names=['dizoo.minigrid.envs.minigrid_env'],
     ),
-    # env_manager=dict(type='base'),
-    env_manager=dict(type='subprocess'),
+    env_manager=dict(type='base'),
+    # env_manager=dict(type='subprocess'),
     policy=dict(type='ngu'),
     reward_model=dict(type='rnd'),
 )
