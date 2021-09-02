019bd55db2f37016d82b99ad4ac79c1b2459cb84
puyuan1996 <2402552459@qq.com>
2021-09-02 17:06:45 +0800

    style(pu): run format.sh

--- ding/policy/r2d2.py
+++ ding/policy/r2d2.py
@@ -179,33 +179,33 @@ class R2D2Policy(Policy):
         bs = self._burnin_step
 
         # data['done'], data['weight'], data['value_gamma'] is used in def _forward_learn() to calculate the q_nstep_td_error,
-        # should be length of [self._unroll_len_add_burnin_step-self._burnin_step-self._nstep] 
+        # should be length of [self._unroll_len_add_burnin_step-self._burnin_step-self._nstep]
         ignore_done = self._cfg.learn.ignore_done
         if ignore_done:
-            data['done'] = [None for _ in range(self._unroll_len_add_burnin_step-bs-self._nstep)]
+            data['done'] = [None for _ in range(self._unroll_len_add_burnin_step - bs - self._nstep)]
         else:
             data['done'] = data['done'][bs:-self._nstep].float()
 
-        # if the data don't include 'weight' or 'value_gamma' then fill in None in a list 
+        # if the data don't include 'weight' or 'value_gamma' then fill in None in a list
         # with length of [self._unroll_len_add_burnin_step-self._burnin_step-self._nstep],
         # below is two different implementation ways
         if 'value_gamma' not in data:
-            data['value_gamma'] =  [None for _ in range(self._unroll_len_add_burnin_step-bs-self._nstep)]
+            data['value_gamma'] = [None for _ in range(self._unroll_len_add_burnin_step - bs - self._nstep)]
         else:
             data['value_gamma'] = data['value_gamma'][bs:-self._nstep]
-        data['weight'] = data.get('weight', [None for _ in range(self._unroll_len_add_burnin_step-bs-self._nstep)])
+        data['weight'] = data.get('weight', [None for _ in range(self._unroll_len_add_burnin_step - bs - self._nstep)])
 
         data['action'] = data['action'][bs:-self._nstep]
         data['reward'] = data['reward'][bs:-self._nstep]
 
-        # the burnin_obs is used to calculate the init hidden state for the calculation of the q_value 
+        # the burnin_obs is used to calculate the init hidden state for the calculation of the q_value
         data['burnin_obs'] = data['obs'][:bs]
-        # the main_obs is used to calculate the q_value, 
+        # the main_obs is used to calculate the q_value,
         # the [bs:-self._nstep] means using the data from [bs] timestep to [self._unroll_len_add_burnin_step-self._nstep] timestep
-        data['main_obs'] = data['obs'][bs:-self._nstep] 
-        # the target_obs is used to calculate the target_q_value 
-        data['target_obs'] = data['obs'][bs + self._nstep:] 
-        
+        data['main_obs'] = data['obs'][bs:-self._nstep]
+        # the target_obs is used to calculate the target_q_value
+        data['target_obs'] = data['obs'][bs + self._nstep:]
+
         return data
 
     def _forward_learn(self, data: dict) -> Dict[str, Any]:
@@ -247,7 +247,7 @@ class R2D2Policy(Policy):
         loss = []
         td_error = []
         value_gamma = data['value_gamma']
-        for t in range(self._unroll_len_add_burnin_step-self._burnin_step-self._nstep):
+        for t in range(self._unroll_len_add_burnin_step - self._burnin_step - self._nstep):
             td_data = q_nstep_td_data(
                 q_value[t], target_q_value[t], action[t], target_q_action[t], reward[t], done[t], weight[t]
             )
@@ -296,8 +296,8 @@ class R2D2Policy(Policy):
         self._nstep = self._cfg.nstep
         self._burnin_step = self._cfg.burnin_step
         self._gamma = self._cfg.discount_factor
-        self._unroll_len_add_burnin_step = self._cfg.unroll_len + self._cfg.burnin_step 
-        self._unroll_len = self._unroll_len_add_burnin_step # for compatibility
+        self._unroll_len_add_burnin_step = self._cfg.unroll_len + self._cfg.burnin_step
+        self._unroll_len = self._unroll_len_add_burnin_step  # for compatibility
 
         self._collect_model = model_wrap(
             self._model, wrapper_name='hidden_state', state_num=self._cfg.collect.env_num, save_prev_state=True
