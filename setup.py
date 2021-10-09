fac84bcf829bbf7a24bac2d404fd078ebd93e30e
niuyazhe <niuyazhe@sensetime.com>
2021-10-09 16:25:56 +0800

    fix(nyz): fix gym version>0.20.0 pendulum-v0 bug(enable docker, smac docker)

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'numpy>=1.10',
         'requests>=2.25.1',
         'six',
-        'gym>=0.20.0',  # pypy incompatible
+        'gym==0.20.0',  # pypy incompatible
         'torch>=1.3.1,<=1.9.0',  # PyTorch 1.9.0 is available, if some errors, you need to do something like https://github.com/opendilab/DI-engine/discussions/81
         'pyyaml',
         'easydict==1.9',
