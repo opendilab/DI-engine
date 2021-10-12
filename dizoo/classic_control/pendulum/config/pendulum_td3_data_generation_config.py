afc1460f970045e0b0670e2813a4fcf261510858
zhangyinmin <yisraelzhang@gmail.com>
2021-10-12 20:15:47 +0800

    feature(zym): add offlineRL algo td3_bc.

--- dizoo/classic_control/pendulum/config/pendulum_td3_data_generation_config.py
+++ dizoo/classic_control/pendulum/config/pendulum_td3_data_generation_config.py
@@ -1,13 +1,13 @@
 from easydict import EasyDict
 
-pendulum_td3_config = dict(
-    exp_name='pendulum_td3',
+pendulum_td3_generation_config = dict(
+    exp_name='td3',
     env=dict(
         collector_env_num=8,
-        evaluator_env_num=5,
+        evaluator_env_num=10,
         # (bool) Scale output action into legal range.
         act_scale=True,
-        n_evaluator_episode=5,
+        n_evaluator_episode=10,
         stop_value=-250,
     ),
     policy=dict(
@@ -28,31 +28,42 @@ pendulum_td3_config = dict(
             ignore_done=True,
             actor_update_freq=2,
             noise=True,
-            noise_sigma=0.1,
+            noise_sigma=0.2,
             noise_range=dict(
                 min=-0.5,
                 max=0.5,
             ),
+            learner = dict(
+                load_path='./td3/ckpt/ckpt_best.pth.tar',
+                hook=dict(
+                    load_ckpt_before_run='./td3/ckpt/ckpt_best.pth.tar',
+                    save_ckpt_after_run=False,
+                )
+            ),
         ),
         collect=dict(
-            n_sample=48,
+            n_sample=10,
             noise_sigma=0.1,
             collector=dict(collect_print_freq=1000, ),
+            save_path='./td3/expert.pkl',
+            data_type = 'hdf5',
         ),
         eval=dict(evaluator=dict(eval_freq=100, ), ),
-        other=dict(replay_buffer=dict(replay_buffer_size=20000, ), ),
+        other=dict(replay_buffer=dict(
+            replay_buffer_size=40000,
+        ), ),
     ),
 )
-pendulum_td3_config = EasyDict(pendulum_td3_config)
-main_config = pendulum_td3_config
+pendulum_td3_generation_config = EasyDict(pendulum_td3_generation_config)
+main_config = pendulum_td3_generation_config
 
-pendulum_td3_create_config = dict(
+pendulum_td3_generation_create_config = dict(
     env=dict(
         type='pendulum',
         import_names=['dizoo.classic_control.pendulum.envs.pendulum_env'],
     ),
     env_manager=dict(type='base'),
-    policy=dict(type='td3'),
+    policy=dict(type='ddpg'),
 )
-pendulum_td3_create_config = EasyDict(pendulum_td3_create_config)
-create_config = pendulum_td3_create_config
+pendulum_td3_generation_create_config = EasyDict(pendulum_td3_generation_create_config)
+create_config = pendulum_td3_generation_create_config
