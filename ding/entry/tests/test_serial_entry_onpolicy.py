d0224285768390377ed8753c46b82d5441f7561c
卫昱宏 <SENSETIME\weiyuhong@cn0214000504l.domain.sensetime.com>
2021-10-13 15:37:31 +0800

    fix(wyh):model test and policy/entry test

---
+++ ding/entry/tests/test_serial_entry_onpolicy.py
@@ -0,0 +1,16 @@
+import pytest
+import time
+import os
+from copy import deepcopy
+
+from ding.entry import serial_pipeline_onpolicy
+from dizoo.smac.config.smac_3s5z_mappo_config import main_config, create_config  # noqa
+
+@pytest.mark.unittest
+def test_mappo():
+    config = [deepcopy(main_config), deepcopy(create_config)]
+    config[0].policy.learn.update_per_collect = 1
+    try:
+        serial_pipeline_onpolicy(config, seed=0, max_iterations=1)
+    except Exception:
+        assert False, "pipeline fail"
