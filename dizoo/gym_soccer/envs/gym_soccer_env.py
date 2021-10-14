fd36c155bebfdef6cf023f308bff5512ad041b31
李可 <like2@CN0014008466M.local>
2021-10-14 19:18:46 +0800

    format

--- dizoo/gym_soccer/envs/gym_soccer_env.py
+++ dizoo/gym_soccer/envs/gym_soccer_env.py
@@ -9,17 +9,18 @@ from ding.envs.common.env_element import EnvElementInfo
 from ding.torch_utils import to_tensor, to_ndarray, to_list
 from gym.utils import seeding
 
+
 @ENV_REGISTRY.register('gym_soccer')
 class GymSoccerEnv(BaseEnv):
-    default_env_id = ['Soccer-v0', 'SoccerEmptyGoal-v0', 'SoccerAgainstKeeper-v0'] 
-    
+    default_env_id = ['Soccer-v0', 'SoccerEmptyGoal-v0', 'SoccerAgainstKeeper-v0']
+
     def __init__(self, cfg: dict = {}) -> None:
         self._cfg = cfg
         self._env_id = cfg.env_id
         assert self._env_id in self.default_env_id
         self._init_flag = False
         self._replay_path = None
-    
+
     def reset(self) -> torch.Tensor:
         if not self._init_flag:
             self._env = gym.make(self._env_id)
@@ -29,12 +30,12 @@ class GymSoccerEnv(BaseEnv):
                     self._env, self._replay_path, video_callable=lambda episode_id: True, force=True
                 )
                 self._env.metadata["render.modes"] = ["human", "rgb_array"]
-                
+
         self._final_eval_reward = 0
         obs = self._env._reset()
         obs = to_ndarray(obs).astype(np.float32)
         return obs
-    
+
     def step(self, action: List) -> BaseEnvTimestep:
         obs, rew, done, info = self._env._step(action)
         self._final_eval_reward += rew
@@ -42,16 +43,16 @@ class GymSoccerEnv(BaseEnv):
             info['final_eval_reward'] = self._final_eval_reward
         obs = to_ndarray(obs).astype(np.float32)
         # reward wrapped to be transfered to a Tensor with shape (1,)
-        rew = to_ndarray([rew])  
+        rew = to_ndarray([rew])
         # '1' indicates the discrete action is associated with the continuous parameters
-        info['action_args_mask'] = np.array([[1, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 1]]) 
+        info['action_args_mask'] = np.array([[1, 1, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 1, 1]])
         return BaseEnvTimestep(obs, rew, done, info)
 
     def seed(self, seed: int, dynamic_seed: bool = True) -> None:
         self._seed = seed
         self._dynamic_seed = dynamic_seed
         np.random.seed(self._seed)
-    
+
     def close(self) -> None:
         self._init_flag = False
 
@@ -59,7 +60,6 @@ class GymSoccerEnv(BaseEnv):
         T = EnvElementInfo
         return BaseEnvInfo(
             agent_num=1,
-
             obs_space=T(
                 (6, ),
                 {
@@ -69,7 +69,6 @@ class GymSoccerEnv(BaseEnv):
                     'dtype': np.float32,
                 },
             ),
-            
             act_space=T(
                 (3, ),
                 {
@@ -79,7 +78,6 @@ class GymSoccerEnv(BaseEnv):
                     'dtype': int,
                 },
             ),
-
             rew_space=T(
                 (1, ),
                 {
