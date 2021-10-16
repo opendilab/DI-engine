f5e4d4ec816fc36e4dae5ae3a9c5b833121d20db
卫昱宏 <SENSETIME\weiyuhong@cn0214000504l.domain.sensetime.com>
2021-10-16 21:15:54 +0800

    fix(wyh):test serial entry onpolicy

--- ding/entry/tests/test_serial_entry_onpolicy.py
+++ ding/entry/tests/test_serial_entry_onpolicy.py
@@ -4,12 +4,12 @@ import os
 from copy import deepcopy
 
 from ding.entry import serial_pipeline_onpolicy
-from dizoo.smac.config.smac_3s5z_mappo_config import main_config, create_config  # noqa
+from dizoo.classic_control.cartpole.config.cartpole_ppo_config import cartpole_ppo_config, cartpole_ppo_create_config
 
 
 @pytest.mark.unittest
 def test_mappo():
-    config = [deepcopy(main_config), deepcopy(create_config)]
+    config = [deepcopy(cartpole_ppo_config), deepcopy(cartpole_ppo_create_config)]
     config[0].policy.learn.epoch_per_collect = 1
     try:
         serial_pipeline_onpolicy(config, seed=0, max_iterations=1)
