b4a089cc148f28f389a5babd413a4aa4845ed036
Xu Jingxin <sailxjx@gmail.com>
2021-10-19 17:57:40 +0800

    Trap exit

--- ding/scripts/docker-test-entry.sh
+++ ding/scripts/docker-test-entry.sh
@@ -2,6 +2,8 @@
 
 CONTAINER_ID=$(docker run --rm -d opendilab/ding:nightly tail -f /dev/null)
 
+# trap "docker rm -f $CONTAINER_ID" EXIT
+
 docker exec $CONTAINER_ID rm -rf /ding &&
   docker cp $(pwd) ${CONTAINER_ID}:/ding &&
   docker exec -it $CONTAINER_ID /ding/ding/scripts/docker-test.sh
