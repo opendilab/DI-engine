eabe1555da4c5be7cf01bf3a7908e262acee448c
Xu Jingxin <sailxjx@gmail.com>
2021-10-20 12:53:30 +0800

    Add buffer.view

--- ding/worker/buffer/buffer.py
+++ ding/worker/buffer/buffer.py
@@ -1,5 +1,6 @@
 from typing import Any, Callable, List
 from ding.worker.buffer.storage import Storage
+import copy
 
 
 def apply_middleware(func_name: str):
@@ -21,19 +22,43 @@ def apply_middleware(func_name: str):
 class Buffer:
 
     def __init__(self, storage: Storage, **kwargs) -> None:
+        """
+        Overview:
+            Initialize the buffer
+        Arguments:
+            - storage (:obj:`Storage`): The storage instance.
+        """
         self.storage = storage
         self.middlewares = []
 
     @apply_middleware("push")
     def push(self, data: Any) -> None:
+        r"""
+        Overview:
+            Push a data into buffer.
+        Arguments:
+            - data (:obj:`Any`): The data which will be pushed into buffer.
+        """
         self.storage.append(data)
 
     @apply_middleware("sample")
     def sample(self, size: int) -> List[Any]:
+        """
+        Overview:
+            Sample data with length ``size``, this function may be wrapped by middlewares.
+        Arguments:
+            - size (:obj:`int`): The number of the data that will be sampled.
+        Returns:
+            - sample_data (:obj:`list`): A list of data with length ``size``.
+        """
         return self.storage.sample(size)
 
     @apply_middleware("clear")
     def clear(self) -> None:
+        """
+        Overview:
+            Clear the storage
+        """
         self.storage.clear()
 
     def use(self, func: Callable) -> "Buffer":
@@ -41,7 +66,8 @@ class Buffer:
         Overview:
             Use algorithm middlewares to modify the behavior of the buffer.
             Every middleware should be a callable function, it will receive three argument parts, including:
-            1. The buffer instance, you can use this instance to visit every thing of the buffer, including the storage.
+            1. The buffer instance, you can use this instance to visit every thing of the buffer,
+               including the storage.
             2. The functions called by the user, there are three methods named `push`, `sample` and `clear`,
                so you can use these function name to decide which action to choose.
             3. The remaining arguments passed by the user to the original function, will be passed in *args.
@@ -51,38 +77,21 @@ class Buffer:
                no more middlewares will be executed during this execution
             2. The remaining values, will be passed to the next middleware or the default function in the buffer.
         Arguments:
-            - func (:obj:`Callable`): the middleware handler
+            - func (:obj:`Callable`): The middleware handler
+        Returns:
+            - buffer (:obj:`Buffer`): The instance self
         """
         self.middlewares.append(func)
         return self
 
-
-class RateLimit:
-    r"""
-    Add rate limit threshold to push function
-    """
-
-    def __init__(self, max_rate: int = float("inf"), window_seconds: int = 30) -> None:
-        self.max_rate = max_rate
-        self.window_seconds = window_seconds
-        self.buffered = []
-
-    def handler(self) -> Callable:
-
-        def _handler(buffer: Buffer, action: str, *args):
-            if action == "push":
-                return self.push(*args)
-            return args
-
-        return _handler
-
-    def push(self, data) -> None:
-        import time
-        current = time.time()
-        # Cut off stale records
-        self.buffered = [t for t in self.buffered if t > current - self.window_seconds]
-        if len(self.buffered) < self.max_rate:
-            self.buffered.append(current)
-            return False, data
-        else:
-            return True, None
+    def view(self) -> "Buffer":
+        r"""
+        Overview:
+            A view is a new instance of buffer, with a deepcopy of every property except the storage.
+            The storage is shared among all the buffer instances.
+        Returns:
+            - buffer (:obj:`Buffer`): The instance self
+        """
+        buffer = Buffer(self.storage)
+        buffer.middlewares = copy.deepcopy(self.middlewares)
+        return buffer
