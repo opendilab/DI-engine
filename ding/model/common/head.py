e334c4edecd9ce0716dd7eeaa781baa8420f90b1
李可 <like2@CN0014008466M.local>
2021-10-21 18:04:41 +0800

    parallel_SDN

--- ding/model/common/head.py
+++ ding/model/common/head.py
@@ -605,6 +605,12 @@ class StochasticDuelingHead(nn.Module):
                 The encoded embedding state tensor, determined with given ``hidden_size``, i.e. ``(B, N=hidden_size)``.
             - a (:obj:`torch.Tensor`):
                 The encoded embedding action tensor, determined with ``action_size``, i.e. ``(B, N=action_size)``.
+            - mu_t (:obj:`torch.Tensor`):
+                The mu output of actor head at timestep t, size (B, action_size)
+            - sigma_t (:obj:`torch.Tensor`):
+                The sigma output of actor head at timestep t, size (B, action_size)
+            - sample_size (:obj:`int`):
+                The num to samples for action when computing the Q value
         Returns:
             - outputs (:obj:`Dict`):
                 Run ``MLP`` with ``DuelingHead`` setups and return the result prediction dictionary.
@@ -612,19 +618,18 @@ class StochasticDuelingHead(nn.Module):
                     - pred (:obj:`torch.Tensor`): Pred tensor of size ``(B, 1)``.
         """
         batch_size = s.shape[0]
-        # action_size = a.shape[1]
+        hidden_size = s.shape[1]
+        action_size = a.shape[1]
         state_cat_action = torch.cat((s,a),dim=1)  # size (B, action_size + state_size)
         a_val = self.A(state_cat_action)  # size (B, 1)
         s_val = self.V(s)  # size (B,1)
-        a_val_sample = torch.zeros((batch_size, sample_size), dtype=torch.float32)  # size (B,sample_size)
-        for i in range(sample_size):
-            # mu_t size (B, action_size)
-            action_sample = torch.normal(mu_t, sigma_t)  # size (B, action_size)
-            # state_cat_action_sample size (B, action_size+state_size)
-            state_cat_action_sample = torch.cat((s,action_sample),dim=1)
-            # a_val_sample[:,i] size == torch.Size([B])
-            a_val_sample[:,i] = self.A(state_cat_action_sample).reshape(1,batch_size) 
-
+        mu_t = (torch.unsqueeze(mu_t, 1)).expand((batch_size, sample_size, action_size,))  # size (B, sample_size, action_size) 
+        sigma_t = (torch.unsqueeze(sigma_t, 1)).expand((batch_size, sample_size, action_size))  # size (B, sample_size, action_size) 
+        expand_s = (torch.unsqueeze(s, 1)).expand((batch_size, sample_size, hidden_size))  # size (B, sample_size, action_size) 
+        action_sample = torch.normal(mu_t, sigma_t)  # size (B, sample_size, action_size) 
+        state_cat_action_sample = torch.cat((expand_s,action_sample),dim=1)   # size (B, sample_size, action_size + hidden_size) 
+        a_val_sample = self.A(state_cat_action_sample)  # size (B, sample_size, 1)
+        a_val_sample = torch.squeeze(a_val_sample, -1)  # (B, sample_size)
         logit = a_val - a_val_sample.mean(dim=-1, keepdim=True) + s_val  # size (B,1)
         return {'pred': logit}
 
