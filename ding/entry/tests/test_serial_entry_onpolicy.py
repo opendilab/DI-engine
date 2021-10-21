37f92d63c7847d12066b56571e57e0156d19930c
卫昱宏 <SENSETIME\weiyuhong@cn0214000504l.domain.sensetime.com>
2021-10-21 16:14:53 +0800

    fix(wyh):ppo_continuous test

--- ding/entry/tests/test_serial_entry_onpolicy.py
+++ ding/entry/tests/test_serial_entry_onpolicy.py
@@ -6,7 +6,7 @@ from copy import deepcopy
 from ding.entry import serial_pipeline_onpolicy
 from dizoo.classic_control.cartpole.config.cartpole_ppo_config import cartpole_ppo_config, cartpole_ppo_create_config
 from dizoo.multiagent_particle.config import cooperative_navigation_mappo_config, cooperative_navigation_mappo_create_config
-
+from dizoo.classic_control.pendulum.config.pendulum_ppo_config import pendulum_ppo_config, pendulum_ppo_create_config
 
 @pytest.mark.unittest
 def test_onpolicy_ppo():
@@ -21,6 +21,16 @@ def test_onpolicy_ppo():
 @pytest.mark.unittest
 def test_mappo():
     config = [deepcopy(cooperative_navigation_mappo_config), deepcopy(cooperative_navigation_mappo_create_config)]
+    config[0].policy.learn.epoch_per_collect = 1
+    try:
+        serial_pipeline_onpolicy(config, seed=0, max_iterations=1)
+    except Exception:
+        assert False, "pipeline fail"
+
+
+@pytest.mark.unittest
+def test_ppo_continuous():
+    config = [deepcopy(pendulum_ppo_config), deepcopy(pendulum_ppo_create_config)]
     config[0].policy.learn.epoch_per_collect = 1
     try:
         serial_pipeline_onpolicy(config, seed=0, max_iterations=1)
