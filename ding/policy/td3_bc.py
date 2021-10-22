7c1b5e9599e8657a3e181124314b0e9fe971edbb
Yinmin.Zhang <40760801+YinminZhang@users.noreply.github.com>
2021-10-22 22:32:38 +0800

    feature(zym): add offlineRL algo td3_bc and polish policy comments(#88)
    * feature(zym): add offlineRL algo td3_bc.
    
    * feature(zym): add offlineRL algo td3_bc.
    
    * feature(zym): add offlineRL algo td3_bc.
    
    * polish(zym): polish some annotations in td3/ddpg/sac/ppo; polish `_forward_collect` and `_foward_eval`.
    
    * fix(lj): fix dimension bug in cql for continuous env.
    
    * fix(zym): fix dimension bug in cql for continuous env.
    
    * fix(zym): fix dimension bug in cql for continuous env.
    
    * polish(zym): update README.md.

--- ding/policy/td3_bc.py
+++ ding/policy/td3_bc.py
@@ -1,17 +1,29 @@
+from typing import List, Dict, Any, Tuple, Union
+from collections import namedtuple
+import torch
+import torch.nn.functional as F
+import copy
+
+from ding.torch_utils import Adam, to_device
+from ding.rl_utils import v_1step_td_data, v_1step_td_error, get_train_sample
+from ding.model import model_wrap
 from ding.utils import POLICY_REGISTRY
+from ding.utils.data import default_collate, default_decollate
+from .base_policy import Policy
+from .common_utils import default_preprocess_learn
 from .ddpg import DDPGPolicy
 
 
-@POLICY_REGISTRY.register('td3')
-class TD3Policy(DDPGPolicy):
+@POLICY_REGISTRY.register('td3_bc')
+class TD3BCPolicy(DDPGPolicy):
     r"""
     Overview:
-        Policy class of TD3 algorithm.
+        Policy class of TD3_BC algorithm.
 
-        Since DDPG and TD3 share many common things, we can easily derive this TD3
+        Since DDPG and TD3 share many common things, we can easily derive this TD3_BC
         class from DDPG class by changing ``_actor_update_freq``, ``_twin_critic`` and noise in model wrapper.
 
-        https://arxiv.org/pdf/1802.09477.pdf
+        https://arxiv.org/pdf/2106.06860.pdf
 
     Property:
         learn_mode, collect_mode, eval_mode
@@ -21,7 +33,7 @@ class TD3Policy(DDPGPolicy):
     == ====================  ========    ==================  =================================   =======================
     ID Symbol                Type        Default Value       Description                         Other(Shape)
     == ====================  ========    ==================  =================================   =======================
-    1  ``type``              str         td3                 | RL policy register name, refer    | this arg is optional,
+    1  ``type``              str         td3_bc              | RL policy register name, refer    | this arg is optional,
                                                              | to registry ``POLICY_REGISTRY``   | a placeholder
     2  ``cuda``              bool        True                | Whether to use cuda for network   |
     3  | ``random_``         int         25000               | Number of randomly collected      | Default to 25000 for
@@ -64,7 +76,7 @@ class TD3Policy(DDPGPolicy):
     # You can refer to DDPG's default config for more details.
     config = dict(
         # (str) RL policy register name (refer to function "POLICY_REGISTRY").
-        type='td3',
+        type='td3_bc',
         # (bool) Whether to use cuda for network.
         cuda=False,
         # (bool type) on_policy: Determine whether on-policy or off-policy.
@@ -126,13 +138,15 @@ class TD3Policy(DDPGPolicy):
                 min=-0.5,
                 max=0.5,
             ),
+            alpha=2.5,
         ),
         collect=dict(
-            n_sample=1,
             # (int) Cut trajectories into pieces with length "unroll_len".
             unroll_len=1,
             # (float) It is a must to add noise during collection. So here omits "noise" and only set "noise_sigma".
             noise_sigma=0.1,
+            # (bool) Whether to normalize the features of every state in the provided dataset.
+            normalize_states=True,
         ),
         eval=dict(
             evaluator=dict(
@@ -147,3 +161,172 @@ class TD3Policy(DDPGPolicy):
             ),
         ),
     )
+
+    def _init_learn(self) -> None:
+        r"""
+        Overview:
+            Learn mode init method. Called by ``self.__init__``.
+            Init actor and critic optimizers, algorithm config.
+        """
+        super(TD3BCPolicy, self)._init_learn()
+        self._alpha = self._cfg.learn.alpha
+        # actor and critic optimizer
+        self._optimizer_actor = Adam(
+            self._model.actor.parameters(),
+            lr=self._cfg.learn.learning_rate_actor,
+            grad_clip_type='clip_norm',
+            clip_value=1.0,
+        )
+        self._optimizer_critic = Adam(
+            self._model.critic.parameters(),
+            lr=self._cfg.learn.learning_rate_critic,
+            grad_clip_type='clip_norm',
+            clip_value=1.0,
+        )
+
+    def _forward_learn(self, data: dict) -> Dict[str, Any]:
+        r"""
+        Overview:
+            Forward and backward function of learn mode.
+        Arguments:
+            - data (:obj:`dict`): Dict type data, including at least ['obs', 'action', 'reward', 'next_obs']
+        Returns:
+            - info_dict (:obj:`Dict[str, Any]`): Including at least actor and critic lr, different losses.
+        """
+        loss_dict = {}
+        data = default_preprocess_learn(
+            data,
+            use_priority=self._cfg.priority,
+            use_priority_IS_weight=self._cfg.priority_IS_weight,
+            ignore_done=self._cfg.learn.ignore_done,
+            use_nstep=False
+        )
+        if self._cuda:
+            data = to_device(data, self._device)
+        # ====================
+        # critic learn forward
+        # ====================
+        self._learn_model.train()
+        self._target_model.train()
+        next_obs = data.get('next_obs')
+        reward = data.get('reward')
+        if self._use_reward_batch_norm:
+            reward = (reward - reward.mean()) / (reward.std() + 1e-8)
+        # current q value
+        q_value = self._learn_model.forward(data, mode='compute_critic')['q_value']
+        q_value_dict = {}
+        if self._twin_critic:
+            q_value_dict['q_value'] = q_value[0].mean()
+            q_value_dict['q_value_twin'] = q_value[1].mean()
+        else:
+            q_value_dict['q_value'] = q_value.mean()
+        # target q value.
+        with torch.no_grad():
+            next_action = self._target_model.forward(next_obs, mode='compute_actor')['action']
+            next_data = {'obs': next_obs, 'action': next_action}
+            target_q_value = self._target_model.forward(next_data, mode='compute_critic')['q_value']
+        if self._twin_critic:
+            # TD3: two critic networks
+            target_q_value = torch.min(target_q_value[0], target_q_value[1])  # find min one as target q value
+            # critic network1
+            td_data = v_1step_td_data(q_value[0], target_q_value, reward, data['done'], data['weight'])
+            critic_loss, td_error_per_sample1 = v_1step_td_error(td_data, self._gamma)
+            loss_dict['critic_loss'] = critic_loss
+            # critic network2(twin network)
+            td_data_twin = v_1step_td_data(q_value[1], target_q_value, reward, data['done'], data['weight'])
+            critic_twin_loss, td_error_per_sample2 = v_1step_td_error(td_data_twin, self._gamma)
+            loss_dict['critic_twin_loss'] = critic_twin_loss
+            td_error_per_sample = (td_error_per_sample1 + td_error_per_sample2) / 2
+        else:
+            # DDPG: single critic network
+            td_data = v_1step_td_data(q_value, target_q_value, reward, data['done'], data['weight'])
+            critic_loss, td_error_per_sample = v_1step_td_error(td_data, self._gamma)
+            loss_dict['critic_loss'] = critic_loss
+        # ================
+        # critic update
+        # ================
+        self._optimizer_critic.zero_grad()
+        for k in loss_dict:
+            if 'critic' in k:
+                loss_dict[k].backward()
+        self._optimizer_critic.step()
+        # ===============================
+        # actor learn forward and update
+        # ===============================
+        # actor updates every ``self._actor_update_freq`` iters
+        if (self._forward_learn_cnt + 1) % self._actor_update_freq == 0:
+            actor_data = self._learn_model.forward(data['obs'], mode='compute_actor')
+            actor_data['obs'] = data['obs']
+            if self._twin_critic:
+                q_value = self._learn_model.forward(actor_data, mode='compute_critic')['q_value'][0]
+                actor_loss = -q_value.mean()
+            else:
+                q_value = self._learn_model.forward(actor_data, mode='compute_critic')['q_value']
+                actor_loss = -q_value.mean()
+
+            # add behavior cloning loss weight(\lambda)
+            lmbda = self._alpha / q_value.abs().mean().detach()
+            # bc_loss = ((actor_data['action'] - data['action'])**2).mean()
+            bc_loss = F.mse_loss(actor_data['action'], data['action'])
+            actor_loss = lmbda * actor_loss + bc_loss
+            loss_dict['actor_loss'] = actor_loss
+            # actor update
+            self._optimizer_actor.zero_grad()
+            actor_loss.backward()
+            self._optimizer_actor.step()
+        # =============
+        # after update
+        # =============
+        loss_dict['total_loss'] = sum(loss_dict.values())
+        self._forward_learn_cnt += 1
+        self._target_model.update(self._learn_model.state_dict())
+        return {
+            'cur_lr_actor': self._optimizer_actor.defaults['lr'],
+            'cur_lr_critic': self._optimizer_critic.defaults['lr'],
+            # 'q_value': np.array(q_value).mean(),
+            'action': data.get('action').mean(),
+            'priority': td_error_per_sample.abs().tolist(),
+            'td_error': td_error_per_sample.abs().mean(),
+            **loss_dict,
+            **q_value_dict,
+        }
+
+    def _forward_eval(self, data: dict) -> dict:
+        r"""
+        Overview:
+            Forward function of eval mode, similar to ``self._forward_collect``.
+        Arguments:
+            - data (:obj:`Dict[str, Any]`): Dict type data, stacked env data for predicting policy_output(action), \
+                values are torch.Tensor or np.ndarray or dict/list combinations, keys are env_id indicated by integer.
+        Returns:
+            - output (:obj:`Dict[int, Any]`): The dict of predicting action for the interaction with env.
+        ReturnsKeys
+            - necessary: ``action``
+            - optional: ``logit``
+        """
+        data_id = list(data.keys())
+        data = default_collate(list(data.values()))
+        if self.cfg.collect.normalize_states:
+            data = (data - self._mean) / self._std
+        if self._cuda:
+            data = to_device(data, self._device)
+        self._eval_model.eval()
+        with torch.no_grad():
+            output = self._eval_model.forward(data, mode='compute_actor')
+        if self._cuda:
+            output = to_device(output, 'cpu')
+        output = default_decollate(output)
+        return {i: d for i, d in zip(data_id, output)}
+
+    def set_norm_statistics(self, mean: float, std: float) -> None:
+        r"""
+        Overview:
+            Set (mean, std) for state normalization.
+        Arguments:
+            - mean (:obj:`float`): Float type data, the mean of state in offlineRL dataset.
+            - std (:obj:`float`): Float type data, the std of state in offlineRL dataset.
+        Returns:
+            - None
+        """
+        self._mean = mean
+        self._std = std
