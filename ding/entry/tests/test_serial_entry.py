c5af1cf240400c570de21290a850c5c736ad2619
Weiyuhong-1998 <83390487+Weiyuhong-1998@users.noreply.github.com>
2021-10-25 14:56:22 +0800

    test(wyh): add more unittest for ppo and sac policy (#104)
    * fix(wyh):reward model test
    
    * fix(wyh):sac ppo test
    
    * fix(wyh):ppo_continuous test
    
    * fix(wyh):style
    
    * fix(wyh):ppo test
    
    Co-authored-by: Swain <niuyazhe314@outlook.com>

--- ding/entry/tests/test_serial_entry.py
+++ ding/entry/tests/test_serial_entry.py
@@ -6,6 +6,8 @@ from copy import deepcopy
 from ding.entry import serial_pipeline, collect_demo_data, serial_pipeline_offline
 from dizoo.classic_control.cartpole.config.cartpole_dqn_config import cartpole_dqn_config, cartpole_dqn_create_config
 from dizoo.classic_control.cartpole.config.cartpole_ppo_config import cartpole_ppo_config, cartpole_ppo_create_config
+from dizoo.classic_control.cartpole.config.cartpole_ppo_offpolicy_config import cartpole_ppo_offpolicy_config, \
+    cartpole_ppo_offpolicy_create_config
 from dizoo.classic_control.cartpole.config.cartpole_a2c_config import cartpole_a2c_config, cartpole_a2c_create_config
 from dizoo.classic_control.cartpole.config.cartpole_impala_config import cartpole_impala_config, cartpole_impala_create_config  # noqa
 from dizoo.classic_control.cartpole.config.cartpole_rainbow_config import cartpole_rainbow_config, cartpole_rainbow_create_config  # noqa
@@ -127,10 +129,21 @@ def test_qrdqn():
 
 @pytest.mark.unittest
 def test_ppo():
-    config = [deepcopy(cartpole_ppo_config), deepcopy(cartpole_ppo_create_config)]
+    config = [deepcopy(cartpole_ppo_offpolicy_config), deepcopy(cartpole_ppo_offpolicy_create_config)]
     config[0].policy.learn.update_per_collect = 1
     try:
-        ppo_main(config[0], seed=0, max_iterations=1)
+        serial_pipeline(config, seed=0, max_iterations=1)
+    except Exception:
+        assert False, "pipeline fail"
+
+
+@pytest.mark.unittest
+def test_ppo_nstep_return():
+    config = [deepcopy(cartpole_ppo_offpolicy_config), deepcopy(cartpole_ppo_offpolicy_create_config)]
+    config[0].policy.learn.update_per_collect = 1
+    config[0].policy.nstep_return = True
+    try:
+        serial_pipeline(config, seed=0, max_iterations=1)
     except Exception:
         assert False, "pipeline fail"
 
@@ -139,6 +152,7 @@ def test_ppo():
 def test_sac():
     config = [deepcopy(pendulum_sac_config), deepcopy(pendulum_sac_create_config)]
     config[0].policy.learn.update_per_collect = 1
+    config[0].policy.learn.auto_alpha = False
     try:
         serial_pipeline(config, seed=0, max_iterations=1)
     except Exception:
@@ -149,7 +163,20 @@ def test_sac():
 def test_sac_auto_alpha():
     config = [deepcopy(pendulum_sac_config), deepcopy(pendulum_sac_create_config)]
     config[0].policy.learn.update_per_collect = 1
-    config[0].policy.learn.is_auto_alpha = True
+    config[0].policy.learn.auto_alpha = True
+    config[0].policy.learn.log_space = False
+    try:
+        serial_pipeline(config, seed=0, max_iterations=1)
+    except Exception:
+        assert False, "pipeline fail"
+
+
+@pytest.mark.unittest
+def test_sac_log_space():
+    config = [deepcopy(pendulum_sac_config), deepcopy(pendulum_sac_create_config)]
+    config[0].policy.learn.update_per_collect = 1
+    config[0].policy.learn.auto_alpha = True
+    config[0].policy.learn.log_space = True
     try:
         serial_pipeline(config, seed=0, max_iterations=1)
     except Exception:
--- dizoo/classic_control/pendulum/config/__init__.py
+++ dizoo/classic_control/pendulum/config/__init__.py
@@ -2,5 +2,6 @@ from .pendulum_ddpg_config import pendulum_ddpg_config, pendulum_ddpg_create_con
 from .pendulum_td3_config import pendulum_td3_config, pendulum_td3_create_config
 from .pendulum_sac_config import pendulum_sac_config, pendulum_sac_create_config
 from .pendulum_d4pg_config import pendulum_d4pg_config, pendulum_d4pg_create_config
+from .pendulum_ppo_config import pendulum_ppo_config, pendulum_ppo_create_config
 from .pendulum_td3_bc_config import pendulum_td3_bc_config, pendulum_td3_bc_create_config
 from .pendulum_td3_data_generation_config import pendulum_td3_generation_config, pendulum_td3_generation_create_config
