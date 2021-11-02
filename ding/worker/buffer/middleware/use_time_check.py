d066372a281cdec25cf48e672045da62026bf4c0
niuyazhe <niuyazhe@sensetime.com>
2021-11-02 17:11:20 +0800

    feature(nyz): add staleness check middleware and polish buffer

--- ding/worker/buffer/middleware/use_time_check.py
+++ ding/worker/buffer/middleware/use_time_check.py
@@ -1,12 +1,11 @@
 from typing import Callable, Any, List
-from collections import deque
 
 
-def use_time_check(max_use: int = float("inf")) -> Callable:
+def use_time_check(buffer_: 'Buffer', max_use: int = float("inf")) -> Callable:  # noqa
     """
     Overview:
         This middleware aims to check the usage times of data in buffer. If the usage times of a data is
-        greater than max_use, this data will be removed from buffer as soon as possible.
+        greater than or equal to max_use, this data will be removed from buffer as soon as possible.
     """
 
     def push(chain: Callable, data: Any, *args, **kwargs) -> None:
@@ -17,20 +16,29 @@ def use_time_check(max_use: int = float("inf")) -> Callable:
         return chain(data, *args, **kwargs)
 
     def sample(chain: Callable, *args, **kwargs) -> List[Any]:
-        kwargs['return_index'] = True
-        kwargs['return_meta'] = True
-        data = chain(*args, **kwargs)
+        data = chain(return_index=True, return_meta=True, *args, **kwargs)
+
         for i, (d, idx, meta) in enumerate(data):
             meta['use_count'] += 1
             if meta['use_count'] >= max_use:
-                print('max_use trigger')  # TODO(nyz)
+                buffer_.delete(idx)
+
+        return_index = kwargs.get('return_index', False)
+        return_meta = kwargs.get('return_meta', False)
+        if return_index and not return_meta:
+            data = list(map(lambda item: (item[0], item[1]), data))
+        elif not return_index and return_meta:
+            data = list(map(lambda item: (item[0], item[2]), data))
+        elif not return_index and not return_meta:
+            data = list(map(lambda item: item[0], data))
+
         return data
 
-    def _immutable_object(action: str, chain: Callable, *args, **kwargs) -> Any:
+    def _use_time_check(action: str, chain: Callable, *args, **kwargs) -> Any:
         if action == "push":
             return push(chain, *args, **kwargs)
         elif action == "sample":
             return sample(chain, *args, **kwargs)
         return chain(*args, **kwargs)
 
-    return _immutable_object
+    return _use_time_check
--- ding/worker/buffer/tests/test_middleware.py
+++ ding/worker/buffer/tests/test_middleware.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 from ding.worker.buffer import Buffer, DequeStorage
-from ding.worker.buffer.middleware import clone_object, use_time_check
+from ding.worker.buffer.middleware import clone_object, use_time_check, staleness_check
 
 
 @pytest.mark.unittest
@@ -36,14 +36,15 @@ def test_clone_object():
             raise Exception("Unexpected type")
 
 
-@pytest.mark.tmp
+def get_data():
+    return {'obs': torch.randn(4), 'reward': torch.randn(1), 'info': 'xxx'}
+
+
+@pytest.mark.unittest
 def test_use_time_check():
-
-    def get_data():
-        return {'obs': torch.randn(4), 'reward': torch.randn(1), 'info': 'xxx'}
-
     N = 6
-    buffer = Buffer(DequeStorage(maxlen=10)).use(use_time_check(max_use=2))
+    buffer = Buffer(DequeStorage(maxlen=10))
+    buffer.use(use_time_check(buffer, max_use=2))
 
     for _ in range(N):
         buffer.push(get_data())
@@ -51,5 +52,27 @@ def test_use_time_check():
     for i in range(2):
         data = buffer.sample(size=N, replace=False)
         assert len(data) == N
-        print('sample i')
-    buffer.sample(size=6, replace=False)
+    with pytest.raises(ValueError):
+        buffer.sample(size=1, replace=False)
+
+
+@pytest.mark.unittest
+def test_staleness_check():
+    N = 6
+    buffer = Buffer(DequeStorage(maxlen=10))
+    buffer.use(staleness_check(buffer, max_staleness=10))
+
+    with pytest.raises(AssertionError):
+        buffer.push(get_data())
+    for _ in range(N):
+        buffer.push(get_data(), meta={'train_iter_data_collected': 0})
+    data = buffer.sample(size=N, replace=False, train_iter_sample_data=9)
+    assert len(data) == N
+    data = buffer.sample(size=N, replace=False, train_iter_sample_data=10)  # edge case
+    assert len(data) == N
+    for _ in range(2):
+        buffer.push(get_data(), meta={'train_iter_data_collected': 5})
+    assert buffer.count() == 8
+    with pytest.raises(ValueError):
+        data = buffer.sample(size=N, replace=False, train_iter_sample_data=11)
+    assert buffer.count() == 2
