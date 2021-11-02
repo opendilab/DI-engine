1ecd8b45f2f578a0ec90d77ada45f8e074d78ca2
Davide Liu <41103541+davide97l@users.noreply.github.com>
2021-11-02 16:15:14 +0800

    Update walker2d_ddpg_gail_config.py

--- dizoo/mujoco/config/walker2d_ddpg_gail_config.py
+++ dizoo/mujoco/config/walker2d_ddpg_gail_config.py
@@ -20,7 +20,7 @@ walker2d_ddpg_gail_default_config = dict(
         learning_rate=1e-3,
         update_per_collect=100,
         expert_data_path='walker2d_ddpg/expert_data_train.pkl',
-        load_path='walker2d_ddpg_gail/reward_model/ckpt/best.pth.tar',
+        load_path='walker2d_ddpg_gail/reward_model/ckpt/ckpt_best.pth.tar',
         collect_count=100000,
         target_new_data_count=1,
     ),
