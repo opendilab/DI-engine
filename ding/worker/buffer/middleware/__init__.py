8cb1c7ce5079ac8443d34f9ad7072322ab4726da
Xu Jingxin <sailxjx@gmail.com>
2021-11-09 11:01:35 +0800

    Padding after sampling from buffer

--- ding/worker/buffer/middleware/__init__.py
+++ ding/worker/buffer/middleware/__init__.py
@@ -2,3 +2,4 @@ from .clone_object import clone_object
 from .use_time_check import use_time_check
 from .staleness_check import staleness_check
 from .priority import priority
+from .padding import padding
