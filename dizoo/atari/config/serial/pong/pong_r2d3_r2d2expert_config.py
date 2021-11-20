e11d43a3e807fa98d9555ccf445ef520c5a456e8
puyuan1996 <2402552459@qq.com>
2021-11-20 20:05:27 +0800

    polish(pu): polish pong r2d3 config

--- dizoo/atari/config/serial/pong/pong_r2d3_r2d2expert_config.py
+++ dizoo/atari/config/serial/pong/pong_r2d3_r2d2expert_config.py
@@ -6,14 +6,12 @@ module_path = os.path.dirname(__file__)
 
 collector_env_num = 8
 evaluator_env_num = 5
-expert_replay_buffer_size=1000  #TODO 1000
+expert_replay_buffer_size=int(5e3)
 
 """agent config"""
 pong_r2d3_config = dict(
-    # exp_name='debug_pong_r2d3_r2d2expert_k0_pho1-4_rbs2e4_l2_run2',
-    # exp_name='debug_pong_r2d3_r2d2expert_k100_pho1-4_rbs2e4_l2',
-    # exp_name='debug_pong_r2d3_r2d2expert_k0_pho1-256_bs512_rbs2e4_l2',
-    exp_name='debug_pong_r2d3_r2d2expert_k0_pho1-256_bs64_rbs2e4_l2',
+    exp_name='debug_pong_r2d3_r2d2expert_k0_pho1-4_rbs1e4_ds5e3',
+    # exp_name='debug_pong_r2d3_r2d2expert_k100_pho1-4_rbs1e4_ds5e3',
 
     env=dict(
         # Whether to use shared memory. Only effective if "env_manager_type" is 'subprocess'
@@ -50,7 +48,7 @@ pong_r2d3_config = dict(
             # in most environments
             value_rescale=True,
             update_per_collect=8,
-            batch_size=64,#512,#64,#512,#64, # TODO(pu)
+            batch_size=64,  # 128 TODO(pu)
             learning_rate=0.0005,
             target_update_theta=0.001,
             # DQFD related parameters
@@ -59,7 +57,7 @@ pong_r2d3_config = dict(
             lambda3=1e-5,  # L2  it's very important to set Adam optimizer optim_type='adamw'.
             lambda_one_step_td=1.0,  # 1-step return
             margin_function=0.8,  # margin function in JE, here we implement this as a constant
-            per_train_iter_k=0,#100,  # TODO(pu)
+            per_train_iter_k=0,  # 100 TODO(pu)
         ),
         collect=dict(
             # NOTE it is important that don't include key n_sample here, to make sure self._traj_len=INF
@@ -67,7 +65,7 @@ pong_r2d3_config = dict(
             env_num=collector_env_num,
             # The hyperparameter pho, the demo ratio, control the propotion of data coming\
             # from expert demonstrations versus from the agent's own experience.
-            pho=1/256,#1/256,#1/256,  # TODO(pu), 1/256,
+            pho=1/4,  # 256 TODO(pu)
         ),
         eval=dict(env_num=evaluator_env_num, ),
         other=dict(
@@ -78,7 +76,7 @@ pong_r2d3_config = dict(
                 decay=100000,
             ),
             replay_buffer=dict(
-                replay_buffer_size=int(2e4),  # TODO(pu)
+                replay_buffer_size=int(1e4),  # TODO(pu)
                 # (Float type) How much prioritization is used: 0 means no prioritization while 1 means full prioritization
                 alpha=0.6,
                 # (Float type)  How much correction is used: 0 means no correction while 1 means full correction
@@ -103,10 +101,8 @@ create_config = pong_r2d3_create_config
 
 """export config"""
 expert_pong_r2d3_config = dict(
-    # exp_name='expert_pong_r2d3_r2d2expert_k0_pho1-4_rbs2e4_l2_run2',
-    # exp_name='expert_pong_r2d3_r2d2expert_k100_pho1-4_rbs2e4_l2',
-    # exp_name='expert_pong_r2d3_r2d2expert_k0_pho1-256_bs512_rbs2e4_l2',
-    exp_name='expert_pong_r2d3_r2d2expert_k0_pho1-256_bs64_rbs2e4_l2',
+    exp_name='expert_pong_r2d3_r2d2expert_k0_pho1-4_rbs1e4_ds5e3',
+    # exp_name='expert_pong_r2d3_r2d2expert_k100_pho1-4_rbs1e4_ds5e3',
     env=dict(
         # Whether to use shared memory. Only effective if "env_manager_type" is 'subprocess'
         manager=dict(shared_memory=True, force_reproducibility=True),
@@ -126,7 +122,7 @@ expert_pong_r2d3_config = dict(
             obs_shape=[4, 84, 84],
             action_shape=6,
             # encoder_hidden_size_list=[64, 64, 128],  # ppo expert policy
-            encoder_hidden_size_list=[128, 128, 512],  # r2d2
+            encoder_hidden_size_list=[128, 128, 512],  # r2d2 expert policy
             # actor_head_hidden_size=128,
             # critic_head_hidden_size=128,
         ),
@@ -140,7 +136,6 @@ expert_pong_r2d3_config = dict(
             # NOTE it is important that don't include key n_sample here, to make sure self._traj_len=INF
             each_iter_n_sample=32,
             # Users should add their own path here (path should lead to a well-trained model)
-            # demonstration_info_path='dizoo/atari/config/serial/pong/demo_path/ppo-off_iteration_16127.pth.tar',
             # demonstration_info_path=module_path + '/demo_path/ppo-off_iteration_16127.pth.tar',
             # demonstration_info_path=module_path + '/demo_path/ppo-off_ckpt_best.pth.tar',
             demonstration_info_path=module_path + '/demo_path/r2d2_iteration_15000.pth.tar',
