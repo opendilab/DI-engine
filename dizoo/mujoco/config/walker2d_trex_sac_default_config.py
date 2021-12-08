8b484f34695acd28f4307e69d5716b08f761bd51
Will-Nie <61083608+Will-Nie@users.noreply.github.com>
2021-12-08 22:39:21 +0800

    feature(nyp): add Trex algorithm (#119)
    * add trex algorithm for pong
    
    * sort style
    
    * add atari, ll,cp; fix device, collision; add_ppo
    
    * add accuracy evaluation
    
    * correct style
    
    * add seed to make sure results are replicable
    
    * remove useless part in cum return  of model part
    
    * add mujoco onppo training pipeline; ppo config
    
    * improve style
    
    * add sac training config for mujoco
    
    * add log, add save data; polish config
    
    * logger; hyperparameter;walker
    
    * correct style
    
    * modify else condition
    
    * change rnd to trex
    
    * revise according to comments, add eposode collect
    
    * new collect mode for trex, fix all bugs, commnets
    
    * final change
    
    * polish after the final comment
    
    * add readme/test
    
    * add test for serial entry of trex/gcl
    
    * sort style

--- dizoo/mujoco/config/walker2d_trex_sac_default_config.py
+++ dizoo/mujoco/config/walker2d_trex_sac_default_config.py
@@ -1,16 +1,33 @@
 from easydict import EasyDict
 
-halfcheetah_sac_default_config = dict(
-    exp_name = 'halfcheetah_sac',
+walker2d_trex_sac_default_config = dict(
+    exp_name = 'walker2d_trex_sac',
     env=dict(
-        env_id='HalfCheetah-v3',
+        manager=dict(shared_memory=True, force_reproducibility=True),
+        env_id='Walker2d-v3',
         norm_obs=dict(use_norm=False, ),
         norm_reward=dict(use_norm=False, ),
         collector_env_num=1,
         evaluator_env_num=8,
         use_act_scale=True,
         n_evaluator_episode=8,
-        stop_value=12000,
+        stop_value=6000,
+    ),
+    reward_model=dict(
+        type='trex',
+        algo_for_model='sac',
+        env_id='Walker2d-v3',
+        min_snippet_length=30,
+        max_snippet_length=100,
+        checkpoint_min=100,
+        checkpoint_max=900,
+        checkpoint_step=100,
+        learning_rate=1e-5,
+        update_per_collect=1,
+        expert_model_path='abs model path',
+        reward_model_path='abs data path + ./walker2d.params',
+        continuous=True,
+        offline_data_path='asb data path',
     ),
     policy=dict(
         cuda=True,
@@ -29,7 +46,7 @@ halfcheetah_sac_default_config = dict(
             learning_rate_q=1e-3,
             learning_rate_policy=1e-3,
             learning_rate_alpha=3e-4,
-            ignore_done=True,
+            ignore_done=False,
             target_theta=0.005,
             discount_factor=0.99,
             alpha=0.2,
@@ -46,20 +63,20 @@ halfcheetah_sac_default_config = dict(
     ),
 )
 
-halfcheetah_sac_default_config = EasyDict(halfcheetah_sac_default_config)
-main_config = halfcheetah_sac_default_config
+walker2d_trex_sac_default_config = EasyDict(walker2d_trex_sac_default_config)
+main_config = walker2d_trex_sac_default_config
 
-halfcheetah_sac_default_create_config = dict(
+walker2d_trex_sac_default_create_config = dict(
     env=dict(
         type='mujoco',
         import_names=['dizoo.mujoco.envs.mujoco_env'],
     ),
-    env_manager=dict(type='base'),
+    env_manager=dict(type='subprocess'),
     policy=dict(
         type='sac',
         import_names=['ding.policy.sac'],
     ),
     replay_buffer=dict(type='naive', ),
 )
-halfcheetah_sac_default_create_config = EasyDict(halfcheetah_sac_default_create_config)
-create_config = halfcheetah_sac_default_create_config
+walker2d_trex_sac_default_create_config = EasyDict(walker2d_trex_sac_default_create_config)
+create_config = walker2d_trex_sac_default_create_config
