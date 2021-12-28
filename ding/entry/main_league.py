4b1346a3815cf08281ef51ba513a01249b6013fe
Xu Jingxin <sailxjx@gmail.com>
2021-12-28 14:53:59 +0800

    Use wait_for in main_league

--- ding/entry/main_league.py
+++ ding/entry/main_league.py
@@ -34,12 +34,11 @@ def league_dispatching(task: Task, cfg, tb_logger, league, policies):
         league.judge_snapshot(player_info["player_id"])
 
     task.on("update_active_player", update_active_player)
-    task.router.register_rpc("update_active_player", update_active_player)
 
     def _league(ctx):
         import random
         num = random.random()
-        print("League dispatching on node {}".format(num))
+        print("League dispatching on node {}, {}".format(task.router.node_id, num))
         time.sleep(1)
         # One episode each round
         i = ctx.total_step % len(league.active_players_ids)
@@ -57,13 +56,13 @@ def league_dispatching(task: Task, cfg, tb_logger, league, policies):
             opponent_policy = policies[opponent_player_id].collect_mode
         # Watch out that in parallel mode, we should not send functions between processes, instead,
         # we should send the objects needed by the policies.
-        session = {
+        collect_session = {
             "policies": [policies[player_id].collect_mode, opponent_policy],
             "player_id": player_id,
             "player_ckpt_path": player_ckpt_path
         }
-        print("Player ID", session["player_id"])
-        task.emit("set_session", session)
+        print("Player ID", collect_session["player_id"])
+        task.emit("set_collect_session", collect_session)
 
         yield
 
@@ -80,18 +79,11 @@ def league_dispatching(task: Task, cfg, tb_logger, league, policies):
 
 
 def collecting(task: Task, cfg, tb_logger, player_ids):
-    session = None
     collectors = {}
-
-    def set_session(new_session):
-        nonlocal session
-        session = new_session
-
-    task.on("set_session", set_session)
 
     def _collect(ctx):
         print("  Collecting on node {}".format(task.router.node_id))
-        time.sleep(1)
+        collect_session = task.wait_for("set_collect_session")[0][0]
 
         if not collectors:
             for player_id in player_ids:
@@ -108,43 +100,31 @@ def collecting(task: Task, cfg, tb_logger, player_ids):
                     instance_name=player_id + '_colllector',
                 )
 
-        # session = task.wait_for("set_session")
-        nonlocal session
-        while not session:  # Waiting
-            time.sleep(0.1)
-
-        collector = collectors[session["player_id"]]
-        collector.reset_policy(session["policies"])
+        collector = collectors[collect_session["player_id"]]
+        collector.reset_policy(collect_session["policies"])
         train_data, episode_info = collector.collect()  # TODO Do we need train_iter?
         train_data, episode_info = train_data[0], episode_info[0]  # only use launch player data for training
         ctx.episode_info = episode_info
         for d in train_data:
             d['adv'] = d['reward']
 
-        session["train_data"] = train_data
-        session["envstep"] = collector.envstep
-        task.emit("set_session", session)  # Shoot and forget
-        del session["policies"]
-        task.router.send_rpc("set_session", session)
-
-        session = None
+        learn_session = {
+            "player_id": collect_session["player_id"],
+            "train_data": train_data,
+            "envstep": collector.envstep,
+            "player_ckpt_path": collect_session["player_ckpt_path"]
+        }
+        task.emit("set_learn_session", learn_session)  # Shoot and forget
 
     return _collect
 
 
 def learning(task: Task, cfg, tb_logger, player_ids, policies):
     learners = {}
-    session = None
-
-    def set_session(new_session):
-        nonlocal session
-        session = new_session
-
-    task.on("set_session", set_session)
-    task.router.register_rpc("set_session", set_session)
 
     def _learn(ctx):
         print("    Learning on node {}".format(task.router.node_id))
+        learn_session = task.wait_for("set_learn_session")[0][0]
         time.sleep(1)
 
         if not learners:
@@ -158,21 +138,14 @@ def learning(task: Task, cfg, tb_logger, player_ids, policies):
                     instance_name=player_id + '_learner'
                 )
 
-        nonlocal session
-        while not (session and session.get("train_data")):  # TODO: Use task.wait_for() to replace local variable
-            time.sleep(0.1)
-
-        learner = learners[session["player_id"]]
+        learner = learners[learn_session["player_id"]]
         state_dict = learner.policy.state_dict()
-        torch.save(state_dict, session["player_ckpt_path"])  # Save to local
+        torch.save(state_dict, learn_session["player_ckpt_path"])  # Save to local
 
         player_info = learner.learn_info
-        player_info['player_id'] = session["player_id"]
+        player_info['player_id'] = learn_session["player_id"]
 
-        task.router.send_rpc("update_active_player", player_info)  # Broadcast to other processes
         task.emit("update_active_player", player_info)  # Broadcast to other middleware
-
-        session = None
 
     return _learn
 
