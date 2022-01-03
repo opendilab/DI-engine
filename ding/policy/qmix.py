30159a52651b521c2e87f41d7a4fb09c7bd1cce9
puyuan1996 <2402552459@qq.com>
2022-01-03 15:42:25 +0800

    polish(pu):polish qmix config

--- ding/policy/qmix.py
+++ ding/policy/qmix.py
@@ -121,9 +121,8 @@ class QMIXPolicy(Policy):
         self._priority = self._cfg.priority
         self._priority_IS_weight = self._cfg.priority_IS_weight
         assert not self._priority and not self._priority_IS_weight, "Priority is not implemented in QMIX"
-        self._optimizer = RMSprop(
-            params=self._model.parameters(), lr=self._cfg.learn.learning_rate, alpha=0.99, eps=0.00001
-        )
+        self._optimizer = RMSprop(params=self._model.parameters(), lr=self._cfg.learn.learning_rate, alpha=0.99,
+                                  eps=0.00001, weight_decay=1e-5)
         self._gamma = self._cfg.learn.discount_factor
 
         self._target_model = copy.deepcopy(self._model)
