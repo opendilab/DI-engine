659b1a0788b361492015dd9c75f51ace25dc112f
Xu Jingxin <sailxjx@gmail.com>
2022-01-12 19:28:04 +0800

    Fix encoding

--- ding/scripts/main_league.sh
+++ ding/scripts/main_league.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BASEDIR=$(dirname "$0")
+BASEDIR=$(dirname "$0")/../entry
 
 kill_descendant_processes() {
   local pid="$1"
@@ -27,26 +27,26 @@ ditask --package $BASEDIR \
   --topology alone \
   --labels league,collect &
 
-ditask --package $BASEDIR \
-  --main main_league.main \
-  --parallel-workers 3 \
-  --protocol tcp \
-  --address 127.0.0.1 \
-  --ports 50525 \
-  --node-ids 10 \
-  --topology alone \
-  --labels learn \
-  --attach-to tcp://127.0.0.1:50515 &
+# ditask --package $BASEDIR \
+#   --main main_league.main \
+#   --parallel-workers 3 \
+#   --protocol tcp \
+#   --address 127.0.0.1 \
+#   --ports 50525 \
+#   --node-ids 10 \
+#   --topology alone \
+#   --labels learn \
+#   --attach-to tcp://127.0.0.1:50515 &
 
-ditask --package $BASEDIR \
-  --main main_league.main \
-  --parallel-workers 1 \
-  --address 127.0.0.1 \
-  --protocol tcp \
-  --ports 50535 \
-  --node-ids 20 \
-  --topology alone \
-  --labels evaluate \
-  --attach-to tcp://127.0.0.1:50515,tcp://127.0.0.1:50525,tcp://127.0.0.1:50526,tcp://127.0.0.1:50527 &
+# ditask --package $BASEDIR \
+#   --main main_league.main \
+#   --parallel-workers 1 \
+#   --address 127.0.0.1 \
+#   --protocol tcp \
+#   --ports 50535 \
+#   --node-ids 20 \
+#   --topology alone \
+#   --labels evaluate \
+#   --attach-to tcp://127.0.0.1:50515,tcp://127.0.0.1:50525,tcp://127.0.0.1:50526,tcp://127.0.0.1:50527 &
 
 sleep 10000
--- ding/scripts/main_league_slurm.sh
+++ ding/scripts/main_league_slurm.sh
@@ -1,39 +1,7 @@
 #!/usr/bin/env bash
 
+export LC_ALL=en_US.utf-8
+export LANG=en_US.utf-8
 BASEDIR=$(dirname "$0")
-
 # srun -p Cerebra_Share --quotatype=reserved --mpi=pmi2 -n6 --ntasks-per-node=3
-ditask --package $BASEDIR/../entry --main main_league.main --platform-spec '
-{
-  "type": "slurm",
-  "tasks": [
-    {
-      "labels": "league,collect",
-      "node_ids": 10,
-    },
-    {
-      "labels": "league,collect",
-      "node_ids": 11
-    },
-    {
-      "labels": "evaluate",
-      "node_ids": 20,
-      "attach_to": "$node.10,$node.11"
-    },
-    {
-      "labels": "learn",
-      "node_ids": 31,
-      "attach_to": "$node.10,$node.11,$node.20"
-    },
-    {
-      "labels": "learn",
-      "node_ids": 32,
-      "attach_to": "$node.10,$node.11,$node.20"
-    },
-    {
-      "labels": "learn",
-      "node_ids": 33,
-      "attach_to": "$node.10,$node.11,$node.20"
-    }
-  ]
-}'
+ditask --package $BASEDIR/../entry --main main_league.main --platform-spec '{"type":"slurm","tasks":[{"labels":"league,collect","node_ids":10},{"labels":"league,collect","node_ids":11},{"labels":"evaluate","node_ids":20,"attach_to":"$node.10,$node.11"},{"labels":"learn","node_ids":31,"attach_to":"$node.10,$node.11,$node.20"},{"labels":"learn","node_ids":32,"attach_to":"$node.10,$node.11,$node.20"},{"labels":"learn","node_ids":33,"attach_to":"$node.10,$node.11,$node.20"}]}'
