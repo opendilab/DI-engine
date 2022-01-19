fix(jrn): modify the tensor type to match the JE computation in dqfd_nstep_td_error and provide a alternative optimizer

ding/policy/dqfd.py
@@ -137,6 +137,7 @@ class DQFDPolicy(DQNPolicy):
         self._priority_IS_weight = self._cfg.priority_IS_weight
         # Optimizer
         self._optimizer = Adam(self._model.parameters(), lr=self._cfg.learn.learning_rate, weight_decay=self.lambda3)
+        # self._optimizer = torch.optim.AdamW(self._model.parameters(), lr=self._cfg.learn.learning_rate, weight_decay=self.lambda3)  # alternative optimizer
 
         self._gamma = self._cfg.discount_factor
         self._nstep = self._cfg.nstep
@@ -195,6 +196,7 @@ class DQFDPolicy(DQNPolicy):
             target_q_action = self._learn_model.forward(data['next_obs'])['action']
             target_q_action_one_step = self._learn_model.forward(data['next_obs_1'])['action']
 
+        is_expert = data['is_expert'].float()  # modify the tensor type to match the JE computation in dqfd_nstep_td_error
         data_n = dqfd_nstep_td_data(
             q_value,
             target_q_value,
@@ -206,7 +208,7 @@ class DQFDPolicy(DQNPolicy):
             data['weight'],
             target_q_value_one_step,
             target_q_action_one_step,
-            data['is_expert']  # set is_expert flag(expert 1, agent 0)
+            is_expert  # set is_expert flag(expert 1, agent 0)
         )
         value_gamma = data.get('value_gamma')
         loss, td_error_per_sample = dqfd_nstep_td_error(