2b21b822529b2742a3380947616e9747977fe6f5
Davide Liu <41103541+davide97l@users.noreply.github.com>
2022-01-25 13:32:01 +0800

    Update test_r2d3.py

--- ding/policy/tests/test_r2d3.py
+++ ding/policy/tests/test_r2d3.py
@@ -4,7 +4,9 @@ from easydict import EasyDict
 from ding.policy.r2d3 import R2D3Policy
 from ding.utils.data import offline_data_save_type
 from torch.utils.tensorboard import SummaryWriter
+from ding.model.wrapper.model_wrappers import ArgmaxSampleWrapper, HiddenStateWrapper, EpsGreedySampleWrapper
 import os
+from typing import List
 
 obs_space = 5
 action_space = 4
@@ -68,6 +70,44 @@ for i in range(100):
     data.append(d)
 
 
+@pytest.mark.parametrize('cfg', [cfg])
+@pytest.mark.unittest
+def test_dataset_1d(cfg):
+    policy = R2D3Policy(cfg, enable_field=['collect', 'eval'])
+    policy._init_learn()
+    assert type(policy._learn_model) == ArgmaxSampleWrapper
+    assert type(policy._target_model) == HiddenStateWrapper
+    policy._reset_learn()
+    policy._reset_learn([0])
+    state = policy._state_dict_learn()
+    policy._load_state_dict_learn(state)
+    policy._init_collect()
+    assert type(policy._collect_model) == EpsGreedySampleWrapper
+    policy._reset_collect()
+    policy._reset_collect([0])
+    policy._init_eval()
+    assert type(policy._eval_model) == ArgmaxSampleWrapper
+    policy._reset_eval()
+    policy._reset_eval([0])
+    assert policy.default_model()[0] == 'drqn'
+    var = policy._monitor_vars_learn()
+    assert type(var) == list
+    assert sum([type(s) == str for s in var]) == len(var)
 
-policy = R2D3Policy(cfg, enable_field=['collect', 'eval'])
-policy._init_learn()
+    '''data = data_1d
+    cfg = EasyDict(cfg)
+    policy = GailRewardModel(cfg, device, tb_logger=SummaryWriter())
+    policy.load_expert_data()
+    assert len(policy.expert_data) == 20
+    state = policy.state_dict()
+    policy.load_state_dict(state)
+    policy.collect_data(data)
+    assert len(policy.train_data) == 20
+    for _ in range(5):
+        policy.train()
+    policy.estimate(data)
+    assert 'reward' in data[0].keys()
+    policy.clear_data()
+    assert len(policy.train_data) == 0
+    if os.path.exists(expert_data_path_1d):
+        os.remove(expert_data_path_1d)'''
