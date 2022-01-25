aba0c629f87649854091e9e59d948f83962e3e1e
Xu Jingxin <sailxjx@gmail.com>
2022-01-25 10:29:10 +0800

    feature(xjx): some incompatible refactorings (#195)
    Changelog:
    
    Merge task.emit and task.emit_remote methods.
    Support slurm platform in ditask.
    Move code of middleware in main_league into middleware dir.
    Change 'task.emit' event to 'task._emit'.
    Fix some bugs in Parallel.
    Forbidden syncing context between processes.
    Support str in match_labels

---
+++ ding/framework/middleware/__init__.py
@@ -0,0 +1,4 @@
+from .league_collector import league_collector
+from .league_dispatcher import league_dispatcher
+from .league_evaluator import league_evaluator
+from .league_learner import league_learner
