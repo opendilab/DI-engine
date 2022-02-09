b1fe2934b540c192c1eb936440490bc602b89891
weiyuhong <weiyuhong@sensetime.com>
2022-02-09 21:09:36 +0800

    test(wyh):trex tests

--- ding/entry/tests/test_serial_entry_trex.py
+++ ding/entry/tests/test_serial_entry_trex.py
@@ -11,6 +11,10 @@ from dizoo.classic_control.cartpole.config.cartpole_trex_offppo_config import ca
      cartpole_trex_ppo_offpolicy_create_config
 from dizoo.classic_control.cartpole.config.cartpole_ppo_offpolicy_config import cartpole_ppo_offpolicy_config,\
      cartpole_ppo_offpolicy_create_config
+from dizoo.atari.config.serial.pong.pong_ppo_offpolicy_config import pong_ppo_config, \
+     pong_ppo_create_config
+from dizoo.atari.config.serial.pong.pong_trex_offppo_config import pong_trex_ppo_config, \
+     pong_trex_ppo_create_config
 from ding.entry.application_entry_trex_collect_data import trex_collecting_data
 
 
@@ -25,6 +29,30 @@ def test_serial_pipeline_reward_model_trex():
     config[0].reward_model.offline_data_path = os.path.abspath(config[0].reward_model.offline_data_path)
     config[0].reward_model.reward_model_path = config[0].reward_model.offline_data_path + '/cartpole.params'
     config[0].reward_model.expert_model_path = './cartpole_ppo_offpolicy'
+    config[0].reward_model.expert_model_path = os.path.abspath(config[0].reward_model.expert_model_path)
+    config[0].reward_model.checkpoint_max = 100
+    config[0].reward_model.checkpoint_step = 100
+    config[0].reward_model.num_snippets = 100
+    args = EasyDict({'cfg': deepcopy(config), 'seed': 0, 'device': 'cpu'})
+    trex_collecting_data(args=args)
+    try:
+        serial_pipeline_reward_model_trex(config, seed=0, max_iterations=1)
+        os.popen('rm -rf {}'.format(config[0].reward_model.offline_data_path))
+    except Exception:
+        assert False, "pipeline fail"
+
+
+@pytest.mark.unittest
+def test_serial_pipeline_pong_reward_model_trex():
+    config = [deepcopy(pong_ppo_config), deepcopy(pong_ppo_create_config)]
+    config[0].policy.learn.learner.hook.save_ckpt_after_iter = 100
+    expert_policy = serial_pipeline(config, seed=0, max_iterations=10)
+
+    config = [deepcopy(pong_trex_ppo_config), deepcopy(pong_trex_ppo_create_config)]
+    config[0].reward_model.offline_data_path = './pong_trex_offppo'
+    config[0].reward_model.offline_data_path = os.path.abspath(config[0].reward_model.offline_data_path)
+    config[0].reward_model.reward_model_path = config[0].reward_model.offline_data_path + '/pong.params'
+    config[0].reward_model.expert_model_path = './ppo_offpolicy_pong'
     config[0].reward_model.expert_model_path = os.path.abspath(config[0].reward_model.expert_model_path)
     config[0].reward_model.checkpoint_max = 100
     config[0].reward_model.checkpoint_step = 100
