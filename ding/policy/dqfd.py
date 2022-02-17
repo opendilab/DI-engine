fix(jrn): modify the tensor type to match the JE computation, provide an alternative optimizer and correct code style

ding/policy/dqfd.py
@@ -2,7 +2,7 @@ from typing import List, Dict, Any, Tuple
 from collections import namedtuple
 import copy
 import torch
-
+from torch.optim import AdamW
 from ding.torch_utils import Adam, to_device
 from ding.rl_utils import q_nstep_td_data, q_nstep_td_error, get_nstep_return_data, get_train_sample, \
     dqfd_nstep_td_error, dqfd_nstep_td_data
@@ -137,7 +137,7 @@ class DQFDPolicy(DQNPolicy):
         self._priority_IS_weight = self._cfg.priority_IS_weight
         # Optimizer
         # two optimizers: the performance of adamW is better than adam, so we recommend using the adamW.
-        self._optimizer = torch.optim.AdamW(self._model.parameters(), lr=self._cfg.learn.learning_rate, weight_decay=self.lambda3)
+        self._optimizer = AdamW(self._model.parameters(), lr=self._cfg.learn.learning_rate, weight_decay=self.lambda3)
         # self._optimizer = Adam(self._model.parameters(), lr=self._cfg.learn.learning_rate, weight_decay=self.lambda3)
 
         self._gamma = self._cfg.discount_factor
@@ -197,7 +197,8 @@ class DQFDPolicy(DQNPolicy):
             target_q_action = self._learn_model.forward(data['next_obs'])['action']
             target_q_action_one_step = self._learn_model.forward(data['next_obs_1'])['action']
 
-        is_expert = data['is_expert'].float()  # modify the tensor type to match the JE computation in dqfd_nstep_td_error
+        # modify the tensor type to match the JE computation in dqfd_nstep_td_error
+        is_expert = data['is_expert'].float()
         data_n = dqfd_nstep_td_data(
             q_value,
             target_q_value,