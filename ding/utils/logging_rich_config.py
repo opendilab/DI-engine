e9ca28060ae617ae77065549976d3dfb07fa5a7f
zjowowen <zjowowen@outlook.com>
2022-03-03 19:15:48 +0800

    Minor change.

--- ding/utils/logging_rich_config.py
+++ ding/utils/logging_rich_config.py
@@ -7,7 +7,20 @@ from rich.logging import RichHandler
 from rich import console
 
 
-def enable_rich_handler(level: int = logging.INFO, terminal_width: Optional[int] = None) -> None:
+def enable_rich_handler(
+        level: int = logging.INFO, terminal_width: Optional[int] = None, terminal_height: Optional[int] = None
+) -> None:
+    r'''
+    Overview:
+        Enable rich handler decoration to logger. Default logging.StreamHandler will be replaced.
+    Arguments:
+        - level (:obj:`int`): Logger Level for Rich handeler, default set to ``logging.INFO``.
+        - terminal_width (:obj:`int`): The designed terminal width for logging message.
+        - terminal_height (:obj:`int`): The designed terminal height for logging message,  
+         default set to ``None`` for an automatic dectection is activated.
+         If no terminal detected, a default value is set for Rich handeler.
+    '''
+
     width: Optional[int] = None
     height: Optional[int] = None
 
@@ -27,7 +40,7 @@ def enable_rich_handler(level: int = logging.INFO, terminal_width: Optional[int]
 
     # get_terminal_size can report 0, 0 if run from pseudo-terminal
     width = terminal_width or width or 285
-    height = height or 25
+    height = terminal_height or height or 25
 
     root = logging.getLogger()
     other_handlers = []
@@ -48,6 +61,12 @@ def enable_rich_handler(level: int = logging.INFO, terminal_width: Optional[int]
 
 
 def disable_rich_handler(level: int = logging.INFO) -> None:
+    r'''
+    Overview:
+        Disable rich handler decoration to logger. RichHandler will be replaced by logging.StreamHandler.
+    Arguments:
+        - level (:obj:`int`): Logger Level for Rich handeler, default set to ``logging.INFO``.
+    '''
     root = logging.getLogger()
     other_handlers = []
 
