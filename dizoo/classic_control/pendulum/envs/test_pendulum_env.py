fa6baa34dfc84593957548abdcdd6153557e1d79
LuciusMos <40848893+LuciusMos@users.noreply.github.com>
2022-03-06 15:32:14 +0800

    fix(zlx): add random_action method in envs (#231)
    * fixbug(zlx): Add random_action method in envs
    
    * fixbug(zlx): Fix comments by zms

--- dizoo/classic_control/pendulum/envs/test_pendulum_env.py
+++ dizoo/classic_control/pendulum/envs/test_pendulum_env.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 from easydict import EasyDict
+from torch import rand
 from dizoo.classic_control.pendulum.envs import PendulumEnv
 
 
@@ -14,7 +15,12 @@ class TestPendulumEnv:
         obs = env.reset()
         assert obs.shape == (3, )
         for i in range(10):
-            random_action = np.tanh(np.random.random(1))
+            # Both ``env.random_action()``, and utilizing ``np.random`` as well as action space,
+            # can generate legal random action.
+            if i < 5:
+                random_action = np.tanh(np.random.random(1))
+            else:
+                random_action = env.random_action()
             timestep = env.step(random_action)
             assert timestep.obs.shape == (3, )
             assert timestep.reward.shape == (1, )
