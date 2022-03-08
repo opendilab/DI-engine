eb8edb4a1a655a66abcbffbbfb56edb23c42c44b
nieyunpeng <nieyunpeng@sensetime.com>
2022-03-08 22:15:50 +0800

    perfect pong_sqil_config

--- dizoo/atari/config/serial/pong/pong_sqil_config.py
+++ dizoo/atari/config/serial/pong/pong_sqil_config.py
@@ -20,7 +20,7 @@ pong_sqil_config = dict(
             encoder_hidden_size_list=[128, 128, 512],
         ),
         nstep=3,
-        discount_factor=0.97,
+        discount_factor=0.97, # discount_factor: 0.97-0.99
         learn=dict(update_per_collect=10, batch_size=32, learning_rate=0.0001, target_update_freq=500, alpha=0.1), # alpha: 0.08-0.12
         collect=dict(n_sample=96, demonstration_info_path='model_path'
                      ),  # Users should add their own model path here. Model path should lead to a model (In DI-engine, it is ``ckpt_best.pth.tar``.)
