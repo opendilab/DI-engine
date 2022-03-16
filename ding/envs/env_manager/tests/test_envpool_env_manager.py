ae60f5be277ab34ed489701cc8ce61b63aabd64f
LiangxuanZhao <zhaoliangxuan@sensetime.com>
2022-03-17 00:15:50 +0800

    fixbug(zlx): add unittest for envpool env manager

---
+++ ding/envs/env_manager/tests/test_envpool_env_manager.py
@@ -0,0 +1,39 @@
+import time
+import signal
+import pytest
+import torch
+import numpy as np
+from easydict import EasyDict
+
+from ..envpool_env_manager import PoolEnvManager
+
+
+@pytest.mark.unittest
+class TestPoolEnvManager:
+
+    def test_naive(self):
+        env_manager_cfg = EasyDict({
+            'env_id': 'Pong-v5',
+            'env_num': 16,
+            'batch_size': 8,
+            'seed': 3,
+        })
+        env_manager = PoolEnvManager(env_manager_cfg)
+        assert env_manager._closed
+        env_manager.launch()
+        # Test step
+        start_time = time.time()
+        for count in range(20):
+            env_id = env_manager.ready_obs.keys()
+            action = {i: np.random.randint(4) for i in env_id}
+            timestep = env_manager.step(action)
+            if count > 10:
+                assert len(timestep) == env_manager_cfg.batch_size
+            print('Count {}'.format(count))
+            print([v.info for v in timestep.values()])
+            print([v.done for v in timestep.values()])
+        end_time = time.time()
+        print('total step time: {}'.format(end_time - start_time))
+        # Test close
+        env_manager.close()
+        assert env_manager._closed
