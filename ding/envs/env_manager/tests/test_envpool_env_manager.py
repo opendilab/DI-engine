03ab4db9e4c919ec7fc507ad2301a002b5c16db9
LiangxuanZhao <zhaoliangxuan@sensetime.com>
2022-03-17 10:40:17 +0800

    fixbug(zlx): Add pip install envpool in github unittest workflows

--- ding/envs/env_manager/tests/test_envpool_env_manager.py
+++ ding/envs/env_manager/tests/test_envpool_env_manager.py
@@ -2,11 +2,6 @@ import time
 import pytest
 import numpy as np
 from easydict import EasyDict
-try:
-    import envpool
-except ImportError:
-    import os
-    os.system('pip install envpool')
 
 from ..envpool_env_manager import PoolEnvManager
 
