05171f989d0419dba398a0e92b1e40e76376cf6e
Xu Jingxin <sailxjx@gmail.com>
2022-03-21 16:42:59 +0800

    Test exceptions in parallel

--- ding/framework/tests/test_parallel.py
+++ ding/framework/tests/test_parallel.py
@@ -58,3 +58,49 @@ def star_parallel_main():
 @pytest.mark.unittest
 def test_parallel_topology():
     Parallel.runner(n_parallel_workers=3, topology="star")(star_parallel_main)
+
+
+def uncaught_exception_main():
+    router = Parallel()
+    if router.node_id == 0:
+        time.sleep(0.1)
+        raise Exception("uncaught exception")
+    else:
+        time.sleep(0.2)
+
+
+@pytest.mark.unittest
+def test_uncaught_exception():
+    # Make one process crash, then the parent process will also crash and output the stack of the wrong process.
+    with pytest.raises(Exception) as exc_info:
+        Parallel.runner(n_parallel_workers=2, topology="mesh")(uncaught_exception_main)
+    e = exc_info._excinfo[1]
+    assert "uncaught exception" in str(e)
+
+
+def disconnected_main():
+    router = Parallel()
+
+    if router.node_id == 0:
+        # Receive two messages then exit
+        greets = []
+        router.on("greeting", lambda: greets.append("."))
+        for _ in range(10):
+            if len(greets) == 1:
+                break
+            else:
+                time.sleep(0.1)
+        assert len(greets) > 0
+    else:
+        # Send 10 greetings even if the target process is exited
+        for i in range(10):
+            router.emit("greeting")
+            time.sleep(0.1)
+        assert i == 9
+
+
+@pytest.mark.unittest
+def test_disconnected():
+    # Make one process exit normally and the rest will still run, even if the network request
+    # is not received by other processes.
+    Parallel.runner(n_parallel_workers=2, topology="mesh")(disconnected_main)
