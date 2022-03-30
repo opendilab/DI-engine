519e03ce71cd8a48ea0ea8ff000cf530f00cc440
Davide Liu <41103541+davide97l@users.noreply.github.com>
2022-03-30 14:23:39 +0800

    polish comment ant onppo traj flag

--- dizoo/mujoco/config/ant_onppo_config.py
+++ dizoo/mujoco/config/ant_onppo_config.py
@@ -1,6 +1,6 @@
 from easydict import EasyDict
 
-ant_ppo_default_config = dict(
+ant_ppo_config = dict(
     exp_name="ant_onppo_seed0",
     env=dict(
         env_id='Ant-v3',
@@ -32,12 +32,14 @@ ant_ppo_default_config = dict(
             clip_ratio=0.2,
             adv_norm=True,
             value_norm=True,
-            ignore_done=False,  # when we recompute advantage, we need the key done in data to split trajectories
+            # When we recompute advantage, we need the key done in data to split trajectories, so we must
+            # use 'ignore_done=False' here, but when we add key 'traj_flag' in data as the backup for key done,
+            # we could choose to use 'ignore_done=True'. 'traj_flag' indicates termination of trajectory.
+            ignore_done=False,
             grad_clip_type='clip_norm',
             grad_clip_value=0.5,
         ),
         collect=dict(
-            collector_env_num=10,
             n_sample=3200,
             unroll_len=1,
             discount_factor=0.99,
@@ -46,10 +48,10 @@ ant_ppo_default_config = dict(
         eval=dict(evaluator=dict(eval_freq=5000, )),
     ),
 )
-ant_ppo_default_config = EasyDict(ant_ppo_default_config)
-main_config = ant_ppo_default_config
+ant_ppo_config = EasyDict(ant_ppo_config)
+main_config = ant_ppo_config
 
-ant_ppo_create_default_config = dict(
+ant_ppo_create_config = dict(
     env=dict(
         type='mujoco',
         import_names=['dizoo.mujoco.envs.mujoco_env'],
@@ -57,8 +59,8 @@ ant_ppo_create_default_config = dict(
     env_manager=dict(type='subprocess'),
     policy=dict(type='ppo'),
 )
-ant_ppo_create_default_config = EasyDict(ant_ppo_create_default_config)
-create_config = ant_ppo_create_default_config
+ant_ppo_create_config = EasyDict(ant_ppo_create_config)
+create_config = ant_ppo_create_config
 
 if __name__ == "__main__":
     # or you can enter `ding -m serial_onpolicy -c ant_onppo_config.py -s 0 --env-step 1e7`
