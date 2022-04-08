012d8f52a45a714ca61673a8d47b79231a51b75e
nieyunpeng <nieyunpeng@sensetime.com>
2022-04-08 14:14:31 +0800

    fix unittest bugs

--- ding/entry/tests/test_serial_entry_trex_onpolicy.py
+++ ding/entry/tests/test_serial_entry_trex_onpolicy.py
@@ -16,13 +16,14 @@ from ding.entry.application_entry_trex_collect_data import trex_collecting_data
 @pytest.mark.unittest
 def test_serial_pipeline_reward_model_trex():
     config = [deepcopy(cartpole_ppo_config), deepcopy(cartpole_ppo_create_config)]
-    expert_policy = serial_pipeline_onpolicy(config, seed=0, max_train_iter=90)
+    config[0].policy.learn.learner.hook.save_ckpt_after_iter = 100
+    expert_policy = serial_pipeline_onpolicy(config, seed=0)
 
     config = [deepcopy(cartpole_trex_ppo_onpolicy_config), deepcopy(cartpole_trex_ppo_onpolicy_create_config)]
-    config[0].reward_model.data_path = './cartpole_trex_onppo'
+    config[0].reward_model.data_path = './cartpole_trex_onppo_seed0'
     config[0].reward_model.data_path = os.path.abspath(config[0].reward_model.data_path)
     config[0].reward_model.reward_model_path = config[0].reward_model.data_path + '/cartpole.params'
-    config[0].reward_model.expert_model_path = './cartpole_ppo'
+    config[0].reward_model.expert_model_path = './cartpole_onppo_seed0'
     config[0].reward_model.expert_model_path = os.path.abspath(config[0].reward_model.expert_model_path)
     config[0].reward_model.checkpoint_max = 100
     config[0].reward_model.checkpoint_step = 100
