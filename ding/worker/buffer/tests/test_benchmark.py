63552ac35d1659db7ac8130daa3d20be580125c7
lixuelin <lixuelin@sensetime.com>
2022-04-15 15:52:41 +0800

    test(lxl): polish benchmark code

--- ding/worker/buffer/tests/test_benchmark.py
+++ ding/worker/buffer/tests/test_benchmark.py
@@ -6,10 +6,9 @@ import torch
 import timeit
 
 
-buffer_list = ["base", "clone", "priority"]
-size_list = [1000, 10000, 100000]
-data_dim_list = [32, 128, 512]
-n = 10000
+size_list = [1000, 10000, 100000]              # test different buffer size, eg: 1000, 10000, 100000;
+data_dim_list = [32, 128, 512]                 # test different tensor dim, eg: 32*32, 128*128, 512*512.
+repeats = 100                                  # repeat times.
 
 
 empty_base_setup = """import sys
@@ -23,8 +22,8 @@ full_base_setup = """import sys
 import torch
 from ding.worker.buffer import DequeBuffer
 buffer = DequeBuffer(size={0})
-data = torch.rand({1}, {1})
 for i in range({0}):
+    data = torch.rand({1}, {1})
     buffer.push(data)
 """
 
@@ -41,8 +40,8 @@ import torch
 from ding.worker.buffer import DequeBuffer
 from ding.worker.buffer.middleware import clone_object
 buffer = DequeBuffer(size={0}).use(clone_object())
-data = torch.rand({1}, {1})
 for i in range({0}):
+    data = torch.rand({1}, {1})
     buffer.push(data)
 """
 
@@ -61,8 +60,8 @@ from ding.worker.buffer import DequeBuffer
 from ding.worker.buffer.middleware import PriorityExperienceReplay
 buffer = DequeBuffer(size={0})
 buffer.use(PriorityExperienceReplay(buffer, buffer_size={0}, IS_weight=True))
-data = torch.rand({1}, {1})
 for i in range({0}):
+    data = torch.rand({1}, {1})
     buffer.push(data)
 """
 
@@ -70,66 +69,51 @@ for i in range({0}):
 def get_mean_std(res):
     return np.mean(res), np.std(res)
 
+def _timeit(buf_type, buf_size, data_dim):
+    # initialize setup and target statements
+    empty_setup = eval("empty_{}_setup".format(buf_type))
+    full_setup = eval("full_{}_setup".format(buf_type))
+
+    push_stmt = "buffer.push(data)"
+    if buf_type == "priority":
+        push_stmt = "buffer.push(data, meta={'priority': 2.0})"
+    sample_stmt = "buffer.sample(128, replace=False)"
+    replace_sample_stmt = "buffer.sample(128, replace=True)"
+
+    print("exp-buffer_{}_{}-data_{:.2f}_KB".format(buf_type, buf_size, sys.getsizeof(torch.rand(data_dim, data_dim).storage()) / 1024))
+
+    # test pushing
+    mean, std = get_mean_std(timeit.repeat(push_stmt, setup=empty_setup.format(buf_size, data_dim), number=repeats))
+    print("Empty Push Test:          mean {:.4f} s, std {:.4f} s".format(mean, std))
+
+    # test sampling without replace
+    mean, std = get_mean_std(timeit.repeat(sample_stmt, setup=full_setup.format(buf_size, data_dim), number=repeats))
+    print("No-Replace Sample Test:   mean {:.4f} s, std {:.4f} s".format(mean, std))
+
+    # test sampling with replace
+    mean, std = get_mean_std(timeit.repeat(replace_sample_stmt, setup=full_setup.format(buf_size, data_dim), number=repeats))
+    print("Replace Sample Test:      mean {:.4f} s, std {:.4f} s".format(mean, std))
+
+    # Attention:
+    # the test results are the sum of repeats, rather than avg!
+
+    print("=" * 100)
+
 
 @pytest.mark.benchmark
-def test_buffer_benchmark():
-    for buf in buffer_list:
-        for s in size_list:
-            for d in data_dim_list:
-                empty_setup = eval("empty_{}_setup".format(buf))
-                full_setup = eval("full_{}_setup".format(buf))
-
-                push_stmt = "buffer.push(data)"
-                if buf == "priority":
-                    push_stmt = "buffer.push(data, meta={'priority': 2.0})"
-                sample_stmt = "buffer.sample(128, replace=False)"
-                replace_sample_stmt = "buffer.sample(128, replace=True)"
-
-                print("exp-buffer_{}-data_{:.2f}_KB".format(s, sys.getsizeof(torch.rand(d, d).storage()) / 1024))
-
-                mean, std = get_mean_std(timeit.repeat(push_stmt, setup=empty_setup.format(s, d)))
-                print("Empty Push Test:          mean {:.4f} s, std {:.4f} s".format(mean, std))
-
-                mean, std = get_mean_std(timeit.repeat(push_stmt, setup=full_setup.format(s, d)))
-                print("Full Push Test:           mean {:.4f} s, std {:.4f} s".format(mean, std))
-
-                mean, std = get_mean_std(timeit.repeat(sample_stmt, setup=full_setup.format(s, d)))
-                print("No-Replace Sample Test:   mean {:.4f} s, std {:.4f} s".format(mean, std))
-
-                mean, std = get_mean_std(timeit.repeat(replace_sample_stmt, setup=full_setup.format(s, d)))
-                print("Replace Sample Test:      mean {:.4f} s, std {:.4f} s".format(mean, std))
-
+def test_base_benchmark():
+    for s in size_list:
+        for d in data_dim_list:
+            _timeit("base", s, d)
 
 @pytest.mark.benchmark
-def test_buffer_benchmark():
-    for buf in buffer_list:
-        for s in size_list:
-            for d in data_dim_list:
-                empty_setup = eval("empty_{}_setup".format(buf))
-                full_setup = eval("full_{}_setup".format(buf))
+def test_clone_benchmark():
+    for s in size_list:
+        for d in data_dim_list:
+            _timeit("clone", s, d)
 
-                push_stmt = "buffer.push(data)"
-                if buf == "priority":
-                    push_stmt = "buffer.push(data, meta={'priority': 2.0})"
-                sample_stmt = "buffer.sample(128, replace=False)"
-                replace_sample_stmt = "buffer.sample(128, replace=True)"
-
-                print("exp-buffer_{}_{}-data_{:.2f}_KB".format(buf, s, sys.getsizeof(torch.rand(d, d).storage()) / 1024))
-
-                mean, std = get_mean_std(timeit.repeat(push_stmt, setup=empty_setup.format(s, d), number=n))
-                print("Empty Push Test:          mean {:.4f} s, std {:.4f} s".format(mean, std))
-                time.sleep(1.0)
-
-                mean, std = get_mean_std(timeit.repeat(push_stmt, setup=full_setup.format(s, d), number=n))
-                print("Full Push Test:           mean {:.4f} s, std {:.4f} s".format(mean, std))
-                time.sleep(1.0)
-
-                mean, std = get_mean_std(timeit.repeat(sample_stmt, setup=full_setup.format(s, d), number=n))
-                print("No-Replace Sample Test:   mean {:.4f} s, std {:.4f} s".format(mean, std))
-                time.sleep(1.0)
-
-                mean, std = get_mean_std(timeit.repeat(replace_sample_stmt, setup=full_setup.format(s, d), number=n))
-                print("Replace Sample Test:      mean {:.4f} s, std {:.4f} s".format(mean, std))
-                time.sleep(1.0)
-
-                print("=" * 100)
+@pytest.mark.benchmark
+def test_priority_benchmark():
+    for s in size_list:
+        for d in data_dim_list:
+            _timeit("priority", s, d)
