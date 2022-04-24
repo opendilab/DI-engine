a65301108d975210c0a3feea8f345483f853fc63
lixuelin <lixuelin@sensetime.com>
2022-04-24 11:51:33 +0800

    modify test_evaluator

--- ding/framework/middleware/tests/test_evaluator.py
+++ ding/framework/middleware/tests/test_evaluator.py
@@ -2,9 +2,8 @@ import pytest
 import torch
 import copy
 from unittest.mock import patch
-from ding.framework import Context
+from ding.framework import Context, task
 from ding.framework.middleware.functional.evaluator import interaction_evaluator
-from ding.framework.middleware.functional.trainer import trainer
 from ding.framework.middleware.tests.mock_for_test import MockPolicy, MockEnv, CONFIG
 
 
@@ -14,10 +13,11 @@ def test_interaction_evaluator():
     ctx = Context(CONFIG.ctx)
     with patch("ding.policy.Policy",  MockPolicy):
         with patch("ding.envs.BaseEnvManagerV2",  MockEnv):
-            policy = MockPolicy()
-            env = MockEnv()
-            for _ in range(100):
-                ctx.train_iter += 1
-                interaction_evaluator(cfg, policy, env)(ctx)
+            with task.start():
+                policy = MockPolicy()
+                env = MockEnv()
+                for _ in range(100):
+                    ctx.train_iter += 1
+                    interaction_evaluator(cfg, policy, env)(ctx)
     print("ctx.last_eval_iter:", ctx.last_eval_iter)
     print("ctx.train_iter:", ctx.train_iter)
