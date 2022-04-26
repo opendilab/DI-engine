3da345560a127e3a7ef32c3dae1e0403964be330
Xu Jingxin <sailxjx@gmail.com>
2022-04-26 16:06:56 +0800

    Add more desc (ci skip)

--- ding/framework/message_queue/nng.py
+++ ding/framework/message_queue/nng.py
@@ -30,6 +30,7 @@ class NNGMQ(MQ):
         sleep(0.1)  # Wait for peers to bind
         for contact in self.attach_to:
             sock.dial(contact)
+        logging.info("NNG listen on {}, attach to {}".format(self.listen_to, self.attach_to))
 
     def publish(self, topic: str, data: bytes) -> None:
         if not self._finished:
