e9f32fb76f4d16da159a8f669c5057f8f9914bcb
hiha3456 <744762298@qq.com>
2022-05-25 21:23:23 +0800

    change a bit

--- ding/framework/middleware/league_actor.py
+++ ding/framework/middleware/league_actor.py
@@ -4,11 +4,16 @@ import logging
 
 from typing import List, Any
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING
 from abc import abstractmethod
 
 from easydict import EasyDict
 from ding.envs import BaseEnvManager
+
+from .functional import inferencer, rolloutor, TransitionList
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ding.framework import OnlineRLContext
 
 class Storage:
 
@@ -60,13 +65,23 @@ class LeagueActor:
     def __init__(self, cfg: EasyDict, policy, env: BaseEnvManager):
         self._running = True
         self._model_updated = True
+
+        self.cfg = cfg
+        self.policy = policy
+        self._transitions = TransitionList(self.env.env_num)
+        self._inferencer = task.wrap(inferencer(cfg, policy, env))
+        self._rolloutor = task.wrap(rolloutor(cfg, policy, env, self._transitions))
+
         task.on("league_job_actor_{}".format(task.router.node_id), self._on_league_job)
         task.on("learner_model", self._on_learner_model)
 
-    def _on_learner_model(self):
+    def _on_learner_model(self, learner_model: "LearnerModel"):
         """
         If get newest learner model, update this actor's model.
         """
+        player_meta = PlayerMeta(player_id=learner_model.player_id, checkpoint=None)
+        policy = self._get_policy(player_meta)
+        policy.load_state_dict(learner_model.state_dict)
         self._model_updated = True
 
         # update policy model
@@ -86,14 +101,35 @@ class LeagueActor:
                 "Waiting for new model on actor: {}, player: {}".format(task.router.node_id, job.launch_player)
             )
             sleep(1)
+
+        policies = []
+        main_player: "PlayerMeta" = None
+        for player in job.players:
+            policies.append(self._get_policy(player))
+            if player.player_id == job.launch_player:
+                main_player = player
+
+        assert main_player, "Can not find active player"
         
         # initialize env
         self._envs.reset()
 
         # 参考StepCollector 
+        ctx = OnlineRLContext()
+        old = ctx.env_step
+        
+        target_size = self.cfg.policy.collect.n_sample * self.cfg.policy.collect.unroll_len
+        current_inferencer = self._inferencer
 
-        # model interaction with env
-        # collector
+        while True:
+            # model interaction with env
+            # collector
+            current_inferencer(ctx)
+            self._rolloutor(ctx)
+            if ctx.env_step - old >= target_size:
+                ctx.trajectories, ctx.trajectory_end_idx = self._transitions.to_trajectories()
+                self._transitions.clear()
+                break
 
         # generate traj
         # get traj from collector
@@ -116,6 +152,17 @@ class LeagueActor:
         task.emit("actor_greeting", task.router.node_id)
         self._envs.close()
         self._running = False
+
+    def _get_policy(self, player: "PlayerMeta") -> "Policy.collect_function":
+        player_id = player.player_id
+        if self._policies.get(player_id):
+            return self._policies.get(player_id)
+        policy: "Policy.collect_function" = self.policy_fn().collect_mode
+        self._policies[player_id] = policy
+        if "historical" in player.player_id:
+            policy.load_state_dict(player.checkpoint.load())
+
+        return policy
 
     def __call__(self):
         if not self._running:
--- ding/framework/middleware/tests/test_league_actor.py
+++ ding/framework/middleware/tests/test_league_actor.py
@@ -3,7 +3,7 @@ from ding.policy import DQNPolicy
 from ding.envs import DingEnvWrapper, BaseEnvManagerV2
 from ding.data import DequeBuffer
 from ding.config import compile_config
-from ding.framework import task
+from ding.framework import task, Parallel
 from ding.framework.context import OnlineRLContext
 from ding.framework.middleware import LeagueActor, Job
 from ding.utils import set_pkg_seed
@@ -17,19 +17,20 @@ def main():
 
     cfg = compile_config(main_config, create_cfg=create_config, auto=True)
     with task.start(async_mode=False, ctx=OnlineRLContext()):
-        collector_env = BaseEnvManagerV2(
-            env_fn=[lambda: DingEnvWrapper(DIStarEnv(distar_cfg)) for _ in range(cfg.env.collector_env_num)],
-            cfg=cfg.env.manager
-        )
+        if task.router.node_id == 0:
+            collector_env = BaseEnvManagerV2(
+                env_fn=[lambda: DingEnvWrapper(DIStarEnv(distar_cfg)) for _ in range(cfg.env.collector_env_num)],
+                cfg=cfg.env.manager
+            )
 
-        set_pkg_seed(cfg.seed, use_cuda=cfg.policy.cuda)
+            set_pkg_seed(cfg.seed, use_cuda=cfg.policy.cuda)
 
-        model = DQN(**cfg.policy.model)
-        policy = DQNPolicy(cfg.policy, model=model)
+            model = DQN(**cfg.policy.model)
+            policy = DQNPolicy(cfg.policy, model=model)
 
-        task.use(LeagueActor(cfg, policy.collect_mode, collector_env))
-        task.run()
-
+            task.use(LeagueActor(cfg, policy.collect_mode, collector_env))
+        elif task.router.node_id == 1:
+            pass
 
 if __name__ == "__main__":
-    main()
+    Parallel.runner(n_parallel_workers=4, protocol="tcp", topology="star")(main)
