7a2a634efffb9de374df56504a8f3d39b5f2844f
hiha3456 <744762298@qq.com>
2022-05-26 15:45:30 +0800

    transfer BattleEpisodeSerialCollector to middleware BattleInferencer

--- ding/framework/middleware/functional/collector.py
+++ ding/framework/middleware/functional/collector.py
@@ -7,6 +7,9 @@ from ding.envs import BaseEnvManager
 from ding.policy import Policy
 from ding.framework import task
 from collections import namedtuple
+from ding.utils import EasyTimer, dicts_to_lists
+from ding.torch_utils import to_tensor, to_ndarray
+from ding.worker.collector.base_serial_collector import to_tensor_transitions
 
 if TYPE_CHECKING:
     from ding.framework import OnlineRLContext
@@ -86,6 +89,10 @@ def inferencer(cfg: EasyDict, policy: Policy, env: BaseEnvManager) -> Callable:
 class BattleInferencer():
     def __init__(self, cfg: EasyDict, policy: List[namedtuple] = None, env: BaseEnvManager = None):
         self._cfg = cfg
+        self._transform_obs = cfg.transform_obs
+        self._timer = EasyTimer()
+
+        # TODO(zms) call self.reset() to reset policy and env
         self._policy = policy
         self._env = env 
 
@@ -101,7 +108,111 @@ class BattleInferencer():
                 the former is a list containing collected episodes if not get_train_sample, \
                 otherwise, return train_samples split by unroll_len.
         """
-        raise NotImplementedError
+        
+        if ctx.n_episode is None:
+            ### TODO(zms): self._default_n_episode comes from self.reset_policy()
+            if self._default_n_episode is None:
+                raise RuntimeError("Please specify collect n_episode")
+            else:
+                ctx.n_episode = self._default_n_episode
+        assert ctx.n_episode >= self._env_num, "Please make sure n_episode >= env_num"
+
+        if ctx.policy_kwargs is None:
+            ctx.policy_kwargs = {}
+        
+        collected_episode = 0
+        return_data = [[] for _ in range(2)]
+        return_info = [[] for _ in range(2)]
+        ready_env_id = set()
+        remain_episode = ctx.n_episode
+
+        while True:
+            with self._timer:
+                # Get current env obs.
+                obs = self._env.ready_obs
+                new_available_env_id = set(obs.keys()).difference(ready_env_id)
+                ready_env_id = ready_env_id.union(set(list(new_available_env_id)[:remain_episode]))
+                remain_episode -= min(len(new_available_env_id), remain_episode)
+                obs = {env_id: obs[env_id] for env_id in ready_env_id}
+
+                # Policy forward.
+                ### TODO(zms): self._obs_pool comes from self.reset
+                self._obs_pool.update(obs)
+                if self._transform_obs:
+                    obs = to_tensor(obs, dtype=torch.float32)
+                obs = dicts_to_lists(obs)
+                policy_output = [p.forward(obs[i], **ctx.policy_kwargs) for i, p in enumerate(self._policy)]
+
+                ### TODO(zms): self._policy_output_pool comes from self.reset
+                self._policy_output_pool.update(policy_output)
+                # Interact with env.
+                actions = {}
+                for env_id in ready_env_id:
+                    actions[env_id] = []
+                    for output in policy_output:
+                        actions[env_id].append(output[env_id]['action'])
+                actions = to_ndarray(actions)
+                timesteps = self._env.step(actions)
+
+            # TODO(nyz) this duration may be inaccurate in async env
+            interaction_duration = self._timer.value / len(timesteps)
+
+            # TODO(nyz) vectorize this for loop
+            for env_id, timestep in timesteps.items():
+                # TODO(zms): self._env_info comes from self.reset()
+                self._env_info[env_id]['step'] += 1
+                # TODO(zms): self._env_info comes from self.reset()
+                self._total_envstep_count += 1
+                with self._timer:
+                    for policy_id, policy in enumerate(self._policy):
+                        policy_timestep_data = [d[policy_id] if not isinstance(d, bool) else d for d in timestep]
+                        policy_timestep = type(timestep)(*policy_timestep_data)
+                        transition = self._policy[policy_id].process_transition(
+                            # TODO(zms): self._policy_output_pool comes from self.reset()
+                            self._obs_pool[env_id][policy_id], self._policy_output_pool[env_id][policy_id],
+                            policy_timestep
+                        )
+                        transition['collect_iter'] = ctx.cleartrain_iter
+                        self._traj_buffer[env_id][policy_id].append(transition)
+                        # prepare data
+                        if timestep.done:
+                            # TODO(zms): to get rid of collector, "to_tensor_transitions" function should be removed and must be somewhere else.
+                            transitions = to_tensor_transitions(self._traj_buffer[env_id][policy_id])
+                            if self._cfg.get_train_sample:
+                                train_sample = self._policy[policy_id].get_train_sample(transitions)
+                                return_data[policy_id].extend(train_sample)
+                            else:
+                                return_data[policy_id].append(transitions)
+                            self._traj_buffer[env_id][policy_id].clear()
+
+                self._env_info[env_id]['time'] += self._timer.value + interaction_duration
+
+                # If env is done, record episode info and reset
+                if timestep.done:
+                    # TODO(zms): self._env_info comes from self.reset()
+                    self._total_episode_count += 1
+                    info = {
+                        'reward0': timestep.info[0]['final_eval_reward'],
+                        'reward1': timestep.info[1]['final_eval_reward'],
+                        'time': self._env_info[env_id]['time'],
+                        'step': self._env_info[env_id]['step'],
+                    }
+                    collected_episode += 1
+                    self._episode_info.append(info)
+                    for i, p in enumerate(self._policy):
+                        p.reset([env_id])
+                    self._reset_stat(env_id)
+                    ready_env_id.remove(env_id)
+                    for policy_id in range(2):
+                        return_info[policy_id].append(timestep.info[policy_id])
+            if collected_episode >= ctx.n_episode:
+                break
+        # log
+        ### TODO: how to deal with log here?
+        # self._output_log(ctx.train_iter)
+        ctx.return_data = return_data
+        ctx.return_info = return_info
+
 
 
 def rolloutor(cfg: EasyDict, policy: Policy, env: BaseEnvManager, transitions: TransitionList) -> Callable:
