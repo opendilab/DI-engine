2b179338e375443b64ddbb8a396e06477e4645e9
weiyuhong <weiyuhong@sensetime.com>
2022-05-26 17:54:09 +0800

    feature(wyh):ppg polish

--- ding/model/common/tests/test_encoder.py
+++ ding/model/common/tests/test_encoder.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import pytest
 
-from ding.model import ConvEncoder, FCEncoder
+from ding.model import ConvEncoder, FCEncoder, ImpalaEncoder
 from ding.torch_utils import is_differentiable
 
 B = 4
@@ -39,3 +39,11 @@ class TestEncoder:
         outputs = model(inputs)
         self.output_check(model, outputs)
         assert outputs.shape == (B, hidden_size_list[-1])
+
+    def test_impalacnn_encoder(self):
+        inputs = torch.randn(B, 3, 64, 64)
+        model = ImpalaEncoder(inshape=(3, 64, 64))
+        print(model)
+        outputs = model(inputs)
+        self.output_check(model, outputs)
+        assert outputs.shape == (B, 256)
