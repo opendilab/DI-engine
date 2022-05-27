bc9ef75edd220bfaebad3c6679f538d906842af1
hiha3456 <744762298@qq.com>
2022-05-27 20:43:03 +0800

    change test

--- ding/framework/middleware/tests/test_league_actor.py
+++ ding/framework/middleware/tests/test_league_actor.py
@@ -1,58 +1,69 @@
-from ding.model import DQN
-from ding.policy import DQNPolicy
-from ding.envs import DingEnvWrapper, BaseEnvManagerV2
-from ding.data import DequeBuffer
-from ding.config import compile_config
-from ding.framework import task, Parallel
-from ding.framework.context import OnlineRLContext
-from ding.framework.middleware import LeagueActor, Job
-from ding.utils import set_pkg_seed
-from dizoo.classic_control.cartpole.config.cartpole_dqn_config import main_config, create_config
-from dizoo.distar.envs.distar_env import DIStarEnv
-from distar.ctools.utils import read_config
+from time import sleep
+import pytest
 from copy import deepcopy
+from ding.envs import BaseEnvManager
+from ding.framework.middleware.league_learner import LearnerModel
+from ding.framework.middleware.tests.league_config import cfg
+from ding.framework.middleware.league_actor import ActorData, LeagueActor, Job
 
-distar_cfg = read_config('C:/Users/hjs/DI-engine/dizoo/distar/envs/test_distar_config.yaml')
-cfg = compile_config(main_config, create_cfg=create_config, auto=True)
+from ding.framework.task import Task
+from ding.model import VAC
+from ding.policy.ppo import PPOPolicy
+from dizoo.league_demo.game_env import GameEnv
+
+class MockLeague:
+    def __init__(self):
+        self.active_players_ids = ["test_node_1", "test_node_2", "test_node_3"]
+        self.update_payoff_cnt = 0
+        self.update_active_player_cnt = 0
+        self.create_historical_player_cnt = 0
+        self.get_job_info_cnt = 0
+
+    def update_payoff(self, job):
+        self.update_payoff_cnt += 1
+        # print("update_payoff: {}".format(job))
+    
+    def update_active_player(self, meta):
+        self.update_active_player_cnt += 1
+        # print("update_active_player: {}".format(meta))
+    
+    def create_historical_player(self, meta):
+        self.create_historical_player_cnt += 1
+        # print("create_historical_player: {}".format(meta))
+    
+    def get_job_info(self, player_id):
+        self.get_job_info_cnt += 1
+        # print("get_job_info")
+        return Job(231890, player_id, False)
 
 def prepare_test():
-
     global cfg
     cfg = deepcopy(cfg)
 
     def env_fn():
-        env = BaseEnvManagerV2(
-            env_fn=[lambda: DingEnvWrapper(DIStarEnv(distar_cfg)) for _ in range(cfg.env.collector_env_num)],
-            cfg=cfg.env.manager
+        env = BaseEnvManager(
+            env_fn=[lambda: GameEnv(cfg.env.env_type) for _ in range(cfg.env.collector_env_num)], cfg=cfg.env.manager
         )
         env.seed(cfg.seed)
         return env
-    
+
     def policy_fn():
-        model = DQN(**cfg.policy.model)
-        policy = DQNPolicy(cfg.policy, model=model)
+        model = VAC(**cfg.policy.model)
+        policy = PPOPolicy(cfg.policy, model=model)
         return policy
-    
-    return cfg, env_fn, policy_fn
+
+    league = BaseLeague(cfg.policy.other.league)
+    return cfg, env_fn, policy_fn, league
+
 
 def main():
     cfg, env_fn, policy_fn = prepare_test()
+    task.start()
     
-    with task.start(async_mode=False, ctx=OnlineRLContext()):
-        if task.router.node_id == 0:
-            collector_env = BaseEnvManagerV2(
-                env_fn=[lambda: DingEnvWrapper(DIStarEnv(distar_cfg)) for _ in range(cfg.env.collector_env_num)],
-                cfg=cfg.env.manager
-            )
-
-            set_pkg_seed(cfg.seed, use_cuda=cfg.policy.cuda)
-
-            model = DQN(**cfg.policy.model)
-            policy = DQNPolicy(cfg.policy, model=model)
-
-            task.use(LeagueActor(cfg, env_fn, policy_fn))
-        elif task.router.node_id == 1:
-            pass
+    if task.router.node_id == 0:
+        LeagueActor()
+    elif task.router.node_id == 1:
+        pass
 
 if __name__ == "__main__":
     Parallel.runner(n_parallel_workers=4, protocol="tcp", topology="star")(main)
