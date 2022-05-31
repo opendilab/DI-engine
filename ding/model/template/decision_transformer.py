0f63e439bafe82a9b1f42bff6419a31b098378ee
wangzilin <wangzilin@sensetime.com>
2022-05-31 03:09:02 +0000

    feature(wzl): update dt model 4

--- ding/model/template/decision_transformer.py
+++ ding/model/template/decision_transformer.py
@@ -10,6 +10,82 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class MaskedCausalAttention(nn.Module):
+
+    def __init__(self, h_dim, max_T, n_heads, drop_p):
+        super().__init__()
+
+        self.n_heads = n_heads
+        self.max_T = max_T
+
+        self.q_net = nn.Linear(h_dim, h_dim)
+        self.k_net = nn.Linear(h_dim, h_dim)
+        self.v_net = nn.Linear(h_dim, h_dim)
+
+        self.proj_net = nn.Linear(h_dim, h_dim)
+
+        self.att_drop = nn.Dropout(drop_p)
+        self.proj_drop = nn.Dropout(drop_p)
+
+        ones = torch.ones((max_T, max_T))
+        mask = torch.tril(ones).view(1, 1, max_T, max_T)
+
+        # register buffer makes sure mask does not get updated
+        # during backpropagation
+        self.register_buffer('mask', mask)
+
+    def forward(self, x):
+        B, T, C = x.shape  # batch size, seq length, h_dim * n_heads
+
+        N, D = self.n_heads, C // self.n_heads  # N = num heads, D = attention dim
+
+        # rearrange q, k, v as (B, N, T, D)
+        q = self.q_net(x).view(B, T, N, D).transpose(1, 2)
+        k = self.k_net(x).view(B, T, N, D).transpose(1, 2)
+        v = self.v_net(x).view(B, T, N, D).transpose(1, 2)
+
+        # weights (B, N, T, T)
+        weights = q @ k.transpose(2, 3) / math.sqrt(D)
+        # causal mask applied to weights
+        weights = weights.masked_fill(self.mask[..., :T, :T] == 0, float('-inf'))
+        # normalize weights, all -inf -> 0 after softmax
+        normalized_weights = F.softmax(weights, dim=-1)
+
+        # attention (B, N, T, D)
+        # normalized_weights.shape: (B, N, T, T)
+        # v.shape: (B, N, T, D)
+        attention = self.att_drop(normalized_weights @ v)
+
+        # gather heads and project (B, N, T, D) -> (B, T, N*D)
+        attention = attention.transpose(1, 2).contiguous().view(B, T, N * D)
+
+        out = self.proj_drop(self.proj_net(attention))
+        return out
+
+
+class Block(nn.Module):
+
+    def __init__(self, h_dim, max_T, n_heads, drop_p):
+        super().__init__()
+        self.attention = MaskedCausalAttention(h_dim, max_T, n_heads, drop_p)
+        self.mlp = nn.Sequential(
+            nn.Linear(h_dim, 4 * h_dim),
+            nn.GELU(),
+            nn.Linear(4 * h_dim, h_dim),
+            nn.Dropout(drop_p),
+        )
+        self.ln1 = nn.LayerNorm(h_dim)
+        self.ln2 = nn.LayerNorm(h_dim)
+
+    def forward(self, x):
+        # Attention -> LayerNorm -> MLP -> LayerNorm
+        x = x + self.attention(x)  # residual
+        x = self.ln1(x)
+        x = x + self.mlp(x)  # residual
+        x = self.ln2(x)
+        return x
+
+
 @MODEL_REGISTRY.register('dt')
 class DecisionTransformer(nn.Module):
 
