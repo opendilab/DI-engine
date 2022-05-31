7609e3a25f3a7c1bd4d1f28000c67adcdc4464dc
niuyazhe <niuyazhe@sensetime.com>
2022-05-31 17:43:29 +0800

    fix(nyz): fix style and update random crop in curl

--- ding/obs_model/curl_obs_model.py
+++ ding/obs_model/curl_obs_model.py
@@ -2,11 +2,9 @@ from typing import TYPE_CHECKING, Dict
 # easydict: enable to access the value of the dictionary as an attribute
 from easydict import EasyDict
 import copy
-import numpy as np
 import torch
 import torch.nn as nn
-import os
-from skimage.util.shape import view_as_windows
+import torch.nn.functional as F
 
 # prevent error from circling
 if TYPE_CHECKING:
@@ -15,25 +13,23 @@ if TYPE_CHECKING:
 # for 84 x 84 inputs
 OUT_DIM = {2: 39, 4: 35, 6: 31}
 
+
 class Encoder(nn.Module):
+
     def __init__(self, obs_shape, encoder_feature_size, num_layers=2, num_filters=32):
         super().__init__()
-        # x : shape : [B, C, H, W]
-        assert len(obs_shape) == 3
+        assert len(obs_shape) == 3, len(obs_shape)
         self.obs_shape = obs_shape
         self.encoder_feature_size = encoder_feature_size
-        print(encoder_feature_size)
         self.num_layers = num_layers
 
-        self.convs = nn.ModuleList(
-            [nn.Conv2d(obs_shape[0], num_filters, 3, stride=2)]
-        )
+        self.convs = nn.ModuleList([nn.Conv2d(obs_shape[0], num_filters, 3, stride=2)])
         for i in range(num_layers - 1):
             self.convs.append(nn.Conv2d(num_filters, num_filters, 3, stride=1))
-        out_dim = OUT_DIM[num_layers] #39
+        out_dim = OUT_DIM[num_layers]
         self.fc = nn.Linear(num_filters * out_dim * out_dim, self.encoder_feature_size)
         self.ln = nn.LayerNorm(self.encoder_feature_size)
-        self.outputs = dict() # initialize
+        self.outputs = dict()  # initialize
 
     def forward_conv(self, obs):
         obs = obs / 255.
@@ -49,7 +45,7 @@ class Encoder(nn.Module):
     def forward(self, obs, detach=False):
         h = self.forward_conv(obs)
         if detach:
-            h = h.detach() #切断分支反向传播
+            h = h.detach()  # shut down back propogation here
         h_fc = self.fc(h)
         self.outputs['fc'] = h_fc
         h_norm = self.ln(h_fc)
@@ -58,28 +54,30 @@ class Encoder(nn.Module):
         self.outputs['tanh'] = out
         return out
 
+
 class CurlObsModel(nn.Module):
+
     @classmethod
     def default_config(cls: type) -> EasyDict:
         # copy.deepcopy: copy objecgs and their subjects
         cfg = EasyDict(copy.deepcopy(cls.config))
         cfg.cfg_type = cls.__name__ + 'Dict'
         return cfg
+
     config = dict(
         batch_size=64,
         encoder_lr=1e-3,
         w_lr=1e-4,
         target_theta=0.005,
         encoder_feature_size=50,
-        frame_stack = 3,
-        image_size = 84,
-        num_layers = 2,
-        num_filters = 32,
-        train_steps = 10,
+        frame_stack=3,
+        image_size=84,
+        num_layers=2,
+        num_filters=32,
+        train_steps=10,
     )
 
-    def __init__(
-            self, cfg: EasyDict, tb_logger: "SummaryWriter") -> None:
+    def __init__(self, cfg: EasyDict, tb_logger: "SummaryWriter") -> None:
         # delete encoder & encoder_target: nn.Module,
         """
         Overview:
@@ -90,7 +88,7 @@ class CurlObsModel(nn.Module):
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
         self.cfg = cfg
-        self.obs_shape = (3*cfg.frame_stack, cfg.image_size, cfg.image_size)
+        self.obs_shape = (3 * cfg.frame_stack, cfg.image_size, cfg.image_size)
         self.encoder = Encoder(self.obs_shape, cfg.encoder_feature_size, cfg.num_layers, cfg.num_filters)
         # self.encoder_target = encoder_target
         self.tb_logger = tb_logger
@@ -98,12 +96,11 @@ class CurlObsModel(nn.Module):
         # parameter
         self.W = nn.Parameter(torch.rand(self.cfg.encoder_feature_size, self.cfg.encoder_feature_size))
         # optimizer
-        self.encoder_optimizer = torch.optim.Adam(
-            self.encoder.parameters(), lr=self.cfg.encoder_lr
-        )
+        self.encoder_optimizer = torch.optim.Adam(self.encoder.parameters(), lr=self.cfg.encoder_lr)
 
         self.cpc_optimizer = torch.optim.Adam(
-            [self.W], lr=self.cfg.w_lr,
+            [self.W],
+            lr=self.cfg.w_lr,
         )
         # loss function
         self.cross_entropy_loss = nn.CrossEntropyLoss()
@@ -152,17 +149,15 @@ class CurlObsModel(nn.Module):
         z_pos = self.encode(obs_positive)
         logits = self.compute_logits(z_anc, z_pos)
 
-        labels = torch.arange(logits.shape[0]).long()#.to(self.device) #有two devices 报错
+        labels = torch.arange(logits.shape[0]).long()  # .to(self.device)
         loss = self.cross_entropy_loss(logits, labels)
 
-        self.encoder_optimizer.zero_grad() # 将模型的参数梯度初始化为0
+        self.encoder_optimizer.zero_grad()  # zero model parameters grad
         self.cpc_optimizer.zero_grad()
         loss.backward()
 
         self.encoder_optimizer.step()
-        self.cpc_optimizer.step() # 更新所有参数
-        # if step % self.log_interval == 0:
-        #     L.log('train/curl_loss', loss, step)
+        self.cpc_optimizer.step()  # update all the parameters
 
     def save(self) -> Dict:
         """
@@ -170,10 +165,10 @@ class CurlObsModel(nn.Module):
             Returneezs state_dict including W and optimizer for saving checkpoint.
         """
         state_dict = {
-                        'encoder': self.encoder.state_dict(),
-                        'W': self.W.detach(),
-                        'encoder optimizer': self.encoder_optimizer.state_dict() ,
-                        'cpc optimizer': self.cpc_optimizer.state_dict()
+            'encoder': self.encoder.state_dict(),
+            'W': self.W.detach(),
+            'encoder optimizer': self.encoder_optimizer.state_dict(),
+            'cpc optimizer': self.cpc_optimizer.state_dict()
         }
         torch.save(state_dict, 'curl.pt')
 
@@ -183,10 +178,10 @@ class CurlObsModel(nn.Module):
             Load state_dict including W and optimizer in order to recover.
         """
         state_dict = {
-                        'encoder': self.encoder.state_dict(),
-                        'W': self.W.detach(),
-                        'encoder optimizer': self.encoder_optimizer.state_dict() ,
-                        'cpc optimizer': self.cpc_optimizer.state_dict()
+            'encoder': self.encoder.state_dict(),
+            'W': self.W.detach(),
+            'encoder optimizer': self.encoder_optimizer.state_dict(),
+            'cpc optimizer': self.cpc_optimizer.state_dict()
         }
         self.encoder.load_state_dict(state_dict['encoder'])
         with torch.no_grad():
@@ -200,27 +195,16 @@ class CurlObsModel(nn.Module):
         args:
         imgs, with shape (B,C,H,W)
         """
-        b = img.shape[0]
-        img_size = img.shape[-1]
-        crop_max = img_size - self.cfg.image_size #16
-        imgs = np.transpose(img,(0,2,3,1)) #(64,100,100,9)
-        H_a = np.random.randint(0,crop_max,b)# 64
-        W_a = np.random.randint(0,crop_max,b)
-        H_p = np.random.randint(0,crop_max,b)
-        W_p = np.random.randint(0,crop_max,b)
-        # view_as_windows(input_array, window_shape, step)
+        anchor = self.random_crop(img, self.cfg.image_size)
+        positive = self.random_crop(img, self.cfg.image_size)
+        return dict(obs_anchor=anchor, obs_positive=positive)
 
-        windows = view_as_windows(
-            imgs, (1, self.cfg.image_size, self.cfg.image_size, 1))[...,0, :,:, 0]
-        # without [···，0，：，：，0] anchor:(64, 9, 1, 84, 84, 1) , 0 means drop
+    @staticmethod
+    def random_crop(img: torch.Tensor, N):
+        B, C, H, W = img.shape
 
-        anchor = windows[np.arange(b), H_a, W_a] #(64,9,84,84)
-        print(anchor.shape)
-        positive = windows[np.arange(b), H_p, W_p]
-
-        anchor = torch.as_tensor(anchor, device=self.device).float()
-        positive = torch.as_tensor(positive, device=self.device).float()
-
-        data = dict(obs_anchor=anchor, obs_positive=positive)
-
-        return data
+        h = torch.randint(0, H - N, (B, ))
+        w = torch.randint(0, W - N, (B, ))
+        windows = F.unfold(img, (N, N), stride=1)  # B, CxNxN, (h+1)*(w+1)
+        windows = windows.view(B, C, N, N, -1)
+        return windows[torch.arange(B), ..., h * (W - N) + w]
--- ding/obs_model/tests/test_curl_obs_model.py
+++ ding/obs_model/tests/test_curl_obs_model.py
@@ -2,13 +2,8 @@ import pytest
 import numpy as np
 import torch
 import torch.nn as nn
-# from ding.obs_model import CurlObsModel
-# from ding.obs_model import Encoder
-from ding.obs_model import *
-from torch.autograd import Variable
+from ding.obs_model import CurlObsModel
 
-# encoder = nn.Linear(32, 50)
-# encoder_target = nn.Linear(32, 50)
 
 def test_curl_compute_logits():
     curl = CurlObsModel(CurlObsModel.default_config(), None)
@@ -18,42 +13,45 @@ def test_curl_compute_logits():
     assert logits.shape == (4, 4)
     print('end')
 
+
 def test_curl_encode():
     curl = CurlObsModel(CurlObsModel.default_config(), None)
     # x : shape : [B, C, H, W] [batch_size, 3 * frame_stack, height, width]
     x = torch.randn(64, 3 * curl.cfg.frame_stack, 84, 84)
     # embedding: :math:`(B, N)`, where ``N = embedding_size/encoder_feature_size``
     z = curl.encode(x)
-    assert  z.shape == (64, 50)
+    assert z.shape == (64, 50)
     print('end')
+
 
 def test_curl_train():
     curl = CurlObsModel(CurlObsModel.default_config(), None)
-    data = {"obs_anchor": torch.randn(64, 9, 84, 84) ,
-            "obs_positive": torch.randn(64, 9, 84, 84)}
+    data = {"obs_anchor": torch.randn(64, 9, 84, 84), "obs_positive": torch.randn(64, 9, 84, 84)}
     assert curl.W.grad is None
     for p in curl.encoder.parameters():
         assert p.grad is None
     curl.train(data)
-    assert curl.W.grad is not None and torch.ne(curl.W, torch.zeros(curl.W.shape)).all()#
+    assert curl.W.grad is not None and torch.ne(curl.W, torch.zeros(curl.W.shape)).all()  #
     for p in curl.encoder.parameters():
         assert p.grad is not None
     # print(curl.encoder.parameters())
     print('end')
+
 
 def test_curl_save():
     curl = CurlObsModel(CurlObsModel.default_config(), None)
     curl.save()
     print('end')
 
+
 def test_curl_load():
     curl = CurlObsModel(CurlObsModel.default_config(), None)
     curl.load()
     print('end')
 
+
 def test_curl_get_augmented_data():
     curl = CurlObsModel(CurlObsModel.default_config(), None)
-    img = np.random.randn(64, 3 * curl.cfg.frame_stack, 100, 100)
+    img = torch.randn(64, 3 * curl.cfg.frame_stack, 100, 100)
     data = curl.get_augmented_data(img)
-    assert data['obs_anchor'].shape == (64,9,84,84)
-    print('end')
+    assert data['obs_anchor'].shape == (64, 9, 84, 84)
