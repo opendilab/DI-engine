f97eadd6fbed69b014f96ad4705d1db9cbbcd174
wangzilin <wangzilin@sensetime.com>
2022-05-31 02:53:04 +0000

    feature(wzl): update dt model

--- ding/model/template/decision_transformer.py
+++ ding/model/template/decision_transformer.py
@@ -1,98 +1,18 @@
-"""
-The code is transplanted from https://github.com/nikhilbarhate99/min-decision-transformer
-"""
-
-from ding.utils import MODEL_REGISTRY
-from ding.torch_utils.network.transformer import Attention
-from typing import Tuple
-import math
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import ipdb
-
-
-class MaskedCausalAttention(nn.Module):
-
-    def __init__(self, h_dim, max_T, n_heads, drop_p):
-        super().__init__()
-
-        self.n_heads = n_heads
-        self.max_T = max_T
-
-        self.q_net = nn.Linear(h_dim, h_dim)
-        self.k_net = nn.Linear(h_dim, h_dim)
-        self.v_net = nn.Linear(h_dim, h_dim)
-
-        self.proj_net = nn.Linear(h_dim, h_dim)
-
-        self.att_drop = nn.Dropout(drop_p)
-        self.proj_drop = nn.Dropout(drop_p)
-
-        ones = torch.ones((max_T, max_T))
-        mask = torch.tril(ones).view(1, 1, max_T, max_T)
-
-        # register buffer makes sure mask does not get updated
-        # during backpropagation
-        self.register_buffer('mask', mask)
-
-    def forward(self, x):
-        B, T, C = x.shape  # batch size, seq length, h_dim * n_heads
-
-        N, D = self.n_heads, C // self.n_heads  # N = num heads, D = attention dim
-
-        # rearrange q, k, v as (B, N, T, D)
-        q = self.q_net(x).view(B, T, N, D).transpose(1, 2)
-        k = self.k_net(x).view(B, T, N, D).transpose(1, 2)
-        v = self.v_net(x).view(B, T, N, D).transpose(1, 2)
-
-        # weights (B, N, T, T)
-        weights = q @ k.transpose(2, 3) / math.sqrt(D)
-        # causal mask applied to weights
-        weights = weights.masked_fill(self.mask[..., :T, :T] == 0, float('-inf'))
-        # normalize weights, all -inf -> 0 after softmax
-        normalized_weights = F.softmax(weights, dim=-1)
-
-        # attention (B, N, T, D)
-        # normalized_weights.shape: (B, N, T, T)
-        # v.shape: (B, N, T, D)
-        attention = self.att_drop(normalized_weights @ v)
-
-        # gather heads and project (B, N, T, D) -> (B, T, N*D)
-        attention = attention.transpose(1, 2).contiguous().view(B, T, N * D)
-
-        out = self.proj_drop(self.proj_net(attention))
-        return out
-
-class Block(nn.Module):
-
-    def __init__(self, h_dim, max_T, n_heads, drop_p):
-        super().__init__()
-        self.attention = MaskedCausalAttention(h_dim, max_T, n_heads, drop_p)
-        self.mlp = nn.Sequential(
-            nn.Linear(h_dim, 4 * h_dim),
-            nn.GELU(),
-            nn.Linear(4 * h_dim, h_dim),
-            nn.Dropout(drop_p),
-        )
-        self.ln1 = nn.LayerNorm(h_dim)
-        self.ln2 = nn.LayerNorm(h_dim)
-
-    def forward(self, x):
-        # Attention -> LayerNorm -> MLP -> LayerNorm
-        x = x + self.attention(x)  # residual
-        x = self.ln1(x)
-        x = x + self.mlp(x)  # residual
-        x = self.ln2(x)
-        return x
-
-
 @MODEL_REGISTRY.register('dt')
 class DecisionTransformer(nn.Module):
 
     def __init__(
-        self, state_dim, act_dim, n_blocks, h_dim, context_len, n_heads, drop_p, max_timestep=4096, continuous=True
-    ):
+        self,
+        state_dim: int,
+        act_dim: int,
+        n_blocks: int,
+        h_dim: int,
+        context_len: int,
+        n_heads: int,
+        drop_p: float,
+        max_timestep: int = 4096,
+        continuous: bool = True
+    ) -> None:
         super().__init__()
         self.continuous = continuous
         self.state_dim = state_dim
@@ -100,6 +20,7 @@ class DecisionTransformer(nn.Module):
         self.h_dim = h_dim
 
         ### transformer blocks
+        # we will serially arrange `return`, `state` and `action`, so here the input_seq_len is 3 * context_len
         input_seq_len = 3 * context_len
         blocks = [Block(h_dim, input_seq_len, n_heads, drop_p) for _ in range(n_blocks)]
         self.transformer = nn.Sequential(*blocks)
@@ -110,23 +31,28 @@ class DecisionTransformer(nn.Module):
         self.embed_rtg = torch.nn.Linear(1, h_dim)
         self.embed_state = torch.nn.Linear(state_dim, h_dim)
 
-        # TODO(pu)
         if self.continuous:
-            use_action_tanh = True  # True for continuous actions
+            action_tanh = True  # True for continuous actions
             self.embed_action = torch.nn.Linear(act_dim, h_dim)
 
         else:
-            use_action_tanh = False  # False for discrete actions
+            action_tanh = False  # False for discrete actions
             self.embed_action = torch.nn.Linear(act_dim, h_dim)
             # self.embed_action = torch.nn.Embedding(act_dim, h_dim)
 
         ### prediction heads
         self.predict_rtg = torch.nn.Linear(h_dim, 1)
         self.predict_state = torch.nn.Linear(h_dim, state_dim)
-        self.predict_action = nn.Sequential(*([nn.Linear(h_dim, act_dim)] + ([nn.Tanh()] if use_action_tanh else [])))
+        self.predict_action = nn.Sequential(*([nn.Linear(h_dim, act_dim)] + ([nn.Tanh()] if action_tanh else [])))
 
-    def forward(self, timesteps, states, actions, returns_to_go):
-
+    def forward(
+        self,
+        timesteps: torch.Tensor,
+        states: torch.Tensor,
+        actions: torch.Tensor,
+        returns_to_go: torch.Tensor
+        ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        
         B, T, _ = states.shape
 
         time_embeddings = self.embed_timestep(timesteps)  # shape: (B,context_len/T,h_dim)
@@ -139,24 +65,28 @@ class DecisionTransformer(nn.Module):
 
         # stack rtg, states and actions and reshape sequence as
         # (r1, s1, a1, r2, s2, a2 ...)
-        # after stack shape: (B,3, context_len/T, h_dim)
+        # after stack shape: (B, 3, context_len/T, h_dim)
         h = torch.stack((returns_embeddings, state_embeddings, action_embeddings),
                         dim=1).permute(0, 2, 1, 3).reshape(B, 3 * T, self.h_dim)
 
         h = self.embed_ln(h)
 
+        print('before transformer')
+
         # transformer and prediction
         h = self.transformer(h)
+
+        print('after transformer')
 
         # get h reshaped such that its size = (B x 3 x T x h_dim) and
         # h[:, 0, t] is conditioned on r_0, s_0, a_0 ... r_t
         # h[:, 1, t] is conditioned on r_0, s_0, a_0 ... r_t, s_t
         # h[:, 2, t] is conditioned on r_0, s_0, a_0 ... r_t, s_t, a_t
-        h = h.reshape(B, T, 3, self.h_dim).permute(0, 2, 1, 3)
+        h = h.reshape(B, T, 3, self.h_dim)
 
         # get predictions
-        return_preds = self.predict_rtg(h[:, 2])  # predict next rtg given r, s, a
-        state_preds = self.predict_state(h[:, 2])  # predict next state given r, s, a
-        action_preds = self.predict_action(h[:, 1])  # predict action given r, s
+        return_preds = self.predict_rtg(h[..., 2, :])  # predict next rtg given r, s, a
+        state_preds = self.predict_state(h[..., 2, :])  # predict next state given r, s, a
+        action_preds = self.predict_action(h[..., 1, :])  # predict action given r, s
 
         return state_preds, action_preds, return_preds
