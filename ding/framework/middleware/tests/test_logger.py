302c824393e95e7b2769482ff46f03b0662b4de1
zhumengshen <744762298@qq.com>
2022-06-06 16:09:23 +0000

    reformat

--- ding/framework/middleware/tests/test_logger.py
+++ ding/framework/middleware/tests/test_logger.py
@@ -14,51 +14,48 @@ test_folder = "test_exp"
 test_path = path.join(os.getcwd(), test_folder)
 cfg = EasyDict({"exp_name": "test_exp"})
 
+
 def get_online_ctx():
     ctx = OnlineRLContext()
     ctx.eval_value = -10000
     ctx.train_iter = 34
     ctx.env_step = 78
-    ctx.train_output = {
-        'priority': [107],
-        '[histogram]test_histogram': [1,2,3,4,5,6],
-        'td_error': 15
-    }
+    ctx.train_output = {'priority': [107], '[histogram]test_histogram': [1, 2, 3, 4, 5, 6], 'td_error': 15}
     return ctx
+
 
 @pytest.fixture(scope='function')
 def online_ctx_output_dict():
     ctx = get_online_ctx()
     return ctx
 
+
 @pytest.fixture(scope='function')
 def online_ctx_output_deque():
     ctx = get_online_ctx()
-    ctx.train_output = deque([
-        ctx.train_output
-    ])
+    ctx.train_output = deque([ctx.train_output])
     return ctx
+
 
 @pytest.fixture(scope='function')
 def online_ctx_output_list():
     ctx = get_online_ctx()
-    ctx.train_output = [
-        ctx.train_output
-    ]
+    ctx.train_output = [ctx.train_output]
     return ctx
+
 
 @pytest.fixture(scope='function')
 def online_scalar_ctx():
     ctx = get_online_ctx()
-    ctx.train_output = {
-        '[scalars]': 1
-    }
+    ctx.train_output = {'[scalars]': 1}
     return ctx
 
+
 class MockOnlineWriter:
+
     def __init__(self):
         self.ctx = get_online_ctx()
-    
+
     def add_scalar(self, tag, scalar_value, global_step):
         if tag in ['basic/eval_episode_reward_mean-env_step', 'basic/eval_episode_reward_mean']:
             assert scalar_value == self.ctx.eval_value
@@ -74,14 +71,16 @@ class MockOnlineWriter:
             assert global_step == self.ctx.train_iter
         else:
             raise NotImplementedError('tag should be in the tags defined')
-    
+
     def add_histogram(self, tag, values, global_step):
         assert tag == 'test_histogram'
-        assert values == [1,2,3,4,5,6]
+        assert values == [1, 2, 3, 4, 5, 6]
         assert global_step in [self.ctx.train_iter, self.ctx.env_step]
+
 
 def mock_get_online_instance():
     return MockOnlineWriter()
+
 
 @pytest.mark.unittest
 class TestOnlineLogger:
@@ -97,12 +96,12 @@ class TestOnlineLogger:
     def test_online_logger_record_output_deque(self, online_ctx_output_deque):
         with patch.object(DistributedWriter, 'get_instance', new=mock_get_online_instance):
             online_logger()(online_ctx_output_deque)
-    
+
     def test_online_logger_record_output_list(self, online_ctx_output_list):
         with patch.object(DistributedWriter, 'get_instance', new=mock_get_online_instance):
             with pytest.raises(NotImplementedError) as exc_info:
                 online_logger()(online_ctx_output_list)
-    
+
     def test_online_logger_scalars(self, online_scalar_ctx):
         with patch.object(DistributedWriter, 'get_instance', new=mock_get_online_instance):
             with pytest.raises(NotImplementedError) as exc_info:
@@ -113,31 +112,28 @@ def get_offline_ctx():
     ctx = OfflineRLContext()
     ctx.eval_value = -10000000000
     ctx.train_iter = 3333
-    ctx.train_output = {
-        'priority': [107],
-        '[histogram]test_histogram': [1,2,3,4,5,6],
-        'td_error': 15
-    }
+    ctx.train_output = {'priority': [107], '[histogram]test_histogram': [1, 2, 3, 4, 5, 6], 'td_error': 15}
     return ctx
+
 
 @pytest.fixture(scope='function')
 def offline_ctx_output_dict():
     ctx = get_offline_ctx()
     return ctx
 
+
 @pytest.fixture(scope='function')
 def offline_scalar_ctx():
     ctx = get_offline_ctx()
-    ctx.train_output = {
-        '[scalars]': 1
-    }
+    ctx.train_output = {'[scalars]': 1}
     return ctx
+
 
 class MockOfflineWriter:
 
     def __init__(self):
         self.ctx = get_offline_ctx()
-    
+
     def add_scalar(self, tag, scalar_value, global_step):
         assert global_step == self.ctx.train_iter
         if tag == 'basic/eval_episode_reward_mean-train_iter':
@@ -149,8 +145,9 @@ class MockOfflineWriter:
 
     def add_histogram(self, tag, values, global_step):
         assert tag == 'test_histogram'
-        assert values == [1,2,3,4,5,6]
+        assert values == [1, 2, 3, 4, 5, 6]
         assert global_step == self.ctx.train_iter
+
 
 def mock_get_offline_instance():
     return MockOfflineWriter()
@@ -162,10 +159,8 @@ class TestOfflineLogger:
     def test_offline_logger_no_scalars(self, offline_ctx_output_dict):
         with patch.object(DistributedWriter, 'get_instance', new=mock_get_offline_instance):
             offline_logger()(offline_ctx_output_dict)
-    
+
     def test_offline_logger_scalars(self, offline_scalar_ctx):
         with patch.object(DistributedWriter, 'get_instance', new=mock_get_offline_instance):
             with pytest.raises(NotImplementedError) as exc_info:
                 offline_logger()(offline_scalar_ctx)
-
-
