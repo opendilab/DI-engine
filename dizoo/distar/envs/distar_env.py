db3afc28a8eaec9cb1b42fd639793ff896ad1772
hiha3456 <744762298@qq.com>
2022-06-09 17:26:35 +0800

    change the distar_env to fit BaseEnvManager, write test_distar_env_with_manager.py to test. change config to get self-play

--- dizoo/distar/envs/distar_env.py
+++ dizoo/distar/envs/distar_env.py
@@ -1,6 +1,6 @@
 from distar.envs.env import SC2Env
 
-from ding.envs import BaseEnv
+from ding.envs import BaseEnv, BaseEnvTimestep
 from distar.agent.default.lib.actions import ACTIONS, NUM_ACTIONS
 from distar.agent.default.lib.features import MAX_DELAY, SPATIAL_SIZE, MAX_SELECTED_UNITS_NUM
 from distar.pysc2.lib.action_dict import ACTIONS_STAT
@@ -13,15 +13,23 @@ class DIStarEnv(SC2Env,BaseEnv):
         super(DIStarEnv, self).__init__(cfg)
 
     def reset(self):
-        return super(DIStarEnv,self).reset()
+        observations, game_info, map_name = super(DIStarEnv,self).reset()
+        return observations
 
     def close(self):
         super(DIStarEnv,self).close()
 
-    def step(self,actions):
+    def step(self, actions):
         # In DI-engine, the return of BaseEnv.step is ('obs', 'reward', 'done', 'info')
         # Here in DI-star, the return is ({'raw_obs': self._obs[agent_idx], 'opponent_obs': opponent_obs, 'action_result': self._action_result[agent_idx]}, reward, episode_complete)
-        return super(DIStarEnv,self).step(actions)
+        next_observations, reward, done = super(DIStarEnv,self).step(actions)
+        timestep = BaseEnvTimestep(
+            obs = next_observations,
+            reward = reward,
+            done = done,
+            info = None
+        )
+        return timestep
 
     def seed(self, seed, dynamic_seed=False):
         self._random_seed = seed
@@ -37,63 +45,66 @@ class DIStarEnv(SC2Env,BaseEnv):
         pass
 
     def random_action(self, obs):
-        raw = obs[0]['raw_obs'].observation.raw_data
+        return_action = {}
+        for policy_id in obs.keys():
+            raw = obs[policy_id]['raw_obs'].observation.raw_data
 
-        all_unit_types = set()
-        self_unit_types = set()
-        
-        for u in raw.units:
-            # Here we select the units except “buildings that are in building progress” for simplification
-            if u.build_progress == 1:
-                all_unit_types.add(u.unit_type)
-                if u.alliance == 1:
-                    self_unit_types.add(u.unit_type)
-        
-        avail_actions = [
-            {0: {'exist_selected_types':[], 'exist_target_types':[]}}, 
-            {168:{'exist_selected_types':[], 'exist_target_types':[]}}
-        ] # no_op and raw_move_camera don't have seleted_units 
-
-        for action_id, action in ACTIONS_STAT.items():
-            exist_selected_types = list(self_unit_types.intersection(set(action['selected_type'])))
-            exist_target_types = list(all_unit_types.intersection(set(action['target_type'])))
-
-            # if an action should have target, but we don't have valid target in this observation, then discard this action
-            if len(action['target_type']) != 0 and len(exist_target_types) == 0:
-                continue
-
-            if len(exist_selected_types) > 0:
-                avail_actions.append({action_id: {'exist_selected_types':exist_selected_types, 'exist_target_types':exist_target_types}})
-        
-        current_action = random.choice(avail_actions)
-        func_id, exist_types = current_action.popitem()
-
-        if func_id not in [0, 168]:
-            correspond_selected_units = [u.tag for u in raw.units if u.unit_type in exist_types['exist_selected_types'] and u.build_progress == 1]
-            correspond_targets = [u.tag for u in raw.units if u.unit_type in exist_types['exist_target_types'] and u.build_progress == 1]
-
-            num_selected_unit = random.randint(0, min(MAX_SELECTED_UNITS_NUM, len(correspond_selected_units)))
-
-            unit_tags =  random.sample(correspond_selected_units, num_selected_unit)
-            target_unit_tag = random.choice(correspond_targets) if len(correspond_targets) > 0 else None
-
-        else:
-            unit_tags = []
-            target_unit_tag = None
+            all_unit_types = set()
+            self_unit_types = set()
             
-        data = {
-            'func_id': func_id, 
-            'skip_steps': random.randint(0, MAX_DELAY - 1),
-            # 'skip_steps': 8,
-            'queued': random.randint(0, 1),
-            'unit_tags': unit_tags, 
-            'target_unit_tag': target_unit_tag, 
-            'location': (
-                random.randint(0, SPATIAL_SIZE[0] - 1),
-                random.randint(0, SPATIAL_SIZE[1] - 1)
-            )
-        }
-        return {0:[data]}
+            for u in raw.units:
+                # Here we select the units except “buildings that are in building progress” for simplification
+                if u.build_progress == 1:
+                    all_unit_types.add(u.unit_type)
+                    if u.alliance == 1:
+                        self_unit_types.add(u.unit_type)
+            
+            avail_actions = [
+                {0: {'exist_selected_types':[], 'exist_target_types':[]}}, 
+                {168:{'exist_selected_types':[], 'exist_target_types':[]}}
+            ] # no_op and raw_move_camera don't have seleted_units 
+
+            for action_id, action in ACTIONS_STAT.items():
+                exist_selected_types = list(self_unit_types.intersection(set(action['selected_type'])))
+                exist_target_types = list(all_unit_types.intersection(set(action['target_type'])))
+
+                # if an action should have target, but we don't have valid target in this observation, then discard this action
+                if len(action['target_type']) != 0 and len(exist_target_types) == 0:
+                    continue
+
+                if len(exist_selected_types) > 0:
+                    avail_actions.append({action_id: {'exist_selected_types':exist_selected_types, 'exist_target_types':exist_target_types}})
+            
+            current_action = random.choice(avail_actions)
+            func_id, exist_types = current_action.popitem()
+
+            if func_id not in [0, 168]:
+                correspond_selected_units = [u.tag for u in raw.units if u.unit_type in exist_types['exist_selected_types'] and u.build_progress == 1]
+                correspond_targets = [u.tag for u in raw.units if u.unit_type in exist_types['exist_target_types'] and u.build_progress == 1]
+
+                num_selected_unit = random.randint(0, min(MAX_SELECTED_UNITS_NUM, len(correspond_selected_units)))
+
+                unit_tags =  random.sample(correspond_selected_units, num_selected_unit)
+                target_unit_tag = random.choice(correspond_targets) if len(correspond_targets) > 0 else None
+
+            else:
+                unit_tags = []
+                target_unit_tag = None
+                
+            data = {
+                'func_id': func_id, 
+                'skip_steps': random.randint(0, MAX_DELAY - 1),
+                # 'skip_steps': 8,
+                'queued': random.randint(0, 1),
+                'unit_tags': unit_tags, 
+                'target_unit_tag': target_unit_tag, 
+                'location': (
+                    random.randint(0, SPATIAL_SIZE[0] - 1),
+                    random.randint(0, SPATIAL_SIZE[1] - 1)
+                )
+            }
+            return_action[policy_id] = [data]
+        return return_action
 
 
     @property
