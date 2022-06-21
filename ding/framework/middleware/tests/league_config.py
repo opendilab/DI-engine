338a2a2c3f82d1da966122db8efe625361cd8850
hiha3456 <744762298@qq.com>
2022-06-21 17:07:59 +0800

    change a bit env_supervisor so it could run DI-star env

--- ding/framework/middleware/tests/league_config.py
+++ ding/framework/middleware/tests/league_config.py
@@ -4,14 +4,15 @@ cfg = EasyDict(
         'env': {
             'manager': {
                 'episode_num': 100000,
-                'max_retry': 1,
-                'retry_type': 'reset',
+                'max_retry': 1000,
+                'retry_type': 'renew',
                 'auto_reset': True,
                 'step_timeout': None,
                 'reset_timeout': None,
                 'retry_waiting_time': 0.1,
                 'cfg_type': 'BaseEnvManagerDict',
-                'shared_memory': False
+                'shared_memory': False,
+                'return_original_data': True
             },
             'collector_env_num': 1,
             'evaluator_env_num': 1,
