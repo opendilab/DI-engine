f843b19647f9b29c44c8d13fc554e8bc8c992f68
蒲源 <48008469+puyuan1996@users.noreply.github.com>
2022-06-23 23:36:45 +0800

    feature(pu): add board games environments (#356)
    * feature(pu): add board_gmaes env including tictactoe, gomoku, chess, go, atari
    
    * style(pu): yapf format
    
    * polish(pu): refactor
    
    * polish(pu): polish as review
    
    * polish(pu): move atari_muzero_env to dizoo/atari
    
    * polish(pu):polish as review
    
    * fix(pu): fix tictactoe expert action
    
    * polish(pu): polish board_games env
    
    * style(pu): yapf format

--- ding/utils/tests/test_compression_helper.py
+++ ding/utils/tests/test_compression_helper.py
@@ -1,5 +1,5 @@
 import random
-
+import numpy as np
 from ding.utils.compression_helper import get_data_compressor, get_data_decompressor
 
 import pytest
@@ -18,3 +18,11 @@ class TestCompression():
             decompressor = get_data_decompressor(s)
             data = self.get_step_data()
             assert data == decompressor(compressor(data))
+
+    def test_arr_to_st(self):
+        data = np.random.randint(0, 255, (96, 96, 3), dtype=np.uint8)
+        compress_names = ['jpeg']
+        for s in compress_names:
+            compressor = get_data_compressor(s)
+            decompressor = get_data_decompressor(s)
+            assert data.shape == decompressor(compressor(data)).shape
