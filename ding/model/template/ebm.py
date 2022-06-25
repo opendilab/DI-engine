c6c98ff93196d2d29da86e0ba53bd640bb401f22
ZHZisZZ <zhanhui@umich.edu>
2022-06-25 20:59:27 +0800

    temp save

--- ding/model/template/ebm.py
+++ ding/model/template/ebm.py
@@ -1,14 +1,95 @@
+"""Adapted from https://github.com/kevinzakka/ibc. 
+"""
+
 from typing import Union, Dict, Optional
 from easydict import EasyDict
 import numpy as np
 import torch
 import torch.nn as nn
 
-from ding.utils import SequenceType, squeeze, MODEL_REGISTRY
-from ..common import RegressionHead, ReparameterizationHead, DiscreteHead, MultiHead, \
-    FCEncoder, ConvEncoder
+from abc import ABC, abstractmethod
 
+from ding.utils import MODEL_REGISTRY, OPTIMIZER_REGISTRY
+
+
+def create_optimizer(optimizer_config):
+    return MODEL_REGISTRY.build(optimizer_config.pop("type"), **optimizer_config)
+
+class StochasticOptimizer(ABC):
+    @abstractmethod
+    def sample(self, batch_size: int, ebm: nn.Module) -> torch.Tensor:
+        """Sample counter-negatives for feeding to the InfoNCE objective."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def infer(self, x: torch.Tensor, ebm: nn.Module) -> torch.Tensor:
+        """Optimize for the best action conditioned on the current observation."""
+        raise NotImplementedError
+
+@OPTIMIZER_REGISTRY.register('dfo')
+class DFO(StochasticOptimizer):
+    def __init__(
+        self,
+        bounds: list[list],
+        noise_scale: float = 0.33,
+        noise_shrink: float = 0.5,
+        iters: int = 3,
+        train_samples: int = 256,
+        inference_samples: int = 2 ** 14,
+    ):
+        self.bounds = bounds
+        self.noise_scale = noise_scale
+        self.noise_shrink = noise_shrink
+        self.iters = iters
+        self.train_samples = train_samples
+        self.inference_samples = inference_samples
+
+    def sample(self, batch_size: int, ebm: nn.Module) -> torch.Tensor:
+        pass
+
+    def infer(self, obs: torch.Tensor, ebm: nn.Module) -> torch.Tensor:
+        pass
+
+@OPTIMIZER_REGISTRY.register('mcmc')
+class MCMC(StochasticOptimizer):
+    pass
 
 @MODEL_REGISTRY.register('ebm')
 class EBM(nn.Module):
-    pass
+    
+    def __init__(
+        self,
+        obs_shape: int,
+        action_shape: int,
+        *args, # TODO: parameters
+        optimizer_config: dict,
+    ):
+        self.net = nn.Linear(obs_shape + action_shape, 1)
+        self.optimizer = create_optimizer(optimizer_config)
+
+    def forward(self, obs, action=None):
+        if not action:
+            return self.optimizer.infer(obs, self)
+        else:
+            # obs: (B, Do)
+            # action: (B, N+1, Da)
+            energy = None
+            return energy
+
+
+
+@MODEL_REGISTRY.register('arebm')
+class AutoregressiveEBM(nn.Module):
+
+    def __init__(
+        self,
+        obs_shape: int,
+        action_shape: int,
+        *args, # TODO: parameters
+        optimizer_config: dict,
+    ):
+        self.net = nn.Linear(obs_shape + action_shape, 1)
+        self.optimizer = create_optimizer(optimizer_config)
+
+    def forward(self, obs, action=None):
+        pass
--- ding/policy/ibc.py
+++ ding/policy/ibc.py
@@ -1,86 +1,107 @@
 from typing import Dict, Any, List, Tuple
+from collections import namedtuple
 
 from abc import ABC, abstractmethod
 
 import torch
 from torch import Tensor
 from torch import nn
+
 from easydict import EasyDict
 
 from ding.torch_utils import to_device
+from ding.utils.data import default_collate, default_decollate
 from ding.utils import POLICY_REGISTRY, OPTIMIZER_REGISTRY
 from ding.policy.common_utils import default_preprocess_learn
-from ding.policy import Policy
+from ding.policy import BehaviourCloningPolicy
 
 
-class StochasticOptimizer(ABC):
-
-    @abstractmethod
-    def sample(self, batch_size: int, ebm: nn.Module) -> torch.Tensor:
-        """Sample counter-negatives for feeding to the InfoNCE objective."""
-        raise NotImplementedError
-
-    @abstractmethod
-    def infer(self, x: torch.Tensor, ebm: nn.Module) -> torch.Tensor:
-        """Optimize for the best action conditioned on the current observation."""
-        raise NotImplementedError
-
-
-@OPTIMIZER_REGISTRY.register('vanilladf')
-class VanillaDerivativeFreeOptimizer(StochasticOptimizer):
-    r"""
-    https://github.com/kevinzakka/ibc
-    """
-    pass
-
-
-@OPTIMIZER_REGISTRY.register('autoregressivedf')
-class AutoregressiveDerivativeFreeOptimizer(StochasticOptimizer):
-    r"""
-    https://github.com/conormdurkan/autoregressive-energy-machines
-    """
-    pass
-
-
-@OPTIMIZER_REGISTRY.register('langevinmcmc')
-class LangevinMCMCOptimizer(StochasticOptimizer):
-    r"""
-    https://github.com/google-research/ibc
-    """
-    pass
-
-
+# TODO: change to BehavioralCloning or BehaviorCloning
 @POLICY_REGISTRY.register('ibc')
-class IBCPolicy(Policy):
+class ImplicitBehaviourCloningPolicy(BehaviourCloningPolicy):
 
-    def _init_learn(self) -> None:
-        pass
-
-    def _forward_learn(self, data: dict) -> Dict[str, Any]:
-        pass
-
-    def _state_dict_learn(self) -> Dict[str, Any]:
-        pass
-
-    def _load_state_dict_learn(self, state_dict: Dict[str, Any]) -> None:
-        pass
-
-    # def _init_collect(self) -> None:
-
-    # def _forward_collect(self, data: dict) -> dict:
-
-    # def _process_transition(self, obs: Any, policy_output: dict, timestep: namedtuple) -> dict:
-
-    # def _get_train_sample(self, data: list) -> Union[None, List[Any]]:
-
-    def _init_eval(self) -> None:
-        pass
-
-    def _forward_eval(self, data: dict) -> dict:
-        pass
+    config = dict(
+        type='ibc',
+        cuda=False,
+        on_policy=False,
+        continuous=True,
+        model=dict(
+            type='ebm', 
+            type='arebm',
+            optimizer=dict(
+                type='mcmc',
+                type='dfo',
+                noise_scale: float
+                noise_shrink: float
+                iters: int
+                train_samples: int
+                inference_samples: int
+                bounds: np.ndarray
+            )
+        ),
+        learn=dict(
+            # multi_gpu=False,
+            # update_per_collect=1,
+            batch_size=32,
+            optim=dict(
+                learning_rate=1e-5,
+                weight_decay=0.0,
+                beta1=0.9,
+                beta2=0.999,
+                lr_scheduler_step=100,
+                lr_scheduler_gamma=0.99,
+            )
+        ),
+        collect=dict(unroll_len=1, ),
+        eval=dict(),
+        other=dict(replay_buffer=dict(replay_buffer_size=10000, )),
+    )
+    
+    def _init_learn(self):
+        optim_cfg = self._cfg.optim  
+        self._optimizer = torch.optim.Adam(
+            self._model.parameters(),
+            lr=optim_cfg.learning_rate,
+            weight_decay=optim_cfg.weight_decay,
+            betas=(optim_cfg.beta1, optim_cfg.beta2),
+        )
+        self._scheduler = torch.optim.lr_scheduler.StepLR(
+            self._optimizer,
+            step_size=optim_cfg.lr_scheduler_step,
+            gamma=optim_cfg.lr_scheduler_gamma,
+        )
 
     def default_model(self) -> Tuple[str, List[str]]:
         pass
+    
+    def _forward_learn(self, data):
+        if not isinstance(data, dict):
+            data = default_collate(data)
+        if self._cuda:
+            data = to_device(data, self._device)
+        self._learn_model.train()
 
-    def _monitor_vars_learn(self) -> List[str]:
+    def _monitor_vars_learn(self):
         pass
+
+    def _init_eval(self):
+        pass
+
+    @torch.no_grad()
+    def _forward_eval(self, data):
+        pass
+
+    # =================================================================== #
+    # Implicit Behavioral Cloning does not need `collect`-related functions
+    # =================================================================== #
+    def _init_collect(self):
+        raise NotImplementedError
+
+    def _forward_collect(self, data: Dict[int, Any], eps: float) -> Dict[int, Any]:
+        raise NotImplementedError
+
+    def _process_transition(self, obs: Any, model_output: dict, timestep: namedtuple) -> dict:
+        raise NotImplementedError
+
+    def _get_train_sample(self, data: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        raise NotImplementedError
