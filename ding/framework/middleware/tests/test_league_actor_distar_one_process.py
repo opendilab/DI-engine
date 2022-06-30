72803ef9f5c7da1b450b5ba7cf05d54479f989e3
hiha3456 <744762298@qq.com>
2022-06-30 13:58:24 +0800

    fix bug

--- ding/framework/middleware/tests/test_league_actor_distar_one_process.py
+++ ding/framework/middleware/tests/test_league_actor_distar_one_process.py
@@ -19,6 +19,7 @@ from ding.framework.supervisor import ChildType
 from ding.framework.middleware import StepLeagueActor
 from ding.framework.middleware.functional import ActorData
 from ding.framework.middleware.tests import DIStarMockPolicy
+from ding.framework.middleware.league_learner import LearnerModel
 from ding.framework.middleware.functional.collector import battle_inferencer_for_distar, battle_rolloutor_for_distar
 
 env_cfg = dict(
