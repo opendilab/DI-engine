a3c9e3942d63253407b6b17b4b950a8da0378d46
hiha3456 <744762298@qq.com>
2022-07-06 14:29:52 +0800

    tests to test the performance againist bot using pretrained mdoel

--- dizoo/distar/policy/test_distar_policy.py
+++ dizoo/distar/policy/test_distar_policy.py
@@ -3,6 +3,9 @@ import pytest
 from dizoo.distar.policy import DIStarPolicy
 from dizoo.distar.envs import get_fake_rl_trajectory, get_fake_env_reset_data, get_fake_env_step_data
 
+import torch
+from ding.utils import set_pkg_seed
+
 
 @pytest.mark.envtest
 class TestDIStarPolicy:
@@ -17,8 +20,16 @@ class TestDIStarPolicy:
     def test_forward_collect(self):
         policy = DIStarPolicy(DIStarPolicy.default_config(), enable_field=['collect'])
         policy = policy.collect_mode
+
+        rl_model = torch.load('./rl_model.pth')
+        policy.load_state_dict(rl_model)
+        
         data = get_fake_env_reset_data()
         policy.reset(data)
-        data = get_fake_env_step_data()
         output = policy.forward(data)
-        print(output)
+
+if __name__ == '__main__':
+    set_pkg_seed(0, use_cuda=False)
+    torch.set_printoptions(profile="full")
+    test = TestDIStarPolicy()
+    test.test_forward_collect()
