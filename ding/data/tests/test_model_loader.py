9f5087b3c661e7820f59a8e9d85935302e7276cd
Xu Jingxin <sailxjx@gmail.com>
2022-07-21 15:28:00 +0800

    Add model loader benchmark

--- ding/data/tests/test_model_loader.py
+++ ding/data/tests/test_model_loader.py
@@ -8,6 +8,7 @@ from ding.model import DQN
 from ding.config import compile_config
 from dizoo.atari.config.serial.pong.pong_dqn_config import main_config, create_config
 from os import path
+import torch
 
 
 @pytest.mark.unittest
@@ -34,6 +35,35 @@ def test_model_loader():
 
         state_dict = loader.load(model_storage)
         model.load_state_dict(state_dict)
+    finally:
+        if path.exists(tempdir):
+            shutil.rmtree(tempdir)
+        loader.shutdown()
+
+
+@pytest.mark.benchmark
+def test_model_loader_benchmark():
+    model = torch.nn.Sequential(torch.nn.Linear(1024, 1024), torch.nn.Linear(1024, 100))  # 40MB
+    tempdir = path.join(tempfile.gettempdir(), "test_model_loader")
+    loader = FileModelLoader(model=model, dirname=tempdir)
+
+    try:
+        loader.start()
+        count = 0
+
+        def send_callback(_):
+            nonlocal count
+            count += 1
+
+        start = time()
+        for _ in range(5):
+            loader.save(send_callback)
+            sleep(0.2)
+
+        while count < 5:
+            sleep(0.001)
+
+        assert time() - start < 1.2
     finally:
         if path.exists(tempdir):
             shutil.rmtree(tempdir)
