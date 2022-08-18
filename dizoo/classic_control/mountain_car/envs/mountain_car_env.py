6ec52afacd71ed7d488ca023a66f5e55ff37f1f7
tc-i-am <sar.021@gmail.com>
2022-08-18 04:02:43 +0000

    figuring out init, reset, seed, close

--- dizoo/classic_control/mountain_car/envs/mountain_car_env.py
+++ dizoo/classic_control/mountain_car/envs/mountain_car_env.py
@@ -8,17 +8,59 @@ from ding.utils import ENV_REGISTRY
 @ENV_REGISTRY.register('mountain_car')
 class MountainCar(BaseEnv):
 
-    def __init__(self, cfg: dict) -> None:
+    def __init__(self, cfg: dict = {}) -> None:
         self._cfg = cfg
-        self._env = gym.make('MountainCar-v0')
         self._init_flag = False
-        pass
+        self._replay_path = None
 
     def seed(self, seed: int, dynamic_seed: bool = True) -> None:
-        pass
+        self._seed = seed
+        self._dynamic_seed = dynamic_seed
+        np.random.seed(self._seed)
 
     def reset(self) -> np.ndarray:
-        pass
+        # Instantiate environment if not already done so
+        if not self._init_flag:
+            self._env = gym.make('MountainCar-v0')
+        self._init_flag = True
+
+        # Check if we have a valid replay path and save replay video accordingly
+        if self._replay_path is not None:
+            self._env = gym.wrappers.RecordVideo(
+                self._env,
+                video_folder=self._replay_path,
+                episode_trigger=lambda episode_id: True,
+                name_prefix='rl-video-{}'.format(id(self))
+            )
+
+        # Set the seeds for randomization.
+        if hasattr(self, '_seed') and hasattr(self, '_dynamic_seed') and self._dynamic_seed:
+            np_seed = 100 * np.random.randint(1, 1000)
+            self._env.seed(self._seed + np_seed)
+            self._action_space.seed(self._seed + np_seed)
+        elif hasattr(self, '_seed'):
+            self._env.seed(self._seed)
+            self._action_space.seed(self._seed)
+        
+        # Get first observation from original environment
+        obs = self._env.reset()
+
+        # Convert to numpy array as output
+        obs = to_ndarray(obs).astype(np.float32)
+
+        # Init final reward : cumulative sum of the real rewards obtained by a whole episode, 
+        # used to evaluate the agent Performance on this environment, not used for training.
+        self._final_eval_reward = 0.
+        return obs
 
     def step(self, action: np.ndarray) -> BaseEnvTimestep:
         pass
+
+    def close(self) -> None:
+        # If init flag is False, then reset() was never run, no point closing.
+        if self._init_flag:
+            self._env.close()
+        self._init_flag = False
+
+if __name__ == '__main__':
+    mtcar = MountainCar()
