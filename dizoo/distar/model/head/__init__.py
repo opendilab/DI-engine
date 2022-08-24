2e60364a3155185632cce1d1349cbcc72ae0f0d6
zhumengshen <zhumengshen@pjlab.org.cn>
2022-08-24 06:56:24 +0000

    move out the distar files into DI-star

--- dizoo/distar/model/head/__init__.py
+++
@@ -1,2 +0,0 @@
-from .action_arg_head import DelayHead, SelectedUnitsHead, TargetUnitHead, LocationHead, QueuedHead
-from .action_type_head import ActionTypeHead
--- dizoo/distar/model/obs_encoder/__init__.py
+++
@@ -1,4 +0,0 @@
-from .scalar_encoder import ScalarEncoder
-from .spatial_encoder import SpatialEncoder
-from .entity_encoder import EntityEncoder
-from .value_encoder import ValueEncoder
