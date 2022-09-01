ebc6011dd002e1bff91109c8ff03fe946482d772
nighood <nighoodren@gmail.com>
2022-09-01 08:11:59 +0000

    polish(rjy): docker part

--- dizoo/dmc2gym/config/dmc2gym_sac_pixel_config.py
+++ dizoo/dmc2gym/config/dmc2gym_sac_pixel_config.py
@@ -1,29 +1,32 @@
 from easydict import EasyDict
 
 dmc2gym_sac_config = dict(
-    exp_name='dmc2gym_sacpixel_seed0',
+    exp_name='dmc2gym_sac_pixel_seed0',
     env=dict(
         env_id='dmc2gym-v0',
-        domain_name = "cartpole",
-        task_name = "swingup",
-        frame_skip = 2,
-        from_pixels = True,
-        channels_first = True,      # obs shape (3, height, width) if True
-        collector_env_num=1,
+        domain_name="cartpole",
+        task_name="swingup",
+        frame_skip=2,
+        from_pixels=True,
+        channels_first=True,  # obs shape (3, height, width) if True
+        collector_env_num=16,
         evaluator_env_num=8,
-        use_act_scale=True,
         n_evaluator_episode=8,
-        stop_value=200,
-        # time_limit = None,
+        use_act_scale=True,
+        stop_value=1e6,
+        manager=dict(shared_memory=False, ),
+        replay_path='./dmc2gym_cartpole_swingup_pixel_sac_eval/video',
     ),
     policy=dict(
+        model_type='pixel',
         cuda=True,
         random_collect_size=10000,
+        load_path="/root/dmc2gym_cartpole_swingup_pixel_sac_eval/ckpt/ckpt_best.pth.tar",
         model=dict(
-            obs_shape=(3,100,100),
+            obs_shape=(3, 100, 100),
             action_shape=1,
             twin_critic=True,
-            encoder_hidden_size_list = [128, 128, 64],
+            encoder_hidden_size_list=[128, 128, 64],
             actor_head_hidden_size=64,
             critic_head_hidden_size=64,
         ),
@@ -33,12 +36,13 @@ dmc2gym_sac_config = dict(
             learning_rate_q=1e-3,
             learning_rate_policy=1e-3,
             learning_rate_alpha=3e-4,
-            ignore_done=False,
+            ignore_done=True,
             target_theta=0.005,
             discount_factor=0.99,
             alpha=0.2,
             reparameterization=True,
-            auto_alpha=False,
+            auto_alpha=True,
+
         ),
         collect=dict(
             n_sample=1,
@@ -46,7 +50,7 @@ dmc2gym_sac_config = dict(
         ),
         command=dict(),
         eval=dict(),
-        other=dict(replay_buffer=dict(replay_buffer_size=1000000, ), ),
+        other=dict(replay_buffer=dict(replay_buffer_size=100000, ), ),
     ),
 )
 
@@ -68,8 +72,22 @@ dmc2gym_sac_create_config = dict(
 dmc2gym_sac_create_config = EasyDict(dmc2gym_sac_create_config)
 create_config = dmc2gym_sac_create_config
 
+# if __name__ == "__main__":
+#     # or you can enter `ding -m serial -c dmc2gym_sac_pixel_config.py -s 0`
+#     from ding.entry import serial_pipeline
+#     serial_pipeline([main_config, create_config], seed=0)
+
 
 if __name__ == "__main__":
-    # or you can enter `ding -m serial -c dmc2gym_sac_config.py -s 0`
+    import copy
+    import argparse
     from ding.entry import serial_pipeline
-    serial_pipeline([main_config, create_config], seed=0)
+
+    for seed in [0, 1, 2]:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--seed', '-s', type=int, default=seed)
+        args = parser.parse_args()
+
+        main_config.exp_name = 'dmc2gym_sac_pixel_rbs1e5_cen8_idt_aat/' + 'seed' + f'{args.seed}'+ '_5M'
+        serial_pipeline([copy.deepcopy(main_config), copy.deepcopy(create_config)], seed=args.seed,
+                        max_env_step=int(5e6))
--- dizoo/dmc2gym/config/dmc2gym_sac_state_config.py
+++ dizoo/dmc2gym/config/dmc2gym_sac_state_config.py
@@ -4,21 +4,23 @@ dmc2gym_sac_config = dict(
     exp_name='dmc2gym_sac_seed0',
     env=dict(
         env_id='dmc2gym-v0',
-        domain_name = "cartpole",
-        task_name = "swingup",
-        frame_skip = 2,
-        from_pixels = False,
-        channels_first = True,      # obs shape (3, height, width) if True
+        domain_name="cartpole",
+        task_name="swingup",
+        frame_skip=2,
+        from_pixels=False,
+        channels_first=True,  # obs shape (3, height, width) if True
         collector_env_num=1,
         evaluator_env_num=8,
-        use_act_scale=True,
         n_evaluator_episode=8,
-        stop_value=200,
-        # time_limit = None,
+        stop_value=1e6,
+        replay_path='./dmc2gym_cartpole_swingup_state_sac_eval/video',
+        # limit_time = None,    # default 10 (s)
     ),
     policy=dict(
+        model_type='state',
         cuda=True,
         random_collect_size=10000,
+        load_path="/root/dmc2gym_cartpole_swingup_state_sac_eval/ckpt/ckpt_best.pth.tar",
         model=dict(
             obs_shape=5,
             action_shape=1,
@@ -69,7 +71,25 @@ dmc2gym_sac_create_config = EasyDict(dmc2gym_sac_create_config)
 create_config = dmc2gym_sac_create_config
 
 
+# if __name__ == "__main__":
+#     # or you can enter `ding -m serial -c dmc2gym_sac_config.py -s 0`
+#     from ding.entry import serial_pipeline
+#     serial_pipeline([main_config, create_config], seed=0)
+
+def train(args):
+    main_config.exp_name = 'dmc2gym_sac_state_old_check/' + 'seed' + f'{args.seed}' + '_5M'
+    serial_pipeline([copy.deepcopy(main_config), copy.deepcopy(create_config)], seed=args.seed,
+                              max_env_step=int(5e6))
+
+
 if __name__ == "__main__":
-    # or you can enter `ding -m serial -c dmc2gym_sac_config.py -s 0`
+    import copy
+    import argparse
     from ding.entry import serial_pipeline
-    serial_pipeline([main_config, create_config], seed=0)
+
+    for seed in [0,1,2]:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--seed', '-s', type=int, default=seed)
+        args = parser.parse_args()
+
+        train(args)
