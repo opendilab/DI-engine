d14c4698755f5c5a98865ec0040d481aec8db8d1
zms <36892400+hiha3456@users.noreply.github.com>
2022-09-05 12:58:39 +0800

    feature(zms): update ctx to dataclass (#467)
    * change ctx type from dict to dataclass
    
    * add fake data of context, fix bug of Offline RL in logger of trainer and evaluator
    
    * fix bug
    
    * reformat
    
    * rm main
    
    * reformat
    
    * add has_attr in context
    
    * Add comments for __post_init__; Add type and default value for some fields

--- ding/framework/tests/test_context.py
+++ ding/framework/tests/test_context.py
@@ -1,14 +1,59 @@
 import pytest
-from ding.framework import Context
 import pickle
+import numpy as np
+from ding.framework import Context, OnlineRLContext, OfflineRLContext
+from dataclasses import dataclass
+
+
+@dataclass
+class MockContext(Context):
+    hello: str = "world"
+    keep_me: int = 0
+    not_keep_me: int = 0
 
 
 @pytest.mark.unittest
 def test_pickable():
-    ctx = Context(hello="world", keep_me=True)
+    ctx = MockContext()
     ctx.keep("keep_me")
     _ctx = pickle.loads(pickle.dumps(ctx))
     assert _ctx.hello == "world"
 
+    ctx.keep_me += 1
+    ctx.not_keep_me += 1
+
     _ctx = ctx.renew()
-    assert _ctx.keep_me
+    assert _ctx.keep_me == 1
+    assert _ctx.not_keep_me == 0
+
+
+@pytest.mark.unittest
+def test_online():
+    ctx = OnlineRLContext()
+    assert ctx.env_step == 0
+    assert ctx.eval_value == -np.inf
+
+    ctx.env_step += 1
+    ctx.eval_value = 1
+    assert ctx.env_step == 1
+    assert ctx.eval_value == 1
+
+    _ctx = ctx.renew()
+    assert _ctx.env_step == 1
+    assert _ctx.eval_value == -np.inf
+
+
+@pytest.mark.unittest
+def test_offline():
+    ctx = OfflineRLContext()
+    assert ctx.train_iter == 0
+    assert ctx.eval_value == -np.inf
+
+    ctx.train_iter += 1
+    ctx.eval_value = 1
+    assert ctx.train_iter == 1
+    assert ctx.eval_value == 1
+
+    _ctx = ctx.renew()
+    assert _ctx.train_iter == 1
+    assert _ctx.eval_value == -np.inf
