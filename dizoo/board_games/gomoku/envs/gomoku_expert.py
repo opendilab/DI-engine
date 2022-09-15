843397802cf68e13ce07e84f34f087a5fbd40fe2
nighood <nighoodren@gmail.com>
2022-09-15 23:26:02 +0800

    polish(rjy): preliminary polish

--- dizoo/board_games/gomoku/envs/gomoku_expert.py
+++ dizoo/board_games/gomoku/envs/gomoku_expert.py
@@ -1,43 +1,94 @@
-# encoding: utf-8
+# Reference link:
+# https://github.com/LouisCaixuran/gomoku/blob/c1b6d508522d9e8c78be827f326bbee54c4dfd8b/gomoku/expert.py
 
 from collections import defaultdict
 import logging
 import numpy as np
 
-
 class GomokuExpert(object):
+    """
+        Overview:
+            The ``GomokuExpert`` used to output rule-based gomoku expert actions. \
+            Input is gomoku board obs(:obj:`np,array`) of shape ``(board_w, board_h)`` and returns action (:obj:`Int`) \
+            The output action is the sequence number i*board_w+j of the placement position (i, j)
+        Interfaces:
+            ``__init__``, ``get_action``.
+    """
     def __init__(self):
-        self.grade = 100
+        """
+        Overview:
+            Init the ``GomokuExpert``.
+        """
         # The weight of pieces of the same color
-        self.max_value = 10012345
+        self.grade = 100
         # Indicates that it has already reached 5, reaching the maximum weight
+        self.max_value = 10012345
         self.init_board_flag = False
 
-    def location_to_move(self, i, j):
-        # location = (i,j),move=j+i*width
+    def location_to_action(self, i, j):
+        """
+        Overview:
+            Convert coordinate to serial number.
+        Arguments:
+            - i (:obj:`Int`): X-axis.
+            - j (:obj:`Int`): Y-axis.
+        Returns:
+            - action (:obj:`Int`): The serial number of the entered coordinates on the chessboard.
+        """
+        # location = (i,j), action=j+i*width
         return j + i * self.board_width
 
-    def move_to_location(self, move):
-        # location = (i,j),dot=j+i*width
+    def action_to_location(self, action):
+        """
+        Overview:
+            Convert serial number to coordinate.
+        Arguments:
+            - action (:obj:`Int`): The serial number of the entered coordinates on the chessboard.
 
-        j = move % self.board_width
-        i = move // self.board_width
+        Returns:
+            - [i, j].
+        """
+        # location = (i,j), action=j+i*width
+        j = action % self.board_width
+        i = action // self.board_width
         return [i, j]
-
+ 
     def get_loc_player(self, i, j):
-        move = self.location_to_move(i, j)
-        return self.board_status[move]
+        """
+        Overview:
+            Returns the state of the pawn at the given coordinates.
+        Arguments:
+            - [i, j](:obj:`[Int, Int]`): The coordinate on the chessboard.
+
+        Returns:
+            - board_status: \
+            0: no pawns, \
+            1: player 1, \
+            2: player 2.
+        """
+        action = self.location_to_action(i, j)
+        return self.board_status[action]
 
     def scan_leftright(self, i, j, player):
+        """
+        Overview:
+            Calculate the estimated value of the pawn from left to right when the player moves at (i,j)
+        Arguments:
+            - i (:obj:`Int`): X-axis.
+            - j (:obj:`Int`): Y-axis.
+            - player (:obj:`Int`): Current player.
+
+        Returns:
+            - value: Situation valuation in this direction.
+        """
+        # Count the number of consecutive own flags or empty chess pieces
         value = 0
         count = 0
-        # Count the number of consecutive own flags or empty chess pieces
         grade = self.grade
-
+        # scan left
         m, n = i, j - 1
-        # move one step to the left
         while n >= 0:
-            # Incoming move position and player
+            # Incoming action position and player
             is_continue, value, grade = self.caculate_once_value(m, n,
                                                                  player,
                                                                  value,
@@ -46,7 +97,7 @@ class GomokuExpert(object):
                 n = n - 1
             else:
                 break
-                # move one step to the left
+                # action one step to the left
             count += 1
 
         grade = self.grade
@@ -61,20 +112,30 @@ class GomokuExpert(object):
                 n = n + 1
             else:
                 break
-                # move one step to the right
+                # action one step to the right
             count += 1
-
+        # Returns the value if there are four consecutive pawn in this direction, otherwise 0
         return value if count >= 4 else 0
-        # If this direction can be connected into 5, return value, otherwise it is 0
 
     def scan_updown(self, i, j, player):
+        """
+        Overview:
+            Calculate the estimated value of the pawn from up to down when the player moves at (i,j)
+        Arguments:
+            - i (:obj:`Int`): X-axis.
+            - j (:obj:`Int`): Y-axis.
+            - player (:obj:`Int`): Current player.
+
+        Returns:
+            - value: Situation valuation in this direction.
+        """
         value = 0
         count = 0
         # Count the number of consecutive own chess pieces or empty chess pieces
         grade = self.grade
 
         m, n = i - 1, j
-        # move one step up
+        # action one step up
         while m >= 0:
             is_continue, value, grade = self.caculate_once_value(m, n,
                                                                  player,
@@ -84,7 +145,7 @@ class GomokuExpert(object):
                 m = m - 1
             else:
                 break
-                # move one step up
+                # action one step up
             count += 1
 
         grade = self.grade
@@ -100,13 +161,24 @@ class GomokuExpert(object):
                 m = m + 1
             else:
                 break
-                # move down one step
+                # action down one step
             count += 1
 
         return value if count >= 4 else 0
         # If this direction can be connected into 5, return value, otherwise it is 0
 
     def scan_left_updown(self, i, j, player):
+        """
+        Overview:
+            Calculate the estimated value of the pawn from top left to bottom right when the player moves at (i,j)
+        Arguments:
+            - i (:obj:`Int`): X-axis.
+            - j (:obj:`Int`): Y-axis.
+            - player (:obj:`Int`): Current player.
+
+        Returns:
+            - value: Situation valuation in this direction.
+        """
         value = 0
         count = 0
         # Count the number of consecutive own chess pieces or empty chess pieces
@@ -138,13 +210,24 @@ class GomokuExpert(object):
                 m, n = m + 1, n + 1
             else:
                 break
-                # Continue to move one step down to the right
+                # Continue to action one step down to the right
             count += 1
 
         return value if count >= 4 else 0
         # If this direction can be connected into 5, return value, otherwise it is 0
 
     def scan_right_updown(self, i, j, player):
+        """
+        Overview:
+            Calculate the estimated value of the pawn from top right to bottom left when the player moves at (i,j)
+        Arguments:
+            - i (:obj:`Int`): X-axis.
+            - j (:obj:`Int`): Y-axis.
+            - player (:obj:`Int`): Current player.
+
+        Returns:
+            - value: Situation valuation in this direction.
+        """
         value = 0
         count = 0
         # Count the number of consecutive own chess pieces or empty chess pieces
@@ -175,99 +258,129 @@ class GomokuExpert(object):
                 m, n = m - 1, n + 1
             else:
                 break
-                # Continue to move up one step to the right
+                # Continue to action up one step to the right
             count += 1
 
         return value if count >= 4 else 0
         # If this direction can be connected into 5, return value, otherwise it is 0
 
     def caculate_once_value(self, m, n, player, value, grade):
-        # Calculate the value of the player at the specified position
+        """
+        Overview:
+            Calculate the income brought by the pawns adjacent to the position (m,n) \
+            when the player places the pawn at the specified position (i,j) \
+            in the current situation
+        Arguments:
+            - m (:obj:`Int`): x.
+            - n (:obj:`Int`): y.
+            - player (:obj:`Int`): current chess player.
+            - value (:obj:`Int`): The current position (the pawn is at (i,j)) is evaluated.
+            - grade (:obj:`Int`): The weight of the pawn in the current position
 
-        # return 0 1 2
+        Returns:
+            - is_continue: 
+            - value:
+            - grade: 
+        """
         loc_player = self.get_loc_player(m, n)
-
         if loc_player == player:
             value += grade
         elif loc_player == 0:
             value += 1
-            grade = grade / 10
             # When encountering an empty chess, reduce the weight of subsequent black chess
+            grade = grade / 10
         else:
             # opponent's pawn
             value -= 5
+            # When encountering an opponent's chess, return
             return 0, value, grade
-            # When encountering an opponent's chess, end the follow-up move
         # 1 means 'is_continue'
         return 1, value, grade
 
     def evaluate_all_value(self, player):
-        # Evaluate all vacancies for value
+        """
+        Overview:
+            Calculate the estimates of all possible positions, \
+            and choose the most favorable position from them.
+        Arguments:
+            - player (:obj:`Int`): current chess player.
 
-        self.move_value = defaultdict(lambda: [0, 0, 0, 0, 0])
-        for move in self.available:
-            i, j = self.move_to_location(move)
+        Returns:
+            - action: the most favorable action
+            - self.action_value[action][4]: Situation valuation under this action
+        """
+        self.action_value = defaultdict(lambda: [0, 0, 0, 0, 0])
+        for action in self.available:
+            i, j = self.action_to_location(action)
 
-            self.move_value[move][0] = self.scan_updown(i, j, player)
-            self.move_value[move][1] = self.scan_leftright(i, j, player)
-            self.move_value[move][2] = self.scan_left_updown(i, j, player)
-            self.move_value[move][3] = self.scan_right_updown(i, j, player)
+            self.action_value[action][0] = self.scan_updown(i, j, player)
+            self.action_value[action][1] = self.scan_leftright(i, j, player)
+            self.action_value[action][2] = self.scan_left_updown(i, j, player)
+            self.action_value[action][3] = self.scan_right_updown(i, j, player)
 
             # Indicates that one direction can already be rushed to 4
             for k in range(4):
-                if self.move_value[move][k] >= 390:
-                    self.move_value[move][k] = 2000
-                elif self.move_value[move][k] >= 302:
-                    self.move_value[move][k] = 1000
+                if self.action_value[action][k] >= 390:
+                    self.action_value[action][k] = 2000
+                elif self.action_value[action][k] >= 302:
+                    self.action_value[action][k] = 1000
 
             # Comprehensive score in all directions
-            self.move_value[move][4] = (self.move_value[move][0] +
-                                        self.move_value[move][1] +
-                                        self.move_value[move][2] +
-                                        self.move_value[move][3])
+            self.action_value[action][4] = (self.action_value[action][0] +
+                                        self.action_value[action][1] +
+                                        self.action_value[action][2] +
+                                        self.action_value[action][3])
 
-        move = max(self.available, key=lambda x: self.move_value[x][4])
+        action = max(self.available, key=lambda x: self.action_value[x][4])
 
-        return move, self.move_value[move][4]
+        return action, self.action_value[action][4]
 
-    def get_move(self, obs):
+    def get_action(self, obs):
+        """
+        Overview:
+            Returns a rule-based expert action.
+        Arguments:
+            - obs (:obj:`np.array`)
+
+        Returns:
+            - expert_action
+        """
         self.board = obs
 
-        if self.init_board_flag is False:
+        if self.init_board_flag == False:
             self.board_width = self.board['observation'][0].shape[0]
             self.board_height = self.board['observation'][0].shape[1]
             # the 2 dim indicates which player is the to_play player, 1 means player 1, 2 means player 2
-            if self.board['observation'][2][0][0] == 1:
-                self.m_player_id = 1
-                self.s_player_id = 2
-            else:
-                self.m_player_id = 2
-                self.s_player_id = 1
             self.init_board_flag = True
-
+        if self.board['observation'][2][0][0] == 1:
+            self.m_player_id = 1
+            self.s_player_id = 2
+        else:
+            self.m_player_id = 2
+            self.s_player_id = 1
         # transform observation,action_mask to self.available, self.board_status
         self.available = []
         self.board_status = np.zeros(self.board_width * self.board_height, 'int8')
         for i in range(self.board_width):
             for j in range(self.board_height):
-                move = self.location_to_move(i, j)
-                if self.board['action_mask'][move] == 1:
-                    self.available.append(move)
+                action = self.location_to_action(i, j)
+                if self.board['action_mask'][action] == 1:
+                    self.available.append(action)
                 if self.board['observation'][0][i][j] == 1:
-                    self.board_status[move] = self.m_player_id
+                    self.board_status[action] = self.m_player_id
                 elif self.board['observation'][1][i][j] == 1:
-                    self.board_status[move] = self.s_player_id
+                    self.board_status[action] = self.s_player_id
 
-        m_move, m_value = self.evaluate_all_value(self.m_player_id)
+        m_action, m_value = self.evaluate_all_value(self.m_player_id)
 
         logging.info("loaction:{loc},value:{value}".format(
-                     loc=self.move_to_location(m_move), value=m_value))
+                     loc=self.action_to_location(m_action), value=m_value))
 
-        s_move, s_value = self.evaluate_all_value(self.s_player_id)
+        s_action, s_value = self.evaluate_all_value(self.s_player_id)
         logging.info("O_loaction:{loc},value:{value}".format(
-                      loc=self.move_to_location(s_move), value=s_value))
+                      loc=self.action_to_location(s_action), value=s_value))
         # Block this position if it is better for the opponent to play here
         if m_value >= s_value:
-            return m_move
+            return m_action
         else:
-            return s_move
+            return s_action
--- dizoo/board_games/gomoku/envs/test_gomoku_expert_action.py
+++ dizoo/board_games/gomoku/envs/test_gomoku_expert_action.py
@@ -1,36 +1,37 @@
+import pytest
 from dizoo.board_games.gomoku.envs.gomoku_env import GomokuEnv
 
 
-def test_naive():
-    env = GomokuEnv()
-    obs = env.reset()
-    print('init board state: ', obs)
-    env.render()
-    done = False
-    while True:
-        # action = env.random_action()
-        action = env.human_to_action()
-        # print('original player 1: ', action)
-        print('player 1: ' + env.action_to_string(action))
-        obs, reward, done, info = env.step(action)
+@pytest.mark.envtest
+class TestExpertAction:
+    def test_naive():
+        env = GomokuEnv()
+        obs = env.reset()
+        print('init board state: ', obs)
         env.render()
-        if done:
-            if reward > 0:
-                print('player 1 (human player) win')
-            else:
-                print('draw')
-            break
+        done = False
+        while True:
+            # action = env.random_action()
+            action = env.human_to_action()
+            # action = env.expert_action()
+            print('original player 1: ',action)
+            print('player 1: ' + env.action_to_string(action))
+            obs, reward, done, info = env.step(action)
+            env.render()
+            if done:
+                if reward > 0:
+                    print('player 1 (human player) win')
+                else:
+                    print('draw')
+                break
 
-        action = env.expert_action()
-        print('player 2 (computer player): ' + env.action_to_string(action))
-        obs, reward, done, info = env.step(action)
-        env.render()
-        if done:
-            if reward > 0:
-                print('player 2 (computer player) win')
-            else:
-                print('draw')
-            break
-
-
-test_naive()
+            action = env.expert_action()
+            print('player 2 (computer player): ' + env.action_to_string(action))
+            obs, reward, done, info = env.step(action)
+            env.render()
+            if done:
+                if reward > 0:
+                    print('player 2 (computer player) win')
+                else:
+                    print('draw')
+                break
