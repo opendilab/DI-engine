ea1b53c27f0c245d402379a5e21cae2dc1e74b32
蒲源 <48008469+puyuan1996@users.noreply.github.com>
2022-10-07 13:17:40 +0800

    polish(pu): adapt minigrid_env and related config to latest MiniGrid v2.0.0 (#500)
    * polish(pu): adapt minigrid_env and config to latest MiniGrid v2.0.0
    
    * polish(pu): polish setup.py for minigrid_env
    
    * fix(pu): add importlib-metadata version requirement to be compatible with flake8

--- dizoo/minigrid/config/minigrid_icm_offppo_config.py
+++ dizoo/minigrid/config/minigrid_icm_offppo_config.py
@@ -6,7 +6,9 @@ minigrid_ppo_icm_config = dict(
         collector_env_num=8,
         evaluator_env_num=5,
         n_evaluator_episode=5,
-        # minigrid env id: 'MiniGrid-Empty-8x8-v0', 'MiniGrid-FourRooms-v0','MiniGrid-DoorKey-16x16-v0'
+        # typical MiniGrid env id:
+        # {'MiniGrid-Empty-8x8-v0', 'MiniGrid-FourRooms-v0', 'MiniGrid-DoorKey-8x8-v0','MiniGrid-DoorKey-16x16-v0'},
+        # please refer to https://github.com/Farama-Foundation/MiniGrid for details.
         env_id='MiniGrid-DoorKey-8x8-v0',
         max_step=300,
         stop_value=0.96,
@@ -21,7 +23,7 @@ minigrid_ppo_icm_config = dict(
     policy=dict(
         cuda=True,
         model=dict(
-            obs_shape=2739,
+            obs_shape=2835,
             action_shape=7,
             encoder_hidden_size_list=[256, 128, 64, 64],
         ),
