9e89c050f740c69da3dae532f48082c22c64be0a
puyuan1996 <2402552459@qq.com>
2022-10-08 20:45:20 +0800

    feature(pu): adapt gym_hybrid_env to be compatible with hardmove env

--- dizoo/gym_hybrid/envs/gym_hybrid_env.py
+++ dizoo/gym_hybrid/envs/gym_hybrid_env.py
@@ -1,19 +1,20 @@
-from typing import Any, List, Dict, Union, Optional
-import time
+import os
+from typing import Dict, Optional
+
 import gym
-import gym_hybrid
-import copy
+import matplotlib.pyplot as plt
 import numpy as np
-from easydict import EasyDict
 from ding.envs import BaseEnv, BaseEnvTimestep
 from ding.envs.common import affine_transform
 from ding.torch_utils import to_ndarray
 from ding.utils import ENV_REGISTRY
+from easydict import EasyDict
+from matplotlib import animation
 
 
 @ENV_REGISTRY.register('gym_hybrid')
 class GymHybridEnv(BaseEnv):
-    default_env_id = ['Sliding-v0', 'Moving-v0']
+    default_env_id = ['Sliding-v0', 'Moving-v0', 'HardMove-v0']
 
     def __init__(self, cfg: EasyDict) -> None:
         self._cfg = cfg
@@ -21,29 +22,22 @@ class GymHybridEnv(BaseEnv):
         assert self._env_id in self.default_env_id
         self._act_scale = cfg.act_scale
         self._init_flag = False
-        self._replay_path = None
+        self._save_replay = cfg.save_replay_gif
+        self._replay_path = cfg.replay_path
+        self._save_replay_count = 0
+        if self._save_replay:
+            self._frames = []
 
     def reset(self) -> np.ndarray:
         if not self._init_flag:
-            self._env = gym.make(self._env_id)
-            if self._replay_path is not None:
-                if gym.version.VERSION > '0.22.0':
-                    # Gym removed classic control rendering to support using pygame instead.
-                    # And thus, gym hybrid currently do not support rendering.
-                    self._env.metadata["render_modes"] = ["human", "rgb_array"]
-                else:
-                    self._env = gym.wrappers.RecordVideo(
-                        self._env,
-                        video_folder=self._replay_path,
-                        episode_trigger=lambda episode_id: True,
-                        name_prefix='rl-video-{}'.format(id(self))
-                    )
-                    self._env.metadata["render.modes"] = ["human", "rgb_array"]
-
+            if self._env_id == 'HardMove-v0':
+                self._env = gym.make(self._env_id, num_actuators=self._cfg.num_actuators)
+            else:
+                self._env = gym.make(self._env_id)
             self._observation_space = self._env.observation_space
             self._action_space = self._env.action_space
             self._reward_space = gym.spaces.Box(
-                low=self._env.reward_range[0], high=self._env.reward_range[1], shape=(1, ), dtype=np.float32
+                low=self._env.reward_range[0], high=self._env.reward_range[1], shape=(1,), dtype=np.float32
             )
             self._init_flag = True
         if hasattr(self, '_seed') and hasattr(self, '_dynamic_seed') and self._dynamic_seed:
@@ -68,29 +62,45 @@ class GymHybridEnv(BaseEnv):
 
     def step(self, action: Dict) -> BaseEnvTimestep:
         if self._act_scale:
-            # acceleration_value.
-            action['action_args'][0] = affine_transform(action['action_args'][0], min_val=0, max_val=1)
-            # rotation_value. Following line can be omitted, because in the affine_transform function,
-            # we have already done the clip(-1,1) operation
-            action['action_args'][1] = affine_transform(action['action_args'][1], min_val=-1, max_val=1)
-            action = [action['action_type'], action['action_args']]
+            if self._env_id == 'HardMove-v0':
+                action = [
+                    action['action_type'], [affine_transform(i, min_val=-1, max_val=1) for i in action['action_args']]
+                ]
+            else:
+                # acceleration_value.
+                action['action_args'][0] = affine_transform(action['action_args'][0], min_val=0, max_val=1)
+                # rotation_value. Following line can be omitted, because in the affine_transform function,
+                # we have already done the clip(-1,1) operation
+                action['action_args'][1] = affine_transform(action['action_args'][1], min_val=-1, max_val=1)
+                action = [action['action_type'], action['action_args']]
+        if self._save_replay:
+            self._frames.append(self._env.render(mode='rgb_array'))
         obs, rew, done, info = self._env.step(action)
         self._final_eval_reward += rew
         if done:
             info['final_eval_reward'] = self._final_eval_reward
+            if self._save_replay:
+                if self._env_id == 'HardMove-v0':
+                    self._env_id = f'hardmove_n{self._cfg.num_actuators}'
+                path = os.path.join(
+                    self._replay_path, '{}_episode_{}.gif'.format(self._env_id, self._save_replay_count)
+                )
+                self.display_frames_as_gif(self._frames, path)
+                self._save_replay_count += 1
+
         obs = to_ndarray(obs)
         if isinstance(obs, list):  # corner case
             for i in range(len(obs)):
                 if len(obs[i].shape) == 0:
                     obs[i] = np.array([obs[i]])
             obs = np.concatenate(obs)
-        assert isinstance(obs, np.ndarray) and obs.shape == (10, )
+        assert isinstance(obs, np.ndarray) and obs.shape == (10,)
         obs = obs.astype(np.float32)
 
-        rew = to_ndarray([rew])  # wrapped to be transfered to a numpy array with shape (1,)
+        rew = to_ndarray([rew])  # wrapped to be transferred to a numpy array with shape (1,)
         if isinstance(rew, list):
             rew = rew[0]
-        assert isinstance(rew, np.ndarray) and rew.shape == (1, )
+        assert isinstance(rew, np.ndarray) and rew.shape == (1,)
         info['action_args_mask'] = np.array([[1, 0], [0, 1], [0, 0]])
         return BaseEnvTimestep(obs, rew, done, info)
 
@@ -121,3 +131,21 @@ class GymHybridEnv(BaseEnv):
     @property
     def reward_space(self) -> gym.spaces.Space:
         return self._reward_space
+
+    def enable_save_replay(self, replay_path: Optional[str] = None) -> None:
+        if replay_path is None:
+            replay_path = './video'
+        self._save_replay = True
+        self._replay_path = replay_path
+        self._save_replay_count = 0
+
+    @staticmethod
+    def display_frames_as_gif(frames: list, path: str) -> None:
+        patch = plt.imshow(frames[0])
+        plt.axis('off')
+
+        def animate(i):
+            patch.set_data(frames[i])
+
+        anim = animation.FuncAnimation(plt.gcf(), animate, frames=len(frames), interval=5)
+        anim.save(path, writer='imagemagick', fps=20)
