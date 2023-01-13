c5119f544ad96086dee09ab329fbfc8cefb3fc64
SolenoidWGT <877825076@qq.com>
2023-01-13 05:33:36 +0000

    change port for nng perf

--- ding/framework/message_queue/perfs/tests/test_perf_nng.py
+++ ding/framework/message_queue/perfs/tests/test_perf_nng.py
@@ -7,7 +7,7 @@ import pytest
 # @pytest.mark.multiprocesstest
 def test_nng():
     params = [
-        ("12376", None, "127.0.0.1", "learner", "0"), ("12378", "tcp://127.0.0.1:12376", "127.0.0.1", "collector", "1")
+        ("12960", None, "127.0.0.1", "learner", "0"), ("12961", "tcp://127.0.0.1:12960", "127.0.0.1", "collector", "1")
     ]
     ctx = mp.get_context("spawn")
     with ctx.Pool(processes=2) as pool:
