bd46c7d0b63e7172004e7015a0f80b03896a2128
niuyazhe <niuyazhe@sensetime.com>
2023-02-14 11:11:53 +0800

    style(nyz): extend treetensor lowest version(ci skip)

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
         'pynng',
         'redis',
         'pettingzoo==1.12.0',
-        'DI-treetensor>=0.4.0',
+        'DI-treetensor>=0.3.0',
         'DI-toolkit>=0.0.2',
         'hbutils>=0.5.0',
         'wandb',
