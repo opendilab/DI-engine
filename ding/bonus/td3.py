0257ae9367b19a98c5c3feba5e6b3fb6d585f680
zjowowen <zjowowen@outlook.com>
2023-02-28 20:50:28 +0800

    polish code

--- ding/bonus/td3.py
+++ ding/bonus/td3.py
@@ -37,7 +37,7 @@ class TD3:
             self.env = get_instance_env(env)
             if cfg is None:
                 # 'It should be default env tuned config'
-                self.cfg = get_instance_config(env)
+                self.cfg = get_instance_config(env, algorithm='td3')
             else:
                 self.cfg = cfg
         elif isinstance(env, BaseEnv):
