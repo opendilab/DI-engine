4b7aa509f4dbf0054b1e47500a763218178067f6
Ruoyu Gao <ruoyu.gao@nyu.edu>
2023-05-15 01:59:57 -0700

    add pong spaceinvaders and qbert for dqn

--- ding/bonus/config.py
+++ ding/bonus/config.py
@@ -1181,6 +1181,157 @@ def get_instance_config(env: str, algorithm: str) -> EasyDict:
                     ),
                 )
             )
+        elif env == 'Pong':
+            cfg.update(
+                dict(
+                    exp_name='Pong-v4-DQN',
+                    seed=0,
+                    env=dict(
+                        env_id='Pong-v4',
+                        collector_env_num=8,
+                        evaluator_env_num=8,
+                        n_evaluator_episode=8,
+                        stop_value=20,
+                        fram_stack=4,
+                    ),
+                    policy=dict(
+                        cuda=True,
+                        priority=False,
+                        discount_factor=0.99,
+                        nstep=3,
+                        learn=dict(
+                            update_per_collect=10,
+                            batch_size=32,
+                            learning_rate=0.0001,
+                            # Frequency of target network update.
+                            target_update_freq=500,
+                        ),
+                        model=dict(
+                            obs_shape=[4, 84, 84],
+                            action_shape=6,
+                            encoder_hidden_size_list=[128, 128, 512],
+                        ),
+                        collect=dict(n_sample=96, ),
+                        other=dict(
+                            eps=dict(
+                                type='exp',
+                                start=1.,
+                                end=0.05,
+                                decay=250000,
+                            ),
+                            replay_buffer=dict(replay_buffer_size=100000, )
+                        ),
+                    ),
+                    wandb_logger=dict(
+                        gradient_logger=True,
+                        video_logger=True,
+                        plot_logger=True,
+                        action_logger=True,
+                        return_logger=False
+                    ),
+                )
+            )
+        elif env == 'SpaceInvaders':
+            cfg.update(
+                dict(
+                    exp_name='SpaceInvaders-v4-DQN',
+                    seed=0,
+                    env=dict(
+                        env_id='SpaceInvadersNoFrameskip-v4',
+                        collector_env_num=8,
+                        evaluator_env_num=8,
+                        n_evaluator_episode=8,
+                        fram_stack=4,
+                        stop_value=2000,
+                    ),
+                    policy=dict(
+                        cuda=True,
+                        priority=False,
+                        discount_factor=0.99,
+                        nstep=3,
+                        learn=dict(
+                            update_per_collect=10,
+                            batch_size=32,
+                            learning_rate=0.0001,
+                            # Frequency of target network update.
+                            target_update_freq=500,
+                            hook=dict(save_ckpt_after_iter=1000000, )
+                        ),
+                        model=dict(
+                            obs_shape=[4, 84, 84],
+                            action_shape=6,
+                            encoder_hidden_size_list=[128, 128, 512],
+                        ),
+                        collect=dict(n_sample=100, ),
+                        other=dict(
+                            eps=dict(
+                                type='exp',
+                                start=1.,
+                                end=0.05,
+                                decay=1000000,
+                            ),
+                            replay_buffer=dict(replay_buffer_size=400000, )
+                        ),
+                    ),
+                    wandb_logger=dict(
+                        gradient_logger=True,
+                        video_logger=True,
+                        plot_logger=True,
+                        action_logger=True,
+                        return_logger=False
+                    ),
+                )
+            )
+        elif env == 'Qbert':
+            cfg.update(
+                dict(
+                    exp_name='Qbert-v4-DQN',
+                    seed=0,
+                    env=dict(
+                        env_id='Qbert-v4',
+                        collector_env_num=8,
+                        evaluator_env_num=8,
+                        n_evaluator_episode=8,
+                        fram_stack=4,
+                        stop_value=30000,
+                    ),
+                    policy=dict(
+                        cuda=True,
+                        priority=False,
+                        discount_factor=0.99,
+                        nstep=3,
+                        learn=dict(
+                            update_per_collect=10,
+                            batch_size=32,
+                            learning_rate=0.0001,
+                            # Frequency of target network update.
+                            target_update_freq=500,
+                        ),
+                        model=dict(
+                            obs_shape=[4, 84, 84],
+                            action_shape=6,
+                            encoder_hidden_size_list=[128, 128, 512],
+                        ),
+                        collect=dict(n_sample=100, ),
+                        other=dict(
+                            eps=dict(
+                                type='exp',
+                                start=1.,
+                                end=0.05,
+                                decay=1000000,
+                            ),
+                            replay_buffer=dict(replay_buffer_size=400000, )
+                        ),
+                    ),
+                    wandb_logger=dict(
+                        gradient_logger=True,
+                        video_logger=True,
+                        plot_logger=True,
+                        action_logger=True,
+                        return_logger=False
+                    ),
+                )
+            )
         else:
             raise KeyError("not supported env type: {}".format(env))
     elif algorithm == 'C51':
