308e25a606fb13ba0250ee9feb0ee4e81316411c
zjowowen <zjowowen@outlook.com>
2023-05-24 10:17:41 +0000

    fix a2c deploy bug

--- ding/bonus/a2c.py
+++ ding/bonus/a2c.py
@@ -125,7 +125,12 @@ class A2CAgent:
 
         def single_env_forward_wrapper(forward_fn, cuda=True):
 
-            forward_fn = model_wrap(forward_fn, wrapper_name='argmax_sample').forward
+            if self.cfg.policy.action_space == 'continuous':
+                forward_fn = model_wrap(forward_fn, wrapper_name='deterministic_sample').forward
+            elif self.cfg.policy.action_space == 'discrete':
+                forward_fn = model_wrap(forward_fn, wrapper_name='argmax_sample').forward
+            else:
+                raise NotImplementedError
 
             def _forward(obs):
                 # unsqueeze means add batch dim, i.e. (O, ) -> (1, O)
