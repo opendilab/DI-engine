603fa5ec30897449546103439ee9546a6ded0334
zhangpaipai <1124321458@qq.com>
2023-06-13 11:42:17 +0800

    polish pg pendulum+hopper config

--- ding/bonus/config.py
+++ ding/bonus/config.py
@@ -445,15 +445,15 @@ def get_instance_config(env: str, algorithm: str) -> EasyDict:
                         ),
                         learn=dict(
                             batch_size=64,
-                            learning_rate=0.001,
-                            entropy_weight=0.001,
+                            learning_rate=0.005,
+                            entropy_weight=0.01,
                         ),
                         collect=dict(
-                            n_episode=8,
+                            n_episode=34,
                             unroll_len=1,
                             discount_factor=0.99,
                         ),
-                        eval=dict(evaluator=dict(eval_freq=200, ))
+                        eval=dict(evaluator=dict(eval_freq=1, ))
                     ),
                     wandb_logger=dict(
                         gradient_logger=True,
@@ -492,11 +492,11 @@ def get_instance_config(env: str, algorithm: str) -> EasyDict:
                             entropy_weight=0.001,
                         ),
                         collect=dict(
-                            n_episode=8,
+                            n_episode=20,
                             unroll_len=1,
                             discount_factor=0.99,
                         ),
-                        eval=dict(evaluator=dict(eval_freq=200, ))
+                        eval=dict(evaluator=dict(eval_freq=1, ))
                     ),
                     wandb_logger=dict(
                         gradient_logger=True,
@@ -535,11 +535,11 @@ def get_instance_config(env: str, algorithm: str) -> EasyDict:
                             entropy_weight=0.001,
                         ),
                         collect=dict(
-                            n_episode=8,
+                            n_episode=20,
                             unroll_len=1,
                             discount_factor=0.99,
                         ),
-                        eval=dict(evaluator=dict(eval_freq=200, ))
+                        eval=dict(evaluator=dict(eval_freq=1, ))
                     ),
                     wandb_logger=dict(
                         gradient_logger=True,
@@ -616,11 +616,11 @@ def get_instance_config(env: str, algorithm: str) -> EasyDict:
                             entropy_weight=0.001,
                         ),
                         collect=dict(
-                            n_episode=8,
+                            n_episode=20,
                             unroll_len=1,
                             discount_factor=0.99,
                         ),
-                        eval=dict(evaluator=dict(eval_freq=200, ))
+                        eval=dict(evaluator=dict(eval_freq=1, ))
                     ),
                     wandb_logger=dict(
                         gradient_logger=True,
@@ -652,16 +652,16 @@ def get_instance_config(env: str, algorithm: str) -> EasyDict:
                             action_shape=1,
                         ),
                         learn=dict(
-                            batch_size=800,
+                            batch_size=4000,
                             learning_rate=0.001,
                             entropy_weight=0.001,
                         ),
                         collect=dict(
-                            n_episode=4,
+                            n_episode=20,
                             unroll_len=1,
                             discount_factor=0.99,
                         ),
-                        eval=dict(evaluator=dict(eval_freq=200, ))
+                        eval=dict(evaluator=dict(eval_freq=1, ))
                     ),
                     wandb_logger=dict(
                         gradient_logger=True,
