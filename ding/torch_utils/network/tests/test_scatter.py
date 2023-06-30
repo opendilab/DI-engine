6fffa6e168cf863b635c7e1e0aa755418f864b95
jayyoung0802 <jayyoung1996@163.com>
2023-06-30 13:32:08 +0800

    polish(yzj): polish scatter_connection's pytest

--- ding/torch_utils/network/tests/test_scatter.py
+++ ding/torch_utils/network/tests/test_scatter.py
@@ -34,13 +34,12 @@ class TestScatterConnection:
         for scatter_type in ['add', 'cover']:
             model = ScatterConnection(scatter_type)
             B, M, N = 10, 20, 3
-            SpatialSize = (13, 17)
-            H, W = 13, 17
+            spatial_size = (13, 17)
             input = torch.randn(size=(B, M, N)).requires_grad_(True)
             coord_x = torch.randint(low=0, high=13, size=(B, M))
             coord_y = torch.randint(low=0, high=17, size=(B, M))
-            output = model.xy_forward(input, SpatialSize, coord_x, coord_y)
+            output = model.xy_forward(input, spatial_size, coord_x, coord_y)
             loss = output.mean()
             loss.backward()
             assert isinstance(input.grad, torch.Tensor)
-            assert output.shape == (B, N, H, W)
+            assert output.shape == (B, N, *spatial_size)
