b188e292c6795fce270542397f2fc56749b114cd
jayyoung0802 <42484691+jayyoung0802@users.noreply.github.com>
2023-06-30 17:13:40 +0800

    feature(yzj): add scatter connection model (#680)
    * feature(yzj): add scatter connection model
    
    * polish(yzj): polish scatter_connection and scatter_connection's pytest
    
    * polish(yzj): polish scatter_connection's pytest

--- ding/torch_utils/network/tests/test_scatter.py
+++ ding/torch_utils/network/tests/test_scatter.py
@@ -29,3 +29,17 @@ class TestScatterConnection:
             loss = output.mean()
             loss.backward()
             assert isinstance(input.grad, torch.Tensor)
+
+    def test_xy_forward(self):
+        for scatter_type in ['add', 'cover']:
+            model = ScatterConnection(scatter_type)
+            B, M, N = 10, 20, 3
+            spatial_size = (13, 17)
+            input = torch.randn(size=(B, M, N)).requires_grad_(True)
+            coord_x = torch.randint(low=0, high=13, size=(B, M))
+            coord_y = torch.randint(low=0, high=17, size=(B, M))
+            output = model.xy_forward(input, spatial_size, coord_x, coord_y)
+            loss = output.mean()
+            loss.backward()
+            assert isinstance(input.grad, torch.Tensor)
+            assert output.shape == (B, N, *spatial_size)
