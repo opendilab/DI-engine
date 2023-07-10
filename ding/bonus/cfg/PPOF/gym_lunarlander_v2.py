d9d93ddd6961ef2e4a3cabe62b573a7a52c49440
zjowowen <zjowowen@outlook.com>
2023-07-10 16:24:45 +0000

    polish code

--- ding/bonus/cfg/PPOF/gym_lunarlander_v2.py
+++ ding/bonus/cfg/PPOF/gym_lunarlander_v2.py
@@ -2,6 +2,7 @@ from easydict import EasyDict
 
 cfg = dict(
     exp_name='LunarLander-v2-PPO',
+    env_id='LunarLander-v2',
     n_sample=400,
     value_norm='popart',
 )
--- ding/bonus/cfg/PPOF/gym_lunarlandercontinuous_v2.py
+++ ding/bonus/cfg/PPOF/gym_lunarlandercontinuous_v2.py
@@ -2,6 +2,7 @@ from easydict import EasyDict
 
 cfg = dict(
     exp_name='LunarLanderContinuous-V2-PPO',
+    env_id='LunarLanderContinuous-v2',
     action_space='continuous',
     n_sample=400,
     act_scale=True,
