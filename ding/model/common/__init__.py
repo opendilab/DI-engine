d24b8f918556439a44edf401a03ade65df6ddee3
niuyazhe <niuyazhe@sensetime.com>
2023-09-09 21:49:05 +0800

    style(nyz): polish model common modules comments

--- ding/model/common/__init__.py
+++ ding/model/common/__init__.py
@@ -1,4 +1,4 @@
-from .head import DiscreteHead, DuelingHead, DistributionHead, RainbowHead, QRDQNHead, \
+from .head import DiscreteHead, DuelingHead, DistributionHead, RainbowHead, QRDQNHead, StochasticDuelingHead, \
     QuantileHead, FQFHead, RegressionHead, ReparameterizationHead, MultiHead, BranchingHead, head_cls_map, \
     independent_normal_dist, AttentionPolicyHead, PopArtVHead, EnsembleHead
 from .encoder import ConvEncoder, FCEncoder, IMPALAConvEncoder
