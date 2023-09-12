a28c3f0919d722f0104f6c582180bc27ec253e1d
zjowowen <zjowowen@outlook.com>
2023-09-12 08:03:22 +0000

    add unittest for montecarlo_return_estimator

--- ding/framework/middleware/tests/test_advantage_estimator.py
+++ ding/framework/middleware/tests/test_advantage_estimator.py
@@ -9,6 +9,7 @@ import torch
 import copy
 
 from ding.framework.middleware.functional.advantage_estimator import gae_estimator
+from ding.framework.middleware.functional.advantage_estimator import montecarlo_return_estimator
 from ding.utils.data import ttorch_collate
 
 from typing import Any, List, Dict, Optional
@@ -99,3 +100,61 @@ def test_gae_estimator():
     trajectory_end_idx_size = 5
     call_gae_estimator(batch_size, trajectory_end_idx_size)
     call_gae_estimator(batch_size, trajectory_end_idx_size, DequeBuffer(size=batch_size))
+
+
+class MockPGPolicy(Mock):
+
+    def __init__(self, cfg) -> None:
+        super(MockPGPolicy, self).__init__()
+        self._cfg = EasyDict(cfg)
+        self._gamma = self._cfg.collect.discount_factor
+        self._unroll_len = self._cfg.collect.unroll_len
+
+    def get_attribute(self, name: str) -> Any:
+        return self._model
+
+
+def call_montecarlo_return_estimator(batch_size: int = 32):
+
+    cfg = dict(
+        learn=dict(ignore_done=False, ),
+        collect=dict(
+            unroll_len=1,
+            discount_factor=0.9,
+        ),
+    )
+    ctx = OnlineRLContext()
+    ctx.episodes = [
+        [
+            treetensor.torch.Tensor(
+                {
+                    'action': treetensor.torch.randint(low=0, high=2, size=(1, )),
+                    'collect_train_iter': [0],
+                    'done': False if i != batch_size - 1 else True,
+                    'logit': treetensor.torch.randn(2),
+                    'next_obs': treetensor.torch.randn(4),
+                    'obs': treetensor.torch.randn(4),
+                    'reward': [1.0],
+                    'value': torch.distributions.uniform.Uniform(0, 4).sample([1])
+                }
+            ) for i in range(batch_size)
+        ]
+    ]
+    ctx.episodes_copy = treetensor.torch.concat(
+        [ttorch_collate(copy.deepcopy(episode), cat_1dim=True) for episode in ctx.episodes], dim=0
+    )
+    with patch("ding.policy.Policy", MockPGPolicy):
+        montecarlo_return_estimator(MockPGPolicy(cfg))(ctx)
+
+    assert torch.equal(ctx.episodes_copy.action, ctx.train_data.action)
+    assert torch.equal(ctx.episodes_copy.collect_train_iter, ctx.train_data.collect_train_iter)
+    assert torch.equal(ctx.episodes_copy.logit, ctx.train_data.logit)
+    assert torch.equal(ctx.episodes_copy.next_obs, ctx.train_data.next_obs)
+    assert torch.equal(ctx.episodes_copy.obs, ctx.train_data.obs)
+    assert torch.equal(ctx.episodes_copy.reward, ctx.train_data.reward)
+
+
+@pytest.mark.unittest
+def test_montecarlo_return_estimator():
+    batch_size = 32
+    call_montecarlo_return_estimator(batch_size)
