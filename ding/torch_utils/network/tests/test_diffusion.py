59dce40f5f406541bd8193e1038947348f8362d5
Super1ce <278042904@qq.com>
2023-09-22 15:37:43 +0800

    add unittest

---
+++ ding/torch_utils/network/tests/test_diffusion.py
@@ -0,0 +1,25 @@
+import pytest
+import torch
+from ding.torch_utils.network.diffusion import DiffusionUNet1d, TemporalValue
+
+batch_size = 2
+transition_dim = 10
+dim = 8
+dim_mults = [1, 2, 4]
+horizon = 4
+
+@pytest.mark.unittest
+class TestDiffusionNet:
+    def test_DiffusionNet1d(self):
+        diffusion = DiffusionUNet1d(transition_dim, dim, dim_mults)
+        input = torch.rand(batch_size, horizon, transition_dim)
+        t = torch.randint(0, 10, (batch_size,)).long()
+        output = diffusion(input, time=t)
+        assert output.shape == (batch_size, horizon, transition_dim)
+
+    def test_TemporalValue(self):
+        value = TemporalValue(horizon, transition_dim, dim, dim_mults=dim_mults)
+        input = torch.rand(batch_size, horizon, transition_dim)
+        t = torch.randint(0, 10, (batch_size,)).long()
+        output = value(input, time=t)
+        assert output.shape == (batch_size, 1)
--- ding/utils/tests/test_normalizer_helper.py
+++ ding/utils/tests/test_normalizer_helper.py
@@ -10,10 +10,27 @@ class TestNormalizerHelper:
         x = numpy.random.randn(10)
         mean = x.mean()
         std = x.std()
+        mins = x.min()
+        maxs = x.max()
         normalizer = DatasetNormalizer({'test': x}, 'GaussianNormalizer', 10)
         test = numpy.random.randn(1)
         normal_test = normalizer.normalize(test, 'test')
-        unnormal_test =normalizer.unnormalize(test, 'test')
+        unnormal_test = normalizer.unnormalize(normal_test, 'test')
         assert unnormal_test == test
         assert normal_test == (test - mean) / std
+
+        normalizer = DatasetNormalizer({'test': x}, 'LimitsNormalizer', 10)
+        test = numpy.random.randn(1)
+        normal_test1 = (test - mins) / (maxs - mins)
+        normal_test1 = 2 * normal_test1 - 1
+        normal_test = normalizer.normalize(test, 'test')
+        unnormal_test = normalizer.unnormalize(normal_test, 'test')
+        assert unnormal_test == test
+        assert normal_test == normal_test1
+
+        normalizer = DatasetNormalizer({'test': x}, 'CDFNormalizer', 10)
+        test = numpy.random.randn(1)
+        normal_test = normalizer.normalize(test, 'test')
+        unnormal_test = normalizer.unnormalize(normal_test, 'test')
+        assert unnormal_test == test
         
