532b5b8af925138c4539f16d7e942680deab2684
zjowowen <zjowowen@outlook.com>
2023-10-12 20:12:25 +0800

    add large batch

--- dizoo/atari/config/serial/pong/__init__.py
+++ dizoo/atari/config/serial/pong/__init__.py
@@ -1,4 +1,5 @@
 from .pong_dqn_config import pong_dqn_config, pong_dqn_create_config
 from .pong_dqn_envpool_config import pong_dqn_envpool_config, pong_dqn_envpool_create_config
 from .pong_dqn_envpool_shink_model_config import pong_dqn_envpool_shink_model_config
+from .pong_dqn_envpool_large_batch_config import pong_dqn_envpool_large_batch_config
 from .pong_dqfd_config import pong_dqfd_config, pong_dqfd_create_config
