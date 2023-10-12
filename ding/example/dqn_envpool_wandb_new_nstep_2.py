ab330011c36fa15230c54e8efdae54f47b3cfb42
zjowowen <zjowowen@outlook.com>
2023-10-12 11:59:08 +0800

    polish code

--- ding/example/dqn_envpool_wandb_new_nstep_2.py
+++ ding/example/dqn_envpool_wandb_new_nstep_2.py
@@ -15,7 +15,7 @@ from ding.framework import task, ding_init
 from ding.framework.context import OnlineRLContext
 from ding.framework.middleware import OffPolicyLearner, envpool_evaluator, data_pusher, \
     eps_greedy_handler, CkptSaver, ContextExchanger, ModelExchanger, online_logger, nstep_reward_enhancer, \
-    termination_checker, wandb_online_logger, epoch_timer, EnvpoolStepCollectorV3, OffPolicyLearnerV2
+    termination_checker, wandb_online_logger, epoch_timer, EnvpoolStepCollectorV2, OffPolicyLearnerV2, OffPolicyLearnerV3
 from ding.utils import set_pkg_seed
 
 from dizoo.atari.config.serial import pong_dqn_envpool_config
@@ -85,7 +85,7 @@ def main(cfg):
         task.use(envpool_evaluator(cfg, policy.eval_mode, evaluator_env))
         task.use(eps_greedy_handler(cfg))
         task.use(
-            EnvpoolStepCollectorV3(
+            EnvpoolStepCollectorV2(
                 cfg,
                 policy.collect_mode,
                 collector_env,
@@ -95,7 +95,7 @@ def main(cfg):
                 )
         task.use(data_pusher(cfg, buffer_))
         #task.use(OffPolicyLearner(cfg, policy.learn_mode, buffer_))
-        task.use(OffPolicyLearnerV2(cfg, policy, buffer_))
+        task.use(OffPolicyLearnerV3(cfg, policy, buffer_))
         task.use(online_logger(train_show_freq=10))
         task.use(
             wandb_online_logger(
