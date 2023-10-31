1e936aae698dee823632b3ac0237e07fb257e49c
‘whl’ <18231213@buaa.edu.cn>
2023-10-31 11:14:14 +0800

    init commit

--- ding/utils/compression_helper.py
+++ ding/utils/compression_helper.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, ByteString, Callable
 import pickle
 import cloudpickle
 import zlib
@@ -8,10 +8,18 @@ import numpy as np
 class CloudPickleWrapper:
     """
     Overview:
-        CloudPickleWrapper can be able to pickle more python object(e.g: an object with lambda expression)
+        CloudPickleWrapper can be able to pickle more python object(e.g: an object with lambda expression).
+    Interfaces:
+        __init__.
     """
 
     def __init__(self, data: Any) -> None:
+        """
+        Overview:
+            Initialize the CloudPickleWrapper using the given arguments.
+        Arguments:
+            - data (:obj:`Any`): The object to be dumped.
+        """
         self.data = data
 
     def __getstate__(self) -> bytes:
@@ -24,18 +32,26 @@ class CloudPickleWrapper:
             self.data = cloudpickle.loads(data)
 
 
-def dummy_compressor(data):
-    r"""
+def dummy_compressor(data: Any) -> Any:
+    """
     Overview:
-        Return input data.
+        Return the raw input data.
+    Arguments:
+        - data (:obj:`Any`): The input data of the compressor.
+    Returns:
+        - output (:obj:`Any`): This compressor will exactly return the input data.
     """
     return data
 
 
-def zlib_data_compressor(data):
-    r"""
+def zlib_data_compressor(data: Any) -> bytes:
+    """
     Overview:
         Takes the input compressed data and return the compressed original data (zlib compressor) in binary format.
+    Arguments:
+        - data (:obj:`Any`): The input data of the compressor.
+    Returns:
+        - output (:obj:`bytes`): The compressed byte-like result.
     Examples:
         >>> zlib_data_compressor("Hello")
         b'x\x9ck`\x99\xca\xc9\x00\x01=\xac\x1e\xa999\xf9S\xf4\x00%L\x04j'
@@ -43,10 +59,14 @@ def zlib_data_compressor(data):
     return zlib.compress(pickle.dumps(data))
 
 
-def lz4_data_compressor(data):
+def lz4_data_compressor(data: Any) -> bytes:
     r"""
     Overview:
         Return the compressed original data (lz4 compressor).The compressor outputs in binary format.
+    Arguments:
+        - data (:obj:`Any`): The input data of the compressor.
+    Returns:
+        - output (:obj:`bytes`): The compressed byte-like result.
     Examples:
         >>> lz4.block.compress(pickle.dumps("Hello"))
         b'\x14\x00\x00\x00R\x80\x04\x95\t\x00\x01\x00\x90\x8c\x05Hello\x94.'
@@ -61,14 +81,15 @@ def lz4_data_compressor(data):
     return lz4.block.compress(pickle.dumps(data))
 
 
-def jpeg_data_compressor(data):
+def jpeg_data_compressor(data: np.ndarray) -> bytes:
     """
     Overview:
-        To reduce memory usage, we can choose to store the jpeg strings of image
-        instead of the numpy array in the buffer.
-        This function encodes the observation numpy arr to the jpeg strings.
+        To reduce memory usage, we can choose to store the jpeg strings of image instead of the numpy array in \
+        the buffer. This function encodes the observation numpy arr to the jpeg strings.
     Arguments:
         data (:obj:`np.array`): the observation numpy arr.
+    Returns:
+        - img_str (:obj:`bytes`): The compressed byte-like result.
     """
     try:
         import cv2
@@ -91,13 +112,13 @@ _COMPRESSORS_MAP = {
 
 
 def get_data_compressor(name: str):
-    r"""
+    """
     Overview:
         Get the data compressor according to the input name
     Arguments:
         - name(:obj:`str`): Name of the compressor, support ``['lz4', 'zlib', 'jpeg', 'none']``
     Return:
-        - (:obj:`Callable`): Corresponding data_compressor, taking input data returning compressed data.
+        - compressor (:obj:`Callable`): Corresponding data_compressor, taking input data returning compressed data.
     Example:
         >>> compress_fn = get_data_compressor('lz4')
         >>> compressed_data = compressed(input_data)
@@ -105,18 +126,26 @@ def get_data_compressor(name: str):
     return _COMPRESSORS_MAP[name]
 
 
-def dummy_decompressor(data):
+def dummy_decompressor(data: Any) -> Any:
     """
     Overview:
-        Return input data.
+        Return the input data.
+    Arguments:
+        - data (:obj:`Any`): The input data of the decompressor.
+    Returns:
+        - output (:obj:`bytes`): The decompressed result, which is exactly the input.
     """
     return data
 
 
-def lz4_data_decompressor(compressed_data):
-    r"""
+def lz4_data_decompressor(compressed_data: bytes) -> Any:
+    """
     Overview:
         Return the decompressed original data (lz4 compressor).
+    Arguments:
+        - data (:obj:`bytes`): The input data of the decompressor.
+    Returns:
+        - output (:obj:`Any`): The decompressed object.
     """
     try:
         import lz4.block
@@ -128,24 +157,29 @@ def lz4_data_decompressor(compressed_data):
     return pickle.loads(lz4.block.decompress(compressed_data))
 
 
-def zlib_data_decompressor(compressed_data):
-    r"""
+def zlib_data_decompressor(compressed_data: bytes) -> Any:
+    """
     Overview:
         Return the decompressed original data (zlib compressor).
+    Arguments:
+        - data (:obj:`bytes`): The input data of the decompressor.
+    Returns:
+        - output (:obj:`Any`): The decompressed object.
     """
     return pickle.loads(zlib.decompress(compressed_data))
 
 
-def jpeg_data_decompressor(compressed_data, gray_scale=False):
+def jpeg_data_decompressor(compressed_data: bytes, gray_scale=False) -> np.ndarray:
     """
     Overview:
-        To reduce memory usage, we can choose to store the jpeg strings of image
-        instead of the numpy array in the buffer.
-        This function decodes the observation numpy arr from the jpeg strings.
+        To reduce memory usage, we can choose to store the jpeg strings of image instead of the numpy array in the \
+        buffer. This function decodes the observation numpy arr from the jpeg strings.
     Arguments:
-        compressed_data (:obj:`str`): the jpeg strings.
-        gray_scale (:obj:`bool`): if the observation is gray, ``gray_scale=True``,
+        - compressed_data (:obj:`bytes`): The jpeg strings.
+        - gray_scale (:obj:`bool`): If the observation is gray, ``gray_scale=True``,
             if the observation is RGB, ``gray_scale=False``.
+    Returns:
+        - arr (:obj:`np.ndarray`): The decompressed numpy array.
     """
     try:
         import cv2
@@ -172,8 +206,8 @@ _DECOMPRESSORS_MAP = {
 }
 
 
-def get_data_decompressor(name: str):
-    r"""
+def get_data_decompressor(name: str) -> Callable:
+    """
     Overview:
         Get the data decompressor according to the input name
     Arguments:
@@ -184,7 +218,7 @@ def get_data_decompressor(name: str):
         For all the decompressors, the input of a bytes-like object is required.
 
     Returns:
-        - (:obj:`Callable`): Corresponding data_decompressor.
+        - decompressor (:obj:`Callable`): Corresponding data decompressor.
     Examples:
         >>> decompress_fn = get_data_decompressor('lz4')
         >>> origin_data = compressed(compressed_data)
