f6c9cb9f161f5352617c39931cd5c0f16a34b34d
zjowowen <zjowowen@outlook.com>
2023-12-14 17:22:52 +0800

    polish code

--- ding/bonus/dqn.py
+++ ding/bonus/dqn.py
@@ -57,9 +57,10 @@ class DQNAgent:
             - seed (:obj:`int`): The random seed, which is set before running the program. \
                 Default to 0.
             - exp_name (:obj:`str`): The name of this experiment, which will be used to create the folder to save \
-                log data. Default to None. If not specified, the folder name will be ``env_id``-``algorithm``. 
+                log data. Default to None. If not specified, the folder name will be ``env_id``-``algorithm``.
             - model (:obj:`torch.nn.Module`): The model of DQN algorithm, which should be an instance of class \
-                :class:`ding.model.DQN`. If not specified, a default model will be generated according to the configuration.
+                :class:`ding.model.DQN`. \
+                If not specified, a default model will be generated according to the configuration.
             - cfg (:obj:Union[EasyDict, dict]): The configuration of DQN algorithm, which is a dict. \
                 Default to None. If not specified, the default configuration will be used. \
                 The default configuration can be found in ``ding/config/example/DQN/gym_lunarlander_v2.py``.
@@ -68,14 +69,14 @@ class DQNAgent:
 
         .. note::
             An RL Agent Instance can be initialized in two basic ways. \
-            For example, we have an environment with id ``LunarLander-v2`` registered in gym, and we want to train an agent \
-            with DQN algorithm with default configuration. Then we can initialize the agent in the following ways: 
-                ``agent = DQNAgent(env_id='LunarLander-v2')`` 
+            For example, we have an environment with id ``LunarLander-v2`` registered in gym, \
+            and we want to train an agent with DQN algorithm with default configuration. \
+            Then we can initialize the agent in the following ways:
+                ``agent = DQNAgent(env_id='LunarLander-v2')``
             or, if we want can specify the env_id in the configuration:
                 ``cfg = {'env': {'env_id': 'LunarLander-v2'}, 'policy': ...... }``
                 ``agent = DQNAgent(cfg=cfg)``
-            
-            There are also other arguments to specify the agent when initializing. 
+            There are also other arguments to specify the agent when initializing.
             For example, if we want to specify the environment instance:
                 ``env = CustomizedEnv('LunarLander-v2')``
                 ``agent = DQNAgent(cfg=cfg, env=env)``
@@ -154,7 +155,8 @@ class DQNAgent:
                 If not specified, it will be set according to the configuration.
             - evaluator_env_num (:obj:`int`): The evaluator environment number. Default to None. \
                 If not specified, it will be set according to the configuration.
-            - n_iter_save_ckpt (:obj:`int`): The frequency of saving checkpoint every training iteration. Default to 1000.
+            - n_iter_save_ckpt (:obj:`int`): The frequency of saving checkpoint every training iteration. \
+                Default to 1000.
             - context (:obj:`str`): The context of the environment manager. Default to None.
             - debug (:obj:`bool`): Whether to use debug mode in the environment manager. Default to False.
             - wandb_sweep (:obj:`bool`): Whether to use wandb sweep, \
@@ -412,7 +414,8 @@ class DQNAgent:
     def best(self):
         """
         Overview:
-            Load the best model from the checkpoint directory, which is by default in folder ``exp_name/ckpt/eval.pth.tar``.
+            Load the best model from the checkpoint directory, \
+            which is by default in folder ``exp_name/ckpt/eval.pth.tar``.
         Returns:
             - (:obj:`DQNAgent`): The agent with the best model.
         
