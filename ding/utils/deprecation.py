96471b0d3135a2d381c70bf1cfc2a1fdd232aa3d
ooooo <3164076421@qq.com>
2024-03-07 00:06:47 +0800

    apply suggestion: \n- add unittest\n- add typelint\n- fix docstring style

--- ding/utils/deprecation.py
+++ ding/utils/deprecation.py
@@ -1,42 +1,38 @@
 import functools
 import textwrap
 import warnings
+from typing import Optional
 
 
-def deprecated(since, removed_in, up_to=None):
-    """Decorate a function to signify its deprecation.
+def deprecated(since: str, removed_in: str, up_to: Optional[str] = None):
+    """
+    Overview:
+        Decorate a function to signify its deprecation.
+    Arguments:
+        - since (:obj:`str`): the version when the function was first deprecated.
+        - removed_in (:obj:`str`): the version when the function will be removed.
+        - up_to (:obj:`Optional[str]`): the new API users should use.
+    Returns:
+        - decorator (:obj:`Callable`): decorated function.
+    Examples:
+        >>> from ding.utils.deprecation import deprecated
+        >>> @deprecated('0.4.1', '0.5.1')
+        >>> def hello():
+        >>>     print('hello')
 
-    Args:
-        since: The version when the function was first deprecated.
-        removed_in: The version when the function will be removed.
-        up_to: The new API users should use.
     """
 
     def decorator(func):
         existing_docstring = func.__doc__ or ""
 
-        deprecated_doc = f'.. deprecated:: {since}\n    This will be removed in {removed_in}'
+        deprecated_doc = f'.. deprecated:: {since}\n    Deprecated and will be removed in version {removed_in}'
 
         if up_to is not None:
             deprecated_doc += f', please use `{up_to}` instead.'
         else:
             deprecated_doc += '.'
 
-        # split docstring at first occurrence of newline
-        summary_and_body = existing_docstring.split("\n", 1)
-
-        if len(summary_and_body) > 1:
-            summary, body = summary_and_body
-
-            body = textwrap.dedent(body)
-
-            new_docstring_parts = [deprecated_doc, "\n\n", summary, body]
-        else:
-            summary = summary_and_body[0]
-
-            new_docstring_parts = [deprecated_doc, "\n\n", summary]
-
-        func.__doc__ = "".join(new_docstring_parts)
+        func.__doc__ = deprecated_doc + "\n\n" + textwrap.dedent(existing_docstring)
 
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
