a9ebcfbe29958ef2445702920eb5a7fe8da98604
nighood <nighoodren@gmail.com>
2024-04-07 17:01:13 +0800

    polish(rjy): fix format

--- ding/rl_utils/adder.py
+++ ding/rl_utils/adder.py
@@ -130,7 +130,7 @@ class Adder(object):
             if cum_reward:
                 data[i]['reward'] = sum([data[i + j]['reward'] * (gamma ** j) for j in range(nstep)])
             else:
-                data[i]['reward'] = torch.stack([data[i + j]['reward'] for j in range(nstep)], dim = -1)
+                data[i]['reward'] = torch.stack([data[i + j]['reward'] for j in range(nstep)], dim=-1)
             data[i]['done'] = data[i + nstep - 1]['done']
             if correct_terminate_gamma:
                 data[i]['value_gamma'] = gamma ** nstep
@@ -143,7 +143,7 @@ class Adder(object):
                 data[i]['reward'] = torch.stack(
                     [data[i + j]['reward']
                      for j in range(len(data) - i)] + [fake_reward for _ in range(nstep - (len(data) - i))],
-                    dim = -1
+                    dim=-1
                 )
             # try:
             #     assert len(data[i]['reward']) == 300
