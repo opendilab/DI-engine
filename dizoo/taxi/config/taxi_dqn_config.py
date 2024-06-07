982a05fca7e4dd4abad9bfecb662e33ce0c8ed86
ruiheng123 <121342992+ruiheng123@users.noreply.github.com>
2024-06-07 17:00:17 +0800

    Add files via upload

--- dizoo/taxi/config/taxi_dqn_config.py
+++ dizoo/taxi/config/taxi_dqn_config.py
@@ -6,34 +6,35 @@ taxi_dqn_config = dict(
         collector_env_num=8,
         evaluator_env_num=8,  
         n_evaluator_episode=10,
-        max_episode_steps=300,
+        stop_value=500_000_000,
+        max_episode_steps=100,
         env_id="Taxi-v3"
     ),
     policy=dict(
         cuda=True,
         load_path="./taxi_dqn_seed0/ckpt/ckpt_best.pth.tar",
         model=dict(
-            obs_shape=4,    
+            obs_shape=34,    
             action_shape=6,
-            encoder_hidden_size_list=[256, 128, 64]
+            encoder_hidden_size_list=[128, 128]
         ),
         nstep=3,
-        discount_factor=0.98,
+        discount_factor=0.99,
         learn=dict(
             update_per_collect=5,
-            batch_size=128,
-            learning_rate=0.001,
+            batch_size=64,
+            learning_rate=0.0001,
         ),
-        collect=dict(n_sample=10),
-        eval=dict(evaluator=dict(eval_freq=5, )),
+        collect=dict(n_sample=32),
+        eval=dict(evaluator=dict(eval_freq=5000, )),
         other=dict(
             eps=dict(
               type="linear",
-              start=0.8,
-              end=0.1,
-              decay=10000  
+              start=1,
+              end=0.01,
+              decay=1000000  
             ),
-            replay_buffer=dict(replay_buffer_size=20000,),
+            replay_buffer=dict(replay_buffer_size=10000,),
         ),
     )
 )
@@ -55,4 +56,4 @@ create_config = taxi_dqn_create_config
 
 if __name__ == "__main__":
     from ding.entry import serial_pipeline
-    serial_pipeline((main_config, create_config), max_env_step=5000, seed=0)
+    serial_pipeline((main_config, create_config), max_env_step=1000000, seed=0)
