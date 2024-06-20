4906c68465db1015b82d618f50049df63b207d74
ruiheng123 <121342992+ruiheng123@users.noreply.github.com>
2024-06-20 13:36:41 +0800

    feature(wrh): update taxi config

--- dizoo/taxi/config/taxi_dqn_config.py
+++ dizoo/taxi/config/taxi_dqn_config.py
@@ -1,40 +1,45 @@
 from easydict import EasyDict
 
 taxi_dqn_config = dict(
-    exp_name='taxi_seed0',
+    exp_name='taxi_dqn_seed0',
     env=dict(
         collector_env_num=8,
-        evaluator_env_num=8,  
-        n_evaluator_episode=10,
-        stop_value=500_000_000,
-        max_episode_steps=100,
-        env_id="Taxi-v3"
+        evaluator_env_num=8,
+        n_evaluator_episode=8,   
+        stop_value=20,           
+        max_episode_steps=60,    
+        env_id="Taxi-v3" 
     ),
     policy=dict(
         cuda=True,
-        load_path="./taxi_dqn_seed0/ckpt/ckpt_best.pth.tar",
         model=dict(
-            obs_shape=34,    
+            obs_shape=34,
             action_shape=6,
             encoder_hidden_size_list=[128, 128]
         ),
+        random_collect_size=5000,
         nstep=3,
         discount_factor=0.99,
         learn=dict(
-            update_per_collect=5,
+            update_per_collect=10,
             batch_size=64,
             learning_rate=0.0001,
+            learner=dict(
+                hook=dict(
+                    log_show_after_iter=1000,
+                )
+            ),
         ),
         collect=dict(n_sample=32),
-        eval=dict(evaluator=dict(eval_freq=5000, )),
+        eval=dict(evaluator=dict(eval_freq=1000, )), 
         other=dict(
             eps=dict(
               type="linear",
               start=1,
-              end=0.01,
-              decay=1000000  
-            ),
-            replay_buffer=dict(replay_buffer_size=50000,),
+              end=0.05,
+              decay=3000000                             
+            ),                                      
+            replay_buffer=dict(replay_buffer_size=100000,),  
         ),
     )
 )
@@ -56,4 +61,4 @@ create_config = taxi_dqn_create_config
 
 if __name__ == "__main__":
     from ding.entry import serial_pipeline
-    serial_pipeline((main_config, create_config), max_env_step=1000000, seed=0)
+    serial_pipeline((main_config, create_config), max_env_step=3000000, seed=20)
